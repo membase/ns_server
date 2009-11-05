086121f8e9475488af1cdf872512b3a3b92e7b94
Aliaksey Kandratsenka <alk@tut.by>
2009-11-05 17:49:20 +0200

    initial successfull graph plotting

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,17 @@ $(function () {
     var sec = $.bbq.getState('sec') || 'overview';
     $('#middle_pane > div').css('display', 'none');
     $('#'+sec).css('display','block');
+    setTimeout(function () {
+      $(window).trigger('sec:' + sec);
+    }, 10);
   });
 
-  $(function () {
-    $(window).trigger('hashchange');
+  $(window).trigger('hashchange');
+
+  $(window).bind('sec:overview', function () {
+    var arr = [10, 5, 46, 100, 74, 25];
+    for (var i = 0; i < 2; i++)
+      arr = arr.concat(arr);
+    $('#testgraph').sparkline(arr, {height: 60, width: 300});
   });
 });
