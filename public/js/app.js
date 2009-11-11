05ce521d2b22892f39e29ce322cfa57179d6a9c1
Aliaksey Kandratsenka <alk@tut.by>
2009-11-11 18:09:51 +0200

    auth and refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,149 @@
+/**
+*
+*  Base64 encode / decode
+*  http://www.webtoolkit.info/
+*
+**/
+// ALK Note: we might want to rewrite this.
+// webtoolkit.info license doesn't permit removal of comments which js minifiers do.
+// also utf8 handling functions are not really utf8, but CESU-8.
+// I.e. it doesn't handle utf16 surrogate pairs at all
+var Base64 = {
+ 
+	// private property
+	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+ 
+	// public method for encoding
+	encode : function (input) {
+		var output = "";
+		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+		var i = 0;
+ 
+		input = Base64._utf8_encode(input);
+ 
+		while (i < input.length) {
+ 
+			chr1 = input.charCodeAt(i++);
+			chr2 = input.charCodeAt(i++);
+			chr3 = input.charCodeAt(i++);
+ 
+			enc1 = chr1 >> 2;
+			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+			enc4 = chr3 & 63;
+ 
+			if (isNaN(chr2)) {
+				enc3 = enc4 = 64;
+			} else if (isNaN(chr3)) {
+				enc4 = 64;
+			}
+ 
+			output = output +
+			this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+			this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+ 
+		}
+ 
+		return output;
+	},
+ 
+	// public method for decoding
+	decode : function (input) {
+		var output = "";
+		var chr1, chr2, chr3;
+		var enc1, enc2, enc3, enc4;
+		var i = 0;
+ 
+		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+ 
+		while (i < input.length) {
+ 
+			enc1 = this._keyStr.indexOf(input.charAt(i++));
+			enc2 = this._keyStr.indexOf(input.charAt(i++));
+			enc3 = this._keyStr.indexOf(input.charAt(i++));
+			enc4 = this._keyStr.indexOf(input.charAt(i++));
+ 
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
+ 
+			output = output + String.fromCharCode(chr1);
+ 
+			if (enc3 != 64) {
+				output = output + String.fromCharCode(chr2);
+			}
+			if (enc4 != 64) {
+				output = output + String.fromCharCode(chr3);
+			}
+ 
+		}
+ 
+		output = Base64._utf8_decode(output);
+ 
+		return output;
+ 
+	},
+ 
+	// private method for UTF-8 encoding
+	_utf8_encode : function (string) {
+//		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+ 
+		for (var n = 0; n < string.length; n++) {
+ 
+			var c = string.charCodeAt(n);
+ 
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			}
+			else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+			else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+ 
+		}
+ 
+		return utftext;
+	},
+ 
+	// private method for UTF-8 decoding
+	_utf8_decode : function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+ 
+		while ( i < utftext.length ) {
+ 
+			c = utftext.charCodeAt(i);
+ 
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+ 
+		}
+ 
+		return string;
+	}
+ 
+}
+
 function formatUptime(seconds, precision) {
   precision = precision || 8;
 
@@ -77,25 +223,56 @@ var StatGraphs = {
   }
 }
 
-$(function () {
-  window.nav = {};
+function addBasicAuth(xhr, login, password) {
+  var auth = 'Basic ' + Base64.encode(login + ':' + password);
+  xhr.setRequestHeader('Authorization', auth);
+}
 
-  nav.go = function (sec) {
-    $.bbq.pushState({sec: sec});
+$.ajaxSetup({
+  error: function () {
+    alert("FIXME: network or server-side error happened! We'll handle it better in the future.");
+  },
+  beforeSend: function (xhr) {
+    if (Page.login) {
+      addBasicAuth(xhr, Page.login, Page.password);
+    }
   }
+});
 
-  $(window).bind('hashchange', function () {
-    var sec = $.bbq.getState('sec') || 'overview';
-    $('#middle_pane > div').css('display', 'none');
-    $('#'+sec).css('display','block');
-    setTimeout(function () {
-      $(window).trigger('sec:' + sec);
-    }, 10);
-  });
+// port of bind() from prototype.js
+Function.prototype.bind = function () {
+  var self = this;
+  var args = $.makeArray(arguments);
+  var forThis = args.shift();
+  return function () {
+    return self.apply(forThis, args.concat($.makeArray(arguments)));
+  }
+};
 
-  $(window).trigger('hashchange');
 
-  function getStatsAsync(callback) {
+function deferringUntilReady(body) {
+  return function () {
+    if (Page.ready) {
+      body.apply(this, arguments);
+      return;
+    }
+    var self = this;
+    var args = arguments;
+    Page.onReady(function () {
+      body.apply(self, args);
+    });
+  }
+}
+
+var Page = {
+  ready: false,
+  onReady: function (thunk) {
+    if (Page.ready)
+      thunk.call(null);
+    else
+      $(window).one('dao:ready', function () {thunk();});
+  },
+  getStatsAsync: deferringUntilReady(function (callback) {
     setTimeout(function () {
       callback({
         stats: {
@@ -111,10 +288,17 @@ $(function () {
                   {name: 'serv2', port: 12323, running: false, uptime: 123123, cache: '', threads: 0, version: '123', os: 'win'},
                   {name: 'serv3', port: 12323, running: true, uptime: 12312, cache: '13gb', threads: 5, version: '123', os: 'bare metal'}]});
     }, 100);
-  }
-
-  $(window).bind('sec:overview', function () {
-    getStatsAsync(function (stats) {
+  }),
+  performLogin: function (login, password) {
+    this.login = login;
+    this.password = password;
+    $.post('/ping', {}, function () {
+      Page.ready = true;
+      $(window).trigger('dao:ready');
+    });
+  },
+  enterOverview: function () {
+    Page.getStatsAsync(function (stats) {
       StatGraphs.update(stats.stats);
       var rows = $.map(stats.stats.hot_keys, function (e) {
         return $.extend({}, e, {total: 0 + e.gets + e.misses});
@@ -123,6 +307,47 @@ $(function () {
 
       $('#server_list_container').get(0).innerHTML = tmpl('server_list_template', {rows: stats.servers});
     });
+  },
+  hookEvents: function () {
+    $(window).bind('sec:overview', Page.enterOverview.bind(Page));
+  }
+};
+
+function loginFormSubmit() {
+  var login = $('#login_form [name=login]').val();
+  var password = $('#login_form [name=password]').val();
+  Page.performLogin(login, password);
+  $(window).one('dao:ready', function () {
+    $('#login_dialog').jqmHide();
+  });
+  return false;
+}
+
+window.nav = {
+  go: function (sec) {
+    $.bbq.pushState({sec: sec});
+  }
+};
+
+$(function () {
+  $('#login_dialog').jqm({modal: true}).jqmShow();
+  setTimeout(function () {
+    $('#login_dialog [name=login]').get(0).focus();
+  }, 100);
+
+  Page.hookEvents();
+
+  Page.onReady(function () {
+    $(window).bind('hashchange', function () {
+      var sec = $.bbq.getState('sec') || 'overview';
+      $('#middle_pane > div').css('display', 'none');
+      $('#'+sec).css('display','block');
+      setTimeout(function () {
+        $(window).trigger('sec:' + sec);
+      }, 10);
+    });
+
+    $(window).trigger('hashchange');
   });
 
   $('#server_list_container .expander').live('click', function (e) {
--- run_sinatra.rb
+++ run_sinatra.rb
@@ -2,10 +2,42 @@
 
 require 'rubygems'
 require 'sinatra'
+require 'json'
+require 'pp'
 
 set :static, true
 set :public, File.expand_path(File.join(File.dirname(__FILE__), 'public'))
 
+module DAO
+  module_function
+  
+end
+
+helpers do
+  def auth_credentials
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    if @auth.provided?
+      @auth.credentials
+    end
+  end
+
+  def with_valid_user
+    user, password = *auth_credentials
+    # fake auth here
+    if user == 'admin' && password == 'admin'
+      yield user
+    else
+      throw(:halt, [401, "Not authorized\n"])
+    end
+  end
+end
+
 get "/" do
   redirect "/index.html"
+end
+
+post "/ping" do
+  with_valid_user do |user|
+    "pong"
+  end
 end
