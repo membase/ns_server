ce19780dc8cd4f520a2f1ee58b92afc00e4df1fe
Steve Yen <steve.yen@gmail.com>
2009-11-14 22:02:14 -0800

    multiget ascii

--- src/mclient_ascii.erl
+++ src/mclient_ascii.erl
@@ -6,6 +6,7 @@
 
 -record(msg, {cmd = "",
               key = "",
+              keys = [],
               flag = 0,
               expire = 0,
               cas = 0,
@@ -15,8 +16,12 @@ cmd(version, Sock, RecvCallback, _Msg) ->
     send_recv(Sock, <<"version\r\n">>,
               RecvCallback);
 
-cmd(get, _Sock, _RecvCallback, _Msg) ->
-    exit({unimplemented});
+cmd(get, Sock, RecvCallback, #msg{keys=Keys}) ->
+    ok = send(Sock, [<<"get ">>,
+                     lists:map(fun (K) -> [K, <<" ">>] end,
+                               Keys),
+                     <<"\r\n">>]),
+    get_recv(Sock, RecvCallback);
 
 cmd(set, Sock, RecvCallback, Msg) ->
     cmd_update(<<"set">>, Sock, RecvCallback, Msg);
@@ -49,6 +54,24 @@ cmd_update(Cmd, Sock, RecvCallback,
                      Data, <<"\r\n">>],
               RecvCallback).
 
+get_recv(Sock, RecvCallback) ->
+    Line = recv_line(Sock),
+    case Line of
+        {error, _}=Err -> Err;
+        {ok, <<"END\r\n">>} -> Line;
+        {ok, <<"VALUE ", Rest/binary>>} ->
+            Parse = io_lib:fread("~s ~u ~u\r\n", binary_to_list(Rest)),
+            {ok, [Key, Flag, DataSize], _} = Parse,
+            {ok, Data} = recv_data(Sock, DataSize + 2),
+            if is_function(RecvCallback) -> RecvCallback(Line,
+                                                         #msg{key=Key,
+                                                              flag=Flag,
+                                                              data=Data});
+               true -> ok
+            end,
+            get_recv(Sock, RecvCallback)
+    end.
+
 %% @doc send an iolist and receive a single line back.
 send_recv(Sock, IoList) ->
     send_recv(Sock, IoList, undefined).
@@ -56,7 +79,7 @@ send_recv(Sock, IoList) ->
 send_recv(Sock, IoList, RecvCallback) ->
     ok = send(Sock, IoList),
     RV = recv_line(Sock),
-    if is_function(RecvCallback) -> RecvCallback(RV);
+    if is_function(RecvCallback) -> RecvCallback(RV, #msg{});
        true -> ok
     end,
     RV.
@@ -65,7 +88,7 @@ send(Sock, IoList) ->
     gen_tcp:send(Sock, iolist_to_binary(IoList)).
 
 %% @doc receive binary data of specified number of bytes length.
-recv_data(_, 0) -> <<>>;
+recv_data(_, 0) -> {ok, <<>>};
 recv_data(Sock, NumBytes) ->
     case gen_tcp:recv(Sock, NumBytes) of
         {ok, Bin} -> {ok, Bin};
@@ -212,6 +235,23 @@ delete_test() ->
         ?assertMatch(RB, <<"DELETED\r\n">>),
 
         {ok, RB1} = send_recv(Sock, "get aaa-st\r\n", nil),
+        ?assertMatch(RB1, <<"END\r\n">>)
+    end)(),
+
+    ok = gen_tcp:close(Sock).
+
+get_test() ->
+    {ok, Sock} = gen_tcp:connect("localhost", 11211,
+                                 [binary, {packet, 0}, {active, false}]),
+    set_test_sock(Sock),
+
+    (fun () ->
+        {ok, RB} = cmd(get, Sock, nil,
+                       #msg{keys= [<<"aaa-st">>, <<"notkey1">>, <<"notkey2">>]}),
+        ?assertMatch(RB, <<"END\r\n">>),
+
+        {ok, RB1} = cmd(get, Sock, nil,
+                       #msg{keys= [<<"notkey0">>, <<"notkey1">>, <<"notkey2">>]}),
         ?assertMatch(RB1, <<"END\r\n">>)
     end)(),
 
