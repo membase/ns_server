c4f1332c83ef6f8445b4d5f5cecb51ae4db53fa9
Steve Yen <steve.yen@gmail.com>
2009-11-15 18:34:22 -0800

    started mc_header record for binary protocol

--- include/mc_client.hrl
+++ include/mc_client.hrl
@@ -4,5 +4,12 @@
                  flag = 0,
                  expire = 0,
                  cas = 0,
-                 data = <<>>}).
+                 data = <<>>,
+                 datatype = 0}).
+
+-record(mc_header, {status = 0,
+                    opaque = 0,
+                    keylen = 0,
+                    extlen = 0,
+                    bodylen = 0}).
 
