e234e7ec9597ac52b72effddfa80e3987fb05de0
Steve Yen <steve.yen@gmail.com>
2009-11-19 17:26:34 -0800

    added a CmdNum to sequence concurrent writes to the OutPid

--- src/mc_server_ascii.erl
+++ src/mc_server_ascii.erl
@@ -8,27 +8,27 @@
 
 -compile(export_all).
 
-process(InSock, OutPid, {ModName, SessData}, Line) ->
+process(InSock, OutPid, CmdNum, {ModName, SessData}, Line) ->
     [Cmd | CmdArgs] = string:tokens(binary_to_list(Line), " "),
     {ok, SessData2} = apply(ModName, cmd,
                             [list_to_atom(Cmd), SessData,
-                             InSock, OutPid, CmdArgs]),
+                             InSock, OutPid, CmdNum, CmdArgs]),
     % TODO: Need error handling here, to send ERROR on unknown cmd.
     % TODO: Does the connection close during other process exit/error?
     {ok, {ModName, SessData2}}.
 
 session(UpstreamSock, Args) ->
     OutPid = spawn_link(?MODULE, loop_out, [UpstreamSock]),
-    loop_in(UpstreamSock, OutPid, Args).
+    loop_in(UpstreamSock, OutPid, 1, Args).
 
-loop_in(InSock, OutPid, Args) ->
+loop_in(InSock, OutPid, CmdNum, Args) ->
     {ok, Line} = mc_ascii:recv_line(InSock),
-    {ok, Args2} = process(InSock, OutPid, Args, Line),
-    loop_in(InSock, OutPid, Args2).
+    {ok, Args2} = process(InSock, OutPid, CmdNum, Args, Line),
+    loop_in(InSock, OutPid, CmdNum + 1, Args2).
 
 loop_out(OutSock) ->
     receive
-        {send, Data} ->
+        {send, _CmdNum, Data} ->
             ok = mc_ascii:send(OutSock, Data),
             loop_out(OutSock)
     end.
