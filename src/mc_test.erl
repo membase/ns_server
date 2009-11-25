47a944842c1d928eda02d1338e075339396da420
Steve Yen <steve.yen@gmail.com>
2009-11-25 11:25:00 -0800

    coverage reports

--- src/mc_test.erl
+++ src/mc_test.erl
@@ -12,21 +12,32 @@ main() ->
 % To run tests:
 %   erl -pa ebin -noshell -s mc_test test -s init stop
 %
+tests() -> [mc_ascii,
+            mc_client_ascii,
+            mc_client_ascii_ac,
+            mc_binary,
+            mc_client_binary,
+            mc_client_binary_ac,
+            mc_bucket,
+            mc_pool,
+            mc_downstream,
+            mc_server_ascii_proxy
+           ].
+
 test() ->
-    Tests = [mc_ascii,
-             mc_client_ascii,
-             mc_client_ascii_ac,
-             mc_binary,
-             mc_client_binary,
-             mc_client_binary_ac,
-             mc_bucket,
-             mc_pool,
-             mc_downstream,
-             mc_server_ascii_proxy
-            ],
-    lists:foreach(fun (Test) ->
-                          io:format("  ~p...~n", [Test]),
-                          apply(Test, test, [])
-                  end,
-                  Tests).
+    cover:start(),
+    cover:compile_directory("src", [{i, "include"}]),
+    Tests = tests(),
+    lists:foreach(
+      fun (Test) ->
+              io:format("  ~p...~n", [Test]),
+              apply(Test, test, []),
+              {ok, Cov} =
+                  cover:analyse_to_file(Test,
+                                        atom_to_list(Test) ++ ".cov.html",
+                                        [html])
+      end,
+      Tests),
+    ok.
+
 
