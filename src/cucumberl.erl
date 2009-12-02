8d4f8d2e0ed74e40d008614e0b74126d28c67263
Steve Yen <steve.yen@gmail.com>
2009-12-01 19:13:06 -0800

    updated to latest cucumberl

--- src/cucumberl.erl
+++ src/cucumberl.erl
@@ -4,14 +4,19 @@
 
 -compile(export_all).
 
+-record(stats, {scenarios = 0,
+                steps = 0}).
+
 % Cucumber parser & driver in erlang, in a single file,
 % implementing a subset of the cucumber/gherkin DSL.
 %
 % Example step implementation pattern in erlang...
 %
-%   step([given, i, have, entered, N, into, the, calculator], _Line) ->
+%   step([given, i, have, entered, N, into, the, calculator], _Info) ->
 %       % Your step implementation here.
 %       anything_but_undefined.
+%
+% The Info is a {Line, LineNum} tuple.
 %
 % Example run...
 %
@@ -20,40 +25,94 @@
 run(FilePath)              -> run(FilePath, []).
 run(FilePath, StepModules) -> run(FilePath, StepModules, 1).
 run(FilePath, StepModules, LineNumStart) ->
-    StepModulesX = StepModules ++ [?MODULE],
-    Lines = lines(FilePath),
-    run_lines(Lines, StepModulesX, LineNumStart).
+    run_lines(lines(FilePath), StepModules, LineNumStart).
 
 run_lines(Lines, StepModules, LineNumStart) ->
-    {_, _, _} =
+    StepModulesEx = StepModules ++ [?MODULE],
+    NumberedLines = numbered_lines(Lines),
+    ExpandedLines = expanded_lines(NumberedLines),
+    {_, _, #stats{scenarios = NScenarios, steps = NSteps}} =
         lists:foldl(
-          fun (Line, {Section, GWT, LineNum} = Acc) ->
+          fun ({LineNum, _Line} = LNL,
+               {Section, GWT, Stats} = Acc) ->
               case LineNum >= LineNumStart of
-                  true  -> process_line(Line, Acc, StepModules);
-                  false -> {Section, GWT, LineNum + 1}
+                  true  -> process_line(LNL, Acc, StepModulesEx);
+                  false -> {Section, GWT, Stats}
               end
           end,
-          {undefined, undefined, 1}, Lines),
+          {undefined, undefined, #stats{}}, ExpandedLines),
+    io:format("~n~p scenarios~n~p steps~n~n",
+              [NScenarios, NSteps]),
     ok.
 
-process_line(Line, {Section, GWT, LineNum}, StepModules) ->
+expanded_lines(NumberedLines) ->
+    % Expand "Scenario Outlines" or tables.
+    {_, _, ExpandedLines} =
+        lists:foldl(
+          fun({_LineNum, Line} = LNL,
+              {LastScenarioOutline, RowHeader, Out}) ->
+             case {LastScenarioOutline, RowHeader, string_to_atoms(Line)} of
+                 {undefined, _, ['scenario', 'outline:' | _]} ->
+                     {[LNL], undefined, Out};
+                 {undefined, _, _} ->
+                     {undefined, undefined, [LNL | Out]};
+                 {LSO, _, ['examples:' | _]} ->
+                     {lists:reverse(LSO), undefined, Out};
+                 {LSO, undefined, ['|' | _] = Row} ->
+                     {LSO, evens(Row), Out};
+                 {LSO, _, ['|' | _] = Row} ->
+                     ESO = lists:reverse(
+                             expand_scenario_outline(LSO, RowHeader,
+                                                     evens(Row))),
+                     {LSO, RowHeader, ESO ++ Out};
+                 {_, _, []} ->
+                     {undefined, undefined, [LNL | Out]};
+                 {LSO, _, _} ->
+                     {[LNL | LSO], RowHeader, Out}
+             end
+          end,
+          {undefined, undefined, []},
+          NumberedLines),
+    lists:reverse(ExpandedLines).
+
+expand_scenario_outline(ScenarioLines, RowHeader, RowTokens) ->
+    KeyValList = lists:zip(RowHeader, RowTokens),
+    lists:map(fun ({LineNum, Line}) ->
+                  {Strs, Placeholders} =
+                      unzip_odd_even(string:tokens(Line, "<>")),
+                  Replacements =
+                      lists:map(
+                        fun (Placeholder) ->
+                            K = list_to_atom(Placeholder),
+                            case lists:keysearch(K, 1, KeyValList) of
+                                {value, {K, Val}} -> atom_to_list(Val)
+                            end
+                        end,
+                        Placeholders),
+                  Line2 =
+                      lists:foldl(fun (X, Acc) -> Acc ++ X end,
+                                  "", zip_odd_even(Strs, Replacements)),
+                  {LineNum, Line2}
+              end,
+              ScenarioLines).
+
+process_line({LineNum, Line},
+             {Section, GWT, #stats{scenarios = NScenarios,
+                                   steps = NSteps} = Stats},
+             StepModules) ->
     % GWT stands for given-when-then.
     % GWT is the previous line's given-when-then atom.
     io:format("~s:~s ",
               [string:left(Line, 65),
                string:left(integer_to_list(LineNum), 4)]),
+
     % Handle quoted sections by spliting by "\"" first.
     {TokenStrs, QuotedStrs} =
         unzip_odd_even(string:tokens(Line, "\"")),
+
     % Atomize the unquoted sections.
-    TokenAtoms =
-      lists:map(fun (X) ->
-                    lists:map(fun (Y) ->
-                                  list_to_atom(string:to_lower(Y))
-                              end,
-                              string:tokens(X, " "))
-                end,
-                TokenStrs),
+    TokenAtoms = lists:map(fun string_to_atoms/1, TokenStrs),
+
     % Zip it back together into a Tokens list that might look like...
     %   [given, i, have, entered, "Joe Armstrong", as, my, name]
     % or
@@ -64,12 +123,22 @@ process_line(Line, {Section, GWT, LineNum}, StepModules) ->
     % Some atoms are reserved words in erlang ('when', 'if', 'then')
     % and need single quoting.
     %
-    Tokens = zip_odd_even(TokenAtoms, QuotedStrs, 1, []),
-    {Section2, GWT2, Result} =
+    Tokens = flat_zip_odd_even(TokenAtoms, QuotedStrs),
+
+    % Run through the StepModule steps, only if we are in a scenario
+    % section, otherwise, skip the line.
+    {Section2, GWT2, Result, Stats2} =
         case {Section, Tokens} of
-            {_, ['scenario:' | _]} -> {scenario,  undefined, undefined};
-            {_, []}                -> {undefined, undefined, undefined};
-            {undefined, _}         -> {undefined, undefined, undefined};
+            {_, ['scenario:' | _]} ->
+                {scenario, undefined, undefined,
+                 Stats#stats{scenarios = NScenarios + 1}};
+            {_, ['scenario', 'outline:' | _]} ->
+                {scenario, undefined, undefined,
+                 Stats#stats{scenarios = NScenarios + 1}};
+            {_, []}                ->
+                {undefined, undefined, undefined, Stats};
+            {undefined, _}         ->
+                {undefined, undefined, undefined, Stats};
             {scenario, [TokensHead | TokensTail]} ->
                 G = case {GWT, TokensHead} of
                         {undefined, _}    -> TokensHead;
@@ -81,26 +150,36 @@ process_line(Line, {Section, GWT, LineNum}, StepModules) ->
                           case Acc of
                               true  -> Acc;
                               false ->
-                                  S = StepModule:step([G | TokensTail], Line),
+                                  S = StepModule:step([G | TokensTail],
+                                                      {Line, LineNum}),
                                   S =/= undefined
                           end
                       end,
                       false, StepModules),
-                {Section, G, R}
+                {Section, G, R, Stats#stats{steps = NSteps + 1}}
         end,
+
+    % Emit result and our accumulator for our calling foldl.
     case {Section2, Result} of
         {scenario, true}  -> io:format("ok~n"),
-                             {Section2, GWT2, LineNum + 1};
-        {scenario, false} -> io:format("NO-STEP~n"),
-                             {undefined, undefined, LineNum + 1};
+                             {Section2, GWT2, Stats2};
+        {scenario, false} -> io:format("NO-STEP~n~n"),
+                             io:format("a step definition snippet...~n"),
+                             io:format("step(~p, _) ->~n  undefined.~n~n",
+                                       [Tokens]),
+                             {undefined, undefined, Stats2};
         _                 -> io:format("~n"),
-                             {Section2, GWT2, LineNum + 1}
+                             {Section2, GWT2, Stats2}
     end.
 
 step(['feature:' | _], _Line)  -> true;
 step(['scenario:' | _], _Line) -> true;
 step([], _) -> true;
 step(_, _)  -> undefined.
+
+numbered_lines(Lines) ->
+    NLines = length(Lines),
+    lists:zip(lists:seq(1, NLines, 1), Lines).
 
 lines(FilePath) ->
     {ok, FB} = file:read_file(FilePath),
@@ -113,19 +192,32 @@ lines([$\n | Rest], CurrLine, Lines) ->
 lines([X | Rest], CurrLine, Lines) ->
     lines(Rest, [X | CurrLine], Lines).
 
-% This zip_odd_even() also does flattening of Odds,
+% This flat_zip_odd_even() also does flattening of Odds,
 % since each Odd might be a list of atoms.
 
-zip_odd_even([], [], _F, Acc) ->
+flat_zip_odd_even(Odds, Evens) ->
+    zip_odd_even(flat, Odds, Evens, 1, []).
+
+zip_odd_even(Odds, Evens) ->
+    zip_odd_even(reg, Odds, Evens, 1, []).
+
+zip_odd_even(_, [], [], _F, Acc) ->
     lists:reverse(Acc);
-zip_odd_even([], [Even | Evens], F, Acc) ->
-    zip_odd_even([], Evens, F, [Even | Acc]);
-zip_odd_even([Odd | Odds], [], F, Acc) ->
-    zip_odd_even(Odds, [], F, lists:reverse(Odd) ++ Acc);
-zip_odd_even([Odd | Odds], Evens, 1, Acc) ->
-    zip_odd_even(Odds, Evens, 0, lists:reverse(Odd) ++ Acc);
-zip_odd_even(Odds, [Even | Evens], 0, Acc) ->
-    zip_odd_even(Odds, Evens, 1, [Even | Acc]).
+zip_odd_even(K, [], [Even | Evens], F, Acc) ->
+    zip_odd_even(K, [], Evens, F, [Even | Acc]);
+
+zip_odd_even(reg, [Odd | Odds], [], F, Acc) ->
+    zip_odd_even(reg, Odds, [], F, [Odd | Acc]);
+zip_odd_even(flat, [Odd | Odds], [], F, Acc) ->
+    zip_odd_even(flat, Odds, [], F, lists:reverse(Odd) ++ Acc);
+
+zip_odd_even(reg, [Odd | Odds], Evens, 1, Acc) ->
+    zip_odd_even(reg, Odds, Evens, 0, [Odd | Acc]);
+zip_odd_even(flat, [Odd | Odds], Evens, 1, Acc) ->
+    zip_odd_even(flat, Odds, Evens, 0, lists:reverse(Odd) ++ Acc);
+
+zip_odd_even(K, Odds, [Even | Evens], 0, Acc) ->
+    zip_odd_even(K, Odds, Evens, 1, [Even | Acc]).
 
 unzip_odd_even(Tokens) ->
     {Odds, Evens, _F} =
@@ -135,6 +227,36 @@ unzip_odd_even(Tokens) ->
                             0 -> {Odds, [X | Evens], 1}
                         end
                     end,
-                    {[], [], 1},
-                    Tokens),
+                    {[], [], 1}, Tokens),
     {lists:reverse(Odds), lists:reverse(Evens)}.
+
+evens(L) ->
+    {_Odds, Evens} = unzip_odd_even(L),
+    Evens.
+
+string_to_atoms(StrWords) ->
+    lists:map(fun (Y) -> list_to_atom(string:to_lower(Y)) end,
+              string:tokens(StrWords, " ")).
+
+% ------------------------------------
+
+unzip_test() ->
+    ?assertMatch({[], []}, unzip_odd_even([])),
+    ?assertMatch({[1], []}, unzip_odd_even([1])),
+    ?assertMatch({[1], [2]}, unzip_odd_even([1, 2])),
+    ?assertMatch({[1, 3], [2]}, unzip_odd_even([1, 2, 3])),
+    ?assertMatch({[1, 3, 5], [2, 4, 6]},
+                 unzip_odd_even([1, 2, 3, 4, 5, 6])).
+
+zip_test() ->
+    ?assertMatch([1, 2, 3, 4, 5, 6],
+                 zip_odd_even([1, 3, 5], [2, 4, 6])),
+    ?assertMatch([1, 2, 3, 4, 5, 6],
+                 flat_zip_odd_even([[1], [3], [5]], [2, 4, 6])).
+
+string_to_atoms_test() ->
+    ?assertMatch([], string_to_atoms("")),
+    ?assertMatch([a, bb, ccc],
+                 string_to_atoms("a bb ccc")),
+    ?assertMatch([a, bb, ccc],
+                 string_to_atoms("  a  bb   ccc  ")).
