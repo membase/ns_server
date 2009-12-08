09dc3e317604f6a211e30f89372b926a1f2eb3c2
Steve Yen <steve.yen@gmail.com>
2009-12-08 13:08:08 -0800

    sync_server from dynomite

---
+++ src/sync_server_sup.erl
@@ -0,0 +1,65 @@
+% Copyright (c) 2008, Cliff Moon
+% Copyright (c) 2008, Powerset, Inc
+% Copyright (c) 2009, NorthScale, Inc
+%
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions
+% are met:
+%
+% * Redistributions of source code must retain the above copyright
+% notice, this list of conditions and the following disclaimer.
+% * Redistributions in binary form must reproduce the above copyright
+% notice, this list of conditions and the following disclaimer in the
+% documentation and/or other materials provided with the distribution.
+% * Neither the name of Powerset, Inc nor the names of its
+% contributors may be used to endorse or promote products derived from
+% this software without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+% COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+% BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+% LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+% CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
+% Original Author: Cliff Moon
+
+-module(sync_server_sup).
+
+-behaviour(supervisor).
+
+%% API
+
+-export([start_link/0, sync_servers/0]).
+
+%% Supervisor callbacks
+
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link() ->
+  supervisor:start_link({local, sync_server_sup}, sync_server_sup, []).
+
+sync_servers() ->
+  lists:filter(fun (undefined) -> false;
+                   (_Child)    -> true
+               end,
+               lists:map(
+                 fun({_Id, Child, _Type, _Modules}) ->
+                         Child
+                 end,
+                 supervisor:which_children(sync_server_sup))).
+
+init([]) ->
+  ChildSpecs = [],
+  {ok, {{one_for_one, 10, 1}, ChildSpecs}}.
+
