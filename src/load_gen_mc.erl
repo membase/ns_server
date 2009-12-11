5fba52857d3f59be4c15fecc34ab3f433fe89fa7
Steve Yen <steve.yen@gmail.com>
2009-12-11 14:58:50 -0800

    load_gen works

--- src/load_gen_mc.erl
+++ src/load_gen_mc.erl
@@ -10,21 +10,27 @@
 
 -compile(export_all).
 
-start(feeder, McHost, McPort, NConns, N) ->
-    start(feeder, McHost, McPort, NConns, N, self()).
-
-start(feeder, _McHost, _McPort, _NConns, 0, _LoadGenPid) -> ok;
-start(feeder, McHost, McPort, NConns, N, LoadGenPid) ->
-    spawn(fun () ->
-              LoadGenPid ! {request, {connect, McHost, McPort, NConns}},
-              LoadGenPid ! {request, {work, all, unused}},
-              LoadGenPid ! input_complete
-          end),
-    start(feeder, McHost, McPort, NConns, N - 1, LoadGenPid).
+main() ->
+    {ok, FeederPid} = start_feeder("127.0.0.1", 11211, 1),
+    {ok, ResultPid} = start_results(),
+    load_gen:start(load_gen_mc, FeederPid, ResultPid).
 
 % --------------------------------------------------------
 
-start(results) -> results_loop().
+start_feeder(McHost, McPort, NConns) ->
+    start_feeder(McHost, McPort, NConns, self()).
+
+start_feeder(McHost, McPort, NConns, LoadGenPid) ->
+    FeederPid = spawn(fun () ->
+                          LoadGenPid ! {request, {connect, McHost, McPort, NConns}},
+                          LoadGenPid ! {request, {work, all, unused}},
+                          LoadGenPid ! input_complete
+                      end),
+    {ok, FeederPid}.
+
+% --------------------------------------------------------
+
+start_results() -> {ok, spawn(fun results_loop/0)}.
 
 results_loop() ->
     receive
@@ -43,27 +49,28 @@ init() -> request_loop([], []).
 
 request_loop(Socks, Workers) ->
     receive
-        {request, _From, {connect, McHost, McPort, NConns}} ->
+        {request, From, {connect, McHost, McPort, NConns} = Req} ->
             MoreSocks =
                 lists:map(
                   fun (_X) ->
                       case gen_tcp:connect(McHost, McPort,
                                            [binary, {packet, 0},
                                             {active, false}]) of
-                          {ok, Sock} -> Sock
+                          {ok, Sock} -> Sock;
+                          Error      -> From ! {response, self(), Req, Error}
                       end
                   end,
                   lists:seq(1, NConns)),
             request_loop(MoreSocks ++ Socks, Workers);
 
-        {request, _From, {work, all, Args}} ->
-            MoreWorkers = spawn_workers(Socks, Args),
+        {request, From, {work, all, Args} = Req} ->
+            MoreWorkers = spawn_workers(From, Req, Socks, Args),
             request_loop([], MoreWorkers ++ Workers);
 
-        {request, _From, {work, N, Args}} ->
+        {request, From, {work, N, Args} = Req} ->
             {WorkerSocks, RemainingSocks} =
                 lists:split(erlang:min(N, length(Socks)), Socks),
-            MoreWorkers = spawn_workers(WorkerSocks, Args),
+            MoreWorkers = spawn_workers(From, Req, WorkerSocks, Args),
             request_loop(RemainingSocks, MoreWorkers ++ Workers);
 
         {request, _From, stop} ->
@@ -78,16 +85,24 @@ stop_workers([])              -> ok;
 stop_workers([Worker | Rest]) -> Worker ! stop,
                                  stop_workers(Rest).
 
-spawn_workers(Socks, Args) ->
-    lists:map(fun (Sock) -> spawn(fun () -> loop(Sock, Args) end) end,
+spawn_workers(From, Req, Socks, Args) ->
+    lists:map(fun (Sock) ->
+                  spawn(fun () ->
+                            loop(From, Req, Sock, 1, Args)
+                        end)
+              end,
               Socks).
 
-loop(Sock, N) ->
+loop(From, Req, Sock, N, Args) ->
+    case N rem 1000 of
+        0 -> From ! {response_progress, self(), Req, N};
+        _ -> ok
+    end,
     receive
         stop -> ok
     after 0 ->
         {ok, _H, _E} = mc_client_binary:cmd(?NOOP, Sock, undefined, blank_he()),
-        loop(Sock, N + 1)
+        loop(From, Req, Sock, N + 1, Args)
     end.
 
 blank_he() ->
