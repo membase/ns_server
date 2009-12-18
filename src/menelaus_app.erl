afca48b29339d48658fa25f8b5b09c26b25f7909
Aliaksey Kandratsenka <alk@tut.by>
2009-12-18 17:22:04 +0200

    added start_subapp/0 function to menelaus_app

--- src/menelaus_app.erl
+++ src/menelaus_app.erl
@@ -7,12 +7,16 @@
 -author('Northscale <info@northscale.com>').
 
 -behaviour(application).
--export([start/2,stop/1]).
+-export([start/2,stop/1,start_subapp/0]).
 
 
 %% @spec start(_Type, _StartArgs) -> ServerRet
 %% @doc application start callback for menelaus.
 start(_Type, _StartArgs) ->
+    menelaus_deps:ensure(),
+    menelaus_sup:start_link().
+
+start_subapp() ->
     menelaus_deps:ensure(),
     menelaus_sup:start_link().
 
