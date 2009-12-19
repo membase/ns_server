e21bd71d6d8b392ef5b5a080676733b7e42cb883
Dustin Sallings <dustin@spy.net>
2009-12-19 12:53:10 -0800

    Separated node disco from node conf event handler.

---
+++ src/node_disco_conf_events.erl
@@ -0,0 +1,35 @@
+-module(node_disco_conf_events).
+
+-behaviour(gen_event).
+
+%% gen_event callbacks
+-export([start_link/0, init/1, handle_event/2, handle_call/2,
+         handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {disco}).
+
+% start_link is required by gen_event, but *never* makes sense because
+% the event handlers are not processes.
+start_link() ->
+    error.
+
+init(DiscoPid) ->
+    {ok, #state{disco=DiscoPid}}.
+
+handle_event(Event, State) ->
+    error_logger:info_msg("Config change:  ~p~n", [Event]),
+    {ok, State}.
+
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+handle_info(_Info, State) ->
+    {ok, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
--- src/ns_node_disco.erl
+++ src/ns_node_disco.erl
@@ -1,48 +1,23 @@
 
 -module(ns_node_disco).
 
--behaviour(gen_event).
 %% API
--export([start_link/0, begin_disco/0, loop/0]).
-
-%% gen_event callbacks
--export([init/1, handle_event/2, handle_call/2,
-         handle_info/2, terminate/2, code_change/3]).
-
--record(state, {disco}).
+-export([start_link/0, init/0, loop/0]).
 
 start_link() ->
-    {ok, spawn_link(?MODULE, begin_disco, [])}.
-
-init(DiscoPid) ->
-    {ok, #state{disco=DiscoPid}}.
-
-handle_event(Event, State) ->
-    error_logger:info_msg("Config change:  ~p~n", [Event]),
-    {ok, State}.
-
-handle_call(_Request, State) ->
-    Reply = ok,
-    {ok, Reply, State}.
-
-handle_info(_Info, State) ->
-    {ok, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+    {ok, spawn_link(?MODULE, init, [])}.
 
 %
 % Disco monitoring
 %
 
-begin_disco() ->
+init() ->
     _NL = init_known_servers(ns_config:search(ns_config:get(), stored_nodes)),
     update_node_list(),
     ok = net_kernel:monitor_nodes(true),
-    gen_event:add_handler(ns_config_events, ?MODULE, self()),
+    % the node_disco_conf_events gen_event handler will inform me when
+    % relevant configuration changes.
+    gen_event:add_handler(ns_config_events, node_disco_conf_events, self()),
     loop().
 
 init_known_servers({value, NodeList}) ->
