2c52df933e702c1ed7ad66058896faafd867234a
Steve Yen <steve.yen@gmail.com>
2010-01-02 13:00:15 -0800

    server-auto-detect handles stop msg, for symmetry with server_ascii/binary

--- src/mc_server_detect.erl
+++ src/mc_server_detect.erl
@@ -34,7 +34,9 @@ switch(ProtocolModule, ProcessorModule, ProcessorEnv,
 
 loop_out(OutSock) ->
     receive
-        {switch, ProtocolModule} -> ProtocolModule:loop_out(OutSock)
+        {switch, ProtocolModule} ->
+            ProtocolModule:loop_out(OutSock);
+        stop -> ok
     end.
 
 session(_Sock, ProcessorEnv) ->
