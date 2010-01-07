05a44d7fb6b918a6d6c3065a7953b070e644dd4a
Steve Yen <steve.yen@gmail.com>
2010-01-06 16:31:49 -0800

    added name param when starting erl

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2010, NorthScale, Inc.
 # All rights reserved.
 cd `dirname $0`
-exec erl -pa `find . -type d -name ebin` -boot start_sasl -eval 'application:start(ns_server).'
+./start_name.sh ns_1
---
+++ start_name.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Copyright (c) 2010, NorthScale, Inc.
+# All rights reserved.
+cd `dirname $0`
+exec erl -pa `find . -type d -name ebin` -name $1 -boot start_sasl -setcookie nocookie -eval 'application:start(ns_server).'
