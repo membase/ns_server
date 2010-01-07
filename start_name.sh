b262c34604d860b081f320936800536c443323cb
Steve Yen <steve.yen@gmail.com>
2010-01-06 22:37:31 -0800

    start_name.sh now takes multiple params

--- start_name.sh
+++ start_name.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2010, NorthScale, Inc.
 # All rights reserved.
 cd `dirname $0`
-exec erl -pa `find . -type d -name ebin` -name $1 -boot start_sasl -setcookie nocookie -eval 'application:start(ns_server).'
+exec erl -pa `find . -type d -name ebin` -name $1 -boot start_sasl -setcookie nocookie -eval 'application:start(ns_server).' $2 $3 $4 $5 $6 $7
