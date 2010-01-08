6ae4021373cfd8322a79869c468d6c2ac41f738c
Steve Yen <steve.yen@gmail.com>
2010-01-07 18:05:22 -0800

    doing config_pull on node_disco random timeout

--- src/ns_node_disco.erl
+++ src/ns_node_disco.erl
@@ -14,7 +14,8 @@
          cookie_init/0, cookie_get/0, cookie_set/1, cookie_sync/0,
          pool_join/2,
          pool_leave/1, pool_leave/0,
-         config_push/0, config_push/1, config_pull/0,
+         config_push/0, config_push/1, config_push/2,
+         config_pull/0, config_pull/1,
          loop/0]).
 
 -include_lib("eunit/include/eunit.hrl").
@@ -34,8 +35,12 @@ init() ->
     gen_event:add_handler(ns_config_events, ns_node_disco_conf_events, self()),
     loop().
 
+% Callback invoked when ns_config keys have changed.
+
 nodes_wanted_updated() ->
     nodes_wanted_updated(nodes_wanted()).
+
+% Callback invoked when ns_config keys have changed.
 
 nodes_wanted_updated(NodeListIn) ->
     cookie_sync(),
@@ -43,14 +48,18 @@ nodes_wanted_updated(NodeListIn) ->
     error_logger:info_msg("nodes_wanted updated ~p~n", [NodeList]),
     lists:filter(fun(N) -> net_adm:ping(N) == pong end, NodeList).
 
+% Read from ns_config nodes_wanted.
+
 nodes_wanted_raw() ->
     case ns_config:search(ns_config:get(), nodes_wanted) of
         {value, L} -> lists:usort(L);
         false      -> []
     end.
 
+% Read from ns_config nodes_wanted, and add ourselves to nodes_wanted,
+% if not already there.
+
 nodes_wanted() ->
-    % Add ourselves to nodes_wanted, if not already.
     W1 = nodes_wanted_raw(),
     W2 = lists:usort([node() | W1]),
     case W2 =/= W1 of
@@ -60,6 +69,7 @@ nodes_wanted() ->
     W2.
 
 % Returns all nodes that we see.
+% TODO: Track flapping nodes and attenuate.
 
 nodes_actual() ->
     lists:usort([node() | nodes()]).
@@ -67,8 +77,6 @@ nodes_actual() ->
 % Returns a subset of the nodes_wanted() that we see.
 
 nodes_actual_proper() ->
-    % TODO: Track flapping nodes and attenuate.
-    %
     Curr = nodes_actual(),
     Want = nodes_wanted(),
     Diff = lists:subtract(Curr, Want),
@@ -78,6 +86,8 @@ nodes_actual_proper() ->
 
 nodes_actual_other() ->
     lists:subtract(ns_node_disco:nodes_actual_proper(), [node()]).
+
+% -----------------------------------------------------------
 
 cookie_gen() ->
     {A1, A2, A3} = erlang:now(),
@@ -90,7 +100,7 @@ cookie_init() ->
                [NewCookie]),
     cookie_set(NewCookie).
 
-% Gets our wanted otp cookie.
+% Sets our wanted otp cookie.
 %
 cookie_set(Cookie) ->
     X = ns_config:set(otp, [{cookie, Cookie}]),
@@ -132,14 +142,27 @@ cookie_sync() ->
             end
     end.
 
+% --------------------------------------------------
+
 loop() ->
+    Timeout = 5000 + trunc(random:uniform() * 55000),
     receive
         {nodeup, Node} ->
-            error_logger:info_msg("new node: ~p~n", [Node]);
+            % Handle network partition healing here.  This is
+            % different than a node restarting, since in a network
+            % partition healing, nodes might be long-running and have
+            % diverged config.
+            error_logger:info_msg("new node: ~p~n", [Node]),
+            spawn(fun() ->
+                      % In a spawned process to not hold up nodeup/down.
+                      config_pull([Node], 1)
+                  end),
+            ok;
         {nodedown, Node} ->
             error_logger:info_msg("lost node: ~p~n", [Node])
+    after Timeout ->
+        config_pull(1)
     end,
-    % TODO: Need to update_node_list(),
     ?MODULE:loop().
 
 % --------------------------------------------------
@@ -195,24 +218,25 @@ pool_leave() ->
 % --------------------------------------------------
 
 config_push() ->
-    Dynamic = ns_config:get_dynamic(node()),
-    config_push(Dynamic).
+    config_push(ns_config:get_dynamic(node())).
 
 config_push(RawKVList) ->
-    Nodes = nodes_actual_other(),
-    misc:pmap(fun(Node) -> ns_config:set_remote(Node, RawKVList) end,
-              Nodes, length(Nodes), 2000),
-    Nodes.
+    config_push(RawKVList, nodes_actual_other()).
 
-config_pull() ->
-    config_pull(misc:shuffle(nodes_actual_other()), 5).
+config_push(RawKVList, OtherNodes) ->
+    misc:pmap(fun(Node) -> ns_config:set_remote(Node, RawKVList) end,
+              OtherNodes, length(OtherNodes), 2000).
+
+config_pull()  -> config_pull(5).
+config_pull(N) -> config_pull(misc:shuffle(nodes_actual_other()), N).
 
 config_pull([], _N)    -> ok;
-config_pull(_Nodes, 0) -> ok;
+config_pull(_Nodes, 0) -> error;
 config_pull([Node | Rest], N) ->
     case (catch ns_config:get_dynamic(Node)) of
         {'EXIT', _, _} -> config_pull(Rest, N - 1);
         {'EXIT', _}    -> config_pull(Rest, N - 1);
-        RemoteKVList   -> ns_config:set(RemoteKVList)
+        RemoteKVList   -> ns_config:set(RemoteKVList),
+                          ok
     end.
 
