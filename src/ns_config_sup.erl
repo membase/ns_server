27297e42e925f1f20d726ffa1620cf019b3484cc
Steve Yen <steve.yen@gmail.com>
2010-01-07 16:01:52 -0800

    made gen_event for ns_config the first spawned child

--- src/ns_config_sup.erl
+++ src/ns_config_sup.erl
@@ -17,14 +17,14 @@ init([]) ->
     error_logger:info_msg("loading config from ~p~n", [CfgPath]),
     {ok, {{rest_for_one, 3, 10},
           [
-           % current local state.
-           {ns_config,
-            {ns_config, start_link, [CfgPath, ns_config_default]},
-            permanent, 10, worker, [ns_config, ns_config_default]},
            % gen_event for the config events.
            {ns_config_events,
             {gen_event, start_link, [{local, ns_config_events}]},
             permanent, 10, worker, []},
+           % current local state.
+           {ns_config,
+            {ns_config, start_link, [CfgPath, ns_config_default]},
+            permanent, 10, worker, [ns_config, ns_config_default]},
            % listens for node discovery config and health changes.
            {ns_node_disco,
             {ns_node_disco, start_link, []},
