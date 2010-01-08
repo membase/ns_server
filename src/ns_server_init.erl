f6d0bf891f21a94f49f01826c25c9b97ba76a58c
Steve Yen <steve.yen@gmail.com>
2010-01-07 16:51:18 -0800

    nodes now do config_pull() and config_push() during initialization
    so, the key-value proplists in ns_config now implement the
    semantics of "last one wins", according to the erlang:now() timestamp.

--- src/ns_server_init.erl
+++ src/ns_server_init.erl
@@ -5,14 +5,16 @@
 
 -export([start_link/0, init/0]).
 
+% Final initialization steps with a transient worker process.
+%
 start_link() ->
     {ok, spawn_link(?MODULE, init, [])}.
 
-% Final initialization steps with a transient worker process.
-%
 init() ->
     % Update our config from remote nodes that are already running.
     ns_node_disco:config_pull(),
+    % And, we might have had changes even more recent that others.
+    ns_node_disco:config_push(),
     % Have ns_config announce all its keys so callbacks get going.
     ns_config:reannounce(),
     ok.
