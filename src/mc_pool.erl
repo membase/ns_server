2e94ebac8923c15aa294062560dd3db83c919c7e
Steve Yen <steve.yen@gmail.com>
2010-01-09 12:06:39 -0800

    mc_pool converted into a gen_server

--- src/mc_pool.erl
+++ src/mc_pool.erl
@@ -3,6 +3,8 @@
 
 -module(mc_pool).
 
+-behaviour(gen_server).
+
 -include_lib("eunit/include/eunit.hrl").
 
 -include("mc_constants.hrl").
@@ -13,37 +15,151 @@
 
 -record(mc_pool, {id,     % Pool id.
                   addrs,  % [mc_addr:create()*].
-                  config, % From ns_config:get().
+                  config, % A PoolConfig from ns_config:get().
                   buckets % [mc_bucket:create()*].
                   }).
 
-%% API for pool.
+%% API
+-export([start_link/1]).
 
-%% TODO: A proper implementation.
-%% TODO: Consider replacing implementation with gen_server.
+%% gen_server callbacks
+-export([init/1, terminate/2, code_change/3,
+         handle_call/3, handle_cast/2, handle_info/2]).
+
+start_link(Name) ->
+    gen_server:start_link({local, name_to_server_name(Name)},
+                          ?MODULE, Name, []).
+
+% -------------------------------------------------------
+
+init(Name) -> build_pool(Name, ns_config:get()).
+
+terminate(_Reason, _State)     -> ok.
+code_change(_OldVsn, State, _) -> {ok, State}.
+handle_cast(stop, State)       -> {stop, shutdown, State}.
+handle_info(_Info, State)      -> {noreply, State}.
+
+handle_call({get_bucket, BucketId}, _From, State) ->
+    {reply, get_bucket(State, BucketId), State};
+
+handle_call({auth_to_bucket, Mech, AuthData}, _From, State) ->
+    {reply, auth_to_bucket(State, Mech, AuthData), State};
+
+handle_call(_, _From, State) ->
+    {reply, ok, State}.
+
+%% API for pool.
 
 create(Id, Addrs, Config, Buckets) ->
     #mc_pool{id = Id, addrs = Addrs, config = Config, buckets = Buckets}.
 
+build_pool(Name, NSConfig) ->
+    case ns_config:search_prop(NSConfig, pools, Name) of
+        undefined ->
+            ns_log:log(?MODULE, 0001, "missing pool config: ~p", [Name]),
+            error;
+        PoolConfig ->
+            build_pool(Name, NSConfig, PoolConfig)
+    end.
+
+build_pool(Name, NSConfig, PoolConfig) ->
+    StartArgs =
+        case ns_port_server:get_port_server_config(NSConfig, memcached) of
+            undefined -> [];
+            {memcached, _Path, S}       -> S;
+            {memcached, _Path, S, _Env} -> S
+        end,
+    case ns_port_server:find_param("-p", StartArgs) of
+        false ->
+            ns_log:log(?MODULE, 0003, "missing memcached port"),
+            error;
+        {value, MemcachedPortStr} ->
+            MP = list_to_integer(MemcachedPortStr),
+            Nodes = ns_node_disco:nodes_actual_proper(),
+            create_pool(Name, PoolConfig, MP, Nodes)
+    end.
+
+create_pool(Name, PoolConfig, MemcachedPort, Nodes) ->
+    % {buckets, [
+    %   {"default", [
+    %     {auth_plain, undefined},
+    %     {size_per_node, 64}, % In MB.
+    %     {cache_expiration_range, {0,600}}
+    %   ]}
+    % ]}
+    BucketConfigs = proplists:get_value(buckets, PoolConfig, []),
+    Buckets =
+        lists:foldl(
+          fun({BucketName, BucketConfig}, Acc) ->
+                  case get_bucket_auth(BucketConfig) of
+                      error -> Acc;
+                      Auth ->
+                          BucketAddrs = nodes_to_addrs(Nodes, MemcachedPort,
+                                                       binary, Auth),
+                          Bucket = mc_bucket:create(BucketName,
+                                                    BucketAddrs,
+                                                    BucketConfig),
+                          [Bucket | Acc]
+                  end;
+             (X, Acc) ->
+                  ns_log:log(?MODULE, 0004, "bucket config error: ~p", [X]),
+                  Acc
+          end,
+          [], BucketConfigs),
+    PoolAddrs = nodes_to_addrs(Nodes, MemcachedPort, binary, undefined),
+    Pool = create(Name, PoolAddrs, PoolConfig, Buckets),
+    {ok, Pool}.
+
 % Returns {ok, Bucket} or false.
+
+get_bucket({mc_pool, Name}, BucketId) ->
+    gen_server:call(name_to_server_name(Name), {get_bucket, BucketId});
+
+get_bucket(PoolPid, BucketId) when is_pid(PoolPid) ->
+    gen_server:call(PoolPid, {get_bucket, BucketId});
+
 get_bucket(#mc_pool{buckets = Buckets}, BucketId) ->
     search_bucket(BucketId, Buckets).
+
+auth_to_bucket({mc_pool, Name}, Mech, AuthData) ->
+    gen_server:call(name_to_server_name(Name),
+                    {auth_to_bucket, Mech, AuthData});
+
+auth_to_bucket(PoolPid, Mech, AuthData) when is_pid(PoolPid) ->
+    gen_server:call(PoolPid,
+                    {auth_to_bucket, Mech, AuthData});
+
+auth_to_bucket(#mc_pool{} = Pool, "PLAIN", {BucketName, _AName, _APswd}) ->
+    % TODO: Proper auth_to_bucket() implementation.
+    {ok, get_bucket(Pool, BucketName)};
+
+auth_to_bucket(#mc_pool{}, _Mech, _AuthData) ->
+    error.
+
+% ------------------------------------------------
+
+name_to_server_name(Name) ->
+    list_to_atom("mc_pool-" ++ Name).
+
+get_bucket_auth(BucketConfig) ->
+    case proplists:get_value(auth_plain, BucketConfig) of
+        undefined                            -> undefined;
+        {_AuthName, _AuthPswd} = A           -> {"PLAIN", A};
+        {_ForName, _AuthName, _AuthPswd} = A -> {"PLAIN", A};
+        X -> ns_log:log(?MODULE, 0005, "bucket auth_plain config error: ~p",
+                        [X]),
+             error
+    end.
 
 nodes_to_addrs(Nodes, Port, Kind, Auth) ->
     PortStr = integer_to_list(Port),
     lists:map(fun(Node) ->
                   % Node is an atom like some_name@host.foo.bar.com
-                  [_Name, Host | _] = string:tokens(atom_to_list(Node), '@'),
-                  Location = Host ++ PortStr,
+                  [_Name, Host | _] = string:tokens(atom_to_list(Node), "@"),
+                  Location = lists:concat([Host, ":", PortStr]),
                   mc_addr:create(Location, Kind, Auth)
               end,
               Nodes).
-
-% TODO: Proper auth_to_bucket() implementation.
-auth_to_bucket(Pool, "PLAIN", {BucketName, _AuthName, _AuthPswd}) ->
-    {ok, get_bucket(Pool, BucketName)};
-auth_to_bucket(_Pool, _Mech, _AuthData) ->
-    error.
 
 % ------------------------------------------------
 
