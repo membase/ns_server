4fd55bfc1e20b328adc80cbd8115b855b1a18b93
Steve Yen <steve.yen@gmail.com>
2010-01-10 18:56:26 -0800

    default_data_directory() implemented

--- src/ns_config_default.erl
+++ src/ns_config_default.erl
@@ -3,7 +3,9 @@
 
 -module(ns_config_default).
 
--export([default/0, mergable/1]).
+-export([default/0, mergable/1,
+         default_data_directory/0,
+         find_root/1, is_root/1]).
 
 default() ->
     [{directory, default_data_directory()},
@@ -33,8 +35,41 @@ default() ->
     ].
 
 default_data_directory() ->
-    % TODO: Do something O/S specific here.
-    "/tmp/data".
+    % When installed, we live in something that looks like...
+    %
+    %   C:/Program Files/NorthScale/Server/
+    %     bin/
+    %       ns_server/ebin/ns_config_default.beam
+    %     priv/
+    %       config
+    %     data
+    %
+    % If we find a directory that h
+    %
+    P1 = filename:absname(code:which(ns_config_default)), % Our beam path.
+    P2 = filename:dirname(P1), % ".../ebin"
+    P3 = filename:dirname(P2), % ".../ns_server"
+    P4 = filename:dirname(P3), % might be sibling to /priv
+    RootPath = case find_root(P4) of
+                   false -> P3;
+                   X     -> X
+               end,
+    DataPath = filename:join(RootPath, "data"),
+    filelib:ensure_dir(DataPath),
+    DataPath.
+
+find_root("") -> false;
+find_root(".") -> false;
+find_root("/") -> false;
+find_root(DirPath) ->
+    case is_root(DirPath) of
+        true  -> DirPath;
+        false -> find_root(filename:dirname(DirPath))
+    end.
+
+is_root(DirPath) ->
+    filelib:is_dir(filename:join(DirPath, "bin")) =:= true andalso
+    filelib:is_dir(filename:join(DirPath, "priv")) =:= true.
 
 mergable(_CurrList) ->
     [otp,
