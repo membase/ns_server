6d48c460688eb556cee0963942035daa5e94ff51
Dustin Sallings <dustin@spy.net>
2010-01-12 15:13:43 -0800

    Log API.

--- src/ns_log.erl
+++ src/ns_log.erl
@@ -3,7 +3,7 @@
 
 -module(ns_log).
 
--export([log/3, log/4]).
+-export([log/3, log/4, recent/0]).
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -11,13 +11,19 @@
 
 % A Code is an number which is module-specific.
 %
+-spec log(atom(), integer(), string()) -> ok.
 log(Module, Code, Msg) ->
     error_logger:info_msg("~p-~p: ~p", [Module, Code, Msg]),
     ok.
 
+-spec log(atom(), integer(), string(), list()) -> ok.
 log(Module, Code, Fmt, Args) ->
     error_logger:info_msg("~p-~p: " ++ Fmt, [Module, Code | Args]),
     ok.
+
+-spec recent() -> list({atom(), integer(), string(), list()}).
+recent() ->
+    [].
 
 % TODO: Implement this placeholder api, possibly as a gen_server
 %       to track the last few log msgs in memory.  A client then might
