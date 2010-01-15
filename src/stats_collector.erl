eafe2026585911af9254cc520adb643017298a6e
Dustin Sallings <dustin@spy.net>
2010-01-15 00:53:18 -0800

    Declare monitoring of multiple buckets on a node.
    This allows us to fetch an arbitrary number of buckets over a single
    connection easily.

--- src/stats_collector.erl
+++ src/stats_collector.erl
@@ -5,23 +5,28 @@
 -behaviour(gen_event).
 %% API
 -export([start_link/0,
-         monitor/3, unmonitor/3]).
+         monitor/3, unmonitor/2]).
 
 %% gen_event callbacks
 -export([init/1, handle_event/2, handle_call/2,
          handle_info/2, terminate/2, code_change/3]).
 
--record(state, {hostname, port, bucket}).
+-record(state, {hostname, port, buckets}).
 
 start_link() ->
     {error, "Don't start_link this."}.
 
-init([Hostname, Port, Bucket]) ->
-    {ok, #state{hostname=Hostname, port=Port, bucket=Bucket}}.
+init([Hostname, Port, Buckets]) ->
+    lists:foreach(fun (Bucket) ->
+                          stats_aggregator:monitoring(Hostname,
+                                                      Port,
+                                                      Bucket)
+                  end, Buckets),
+    {ok, #state{hostname=Hostname, port=Port, buckets=Buckets}}.
 
 handle_event(collect, State) ->
-    error_logger:info_msg("Collecting from ~p@~p:~p.~n",
-                          [State#state.bucket,
+    error_logger:info_msg("Collecting from ~p@~s:~p.~n",
+                          [State#state.buckets,
                            State#state.hostname,
                            State#state.port]),
     {ok, State}.
@@ -33,7 +38,12 @@ handle_call(_Request, State) ->
 handle_info(_Info, State) ->
     {ok, State}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, State) ->
+    lists:foreach(fun (Bucket) ->
+                          stats_aggregator:unmonitoring(State#state.hostname,
+                                                        State#state.port,
+                                                        Bucket)
+                  end, State#state.buckets),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
@@ -43,11 +53,9 @@ code_change(_OldVsn, State, _Extra) ->
 %% Entry Points.
 %
 
-monitor(Hostname, Port, Bucket) ->
-    stats_aggregator:monitoring(Hostname, Port, Bucket),
-    ok = gen_event:add_handler(?SERVER, {?MODULE, {Hostname, Port, Bucket}},
-                               [Hostname, Port, Bucket]).
+monitor(Hostname, Port, Buckets) ->
+    ok = gen_event:add_handler(?SERVER, {?MODULE, {Hostname, Port}},
+                               [Hostname, Port, Buckets]).
 
-unmonitor(Hostname, Port, Bucket) ->
-    ok = gen_event:delete_handler(?SERVER, {?MODULE, {Hostname, Port, Bucket}}, []),
-    stats_aggregator:unmonitoring(Hostname, Port, Bucket).
+unmonitor(Hostname, Port) ->
+    ok = gen_event:delete_handler(?SERVER, {?MODULE, {Hostname, Port}}, []).
