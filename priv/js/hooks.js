c72833a89db2066c72f25cc5d2c597777f93b4a5
Aliaksey Kandratsenka <alk@tut.by>
2010-01-18 15:02:10 +0200

    initial REST API mocking refactoring

--- priv/js/hooks.js
+++ priv/js/hooks.js
@@ -82,11 +82,8 @@ var TestingSupervisor = {
                            shortText: "Server Node Down",
                            text: "Server node is no longer available"}]},
   interceptedAjax: function (original, options) {
-    if (options.type != 'GET') {
-      _.defer(function () {
-        options.success('', 'success');
-      });
-      return;
+    if (options.type != 'GET' && options.type != 'POST') {
+      throw new Error("unknown method: " + options.type);
     }
     console.log("intercepted ajax:", options);
     var url = options.url;
@@ -106,8 +103,13 @@ var TestingSupervisor = {
           data.call(null, fakeResponse);
           return;
         }
-        options.success.call(null, data);
+        options.success(data, 'success');
       });
+    }
+
+    if (options.type == 'POST') {
+      fakeResponse('');
+      return;
     }
 
     var resp;
@@ -119,81 +121,10 @@ var TestingSupervisor = {
            uri: "/pools/default"}]};
       } else {
         // /pools/:id
-        resp = {nodes: [{hostname: "mickey-mouse.disney.com",
-                         status: "healthy",
-                         ports: {proxy: 11211,
-                                 direct: 11311},
-                         otp_node: "ns1@mickey-mouse.disney.com",
-                         otp_cookie: "SADFDFGDFG"},
-                        {hostname: "donald-duck.disney.com",
-                         status: "healthy",
-                         ports: {proxy: 11211,
-                                 direct: 11311},
-                         otp_node: "ns1@donald-duck.disney.com",
-                         otp_cookie: "SADFDFGDFG"},
-                        {hostname: "goofy.disney.com",
-                         status: "healthy",
-                         ports: {proxy: 11211,
-                                 direct: 11311},
-                         otp_node: "ns1@goofy.disney.com",
-                         otp_cookie: "SADFDFGDFG"}],
-                buckets: {
-                  // GET returns first page of bucket details with link to next page
-                  uri: "/buckets",
-                  // returns just names and uris, but complete (i.e. without pagination)
-                  shallowList: "/buckets?shallow=true"
-                },
-                stats: {uri: "/buckets/4/stats?really_for_pool=1"},
-                name: "Default Pool"};
+        resp = this.handlePoolDetails();
       }
     } else if (path[0] == "buckets" && path.length == 1) {
-      resp = [{name: "default",
-               uri: "/buckets/4",
-               flushCacheURI: "/buckets/4/flush",
-               passwordURI: "/buckets/4/password",
-               basicStats: {
-                 cacheSize: 64, // in megs
-                 opsPerSec: 100,
-                 evictionsPerSec: 5,
-                 cachePercentUsed: 50
-               },
-               sampleConnectionString: "default sample connection string"},
-              {name: "Excerciser Application",
-               uri: "/buckets/5",
-               testAppBucket: true,
-               testAppRunning: false,
-               testAppStatusURI: "/testappuri",
-               flushCacheURI: "/buckets/5/flush",
-               passwordURI: "/buckets/5/password",
-               basicStats: {
-                 cacheSize: 65, // in megs
-                 opsPerSec: 101,
-                 evictionsPerSec: 6,
-                 cachePercentUsed: 51
-               },
-               sampleConnectionString: "Excerciser Application sample connection string"},
-              {name: "new-year-site",
-               uri: "/buckets/6",
-               flushCacheURI: "/buckets/6/flush",
-               passwordURI: "/buckets/6/password",
-               basicStats: {
-                 cacheSize: 66, // in megs
-                 opsPerSec: 102,
-                 evictionsPerSec: 7,
-                 cachePercentUsed: 52
-               },
-               sampleConnectionString: "new-year-site sample connection string"},
-              {name: "new-year-site-staging",
-               uri: "/buckets/7",
-               flushCacheURI: "/buckets/7/flush",
-               passwordURI: "/buckets/7/password",
-               basicStats: {
-                 cacheSize: 67, // in megs
-                 opsPerSec: 103,
-                 evictionsPerSec: 8,
-                 cachePercentUsed: 53
-               },
-               sampleConnectionString: "new-year-site-staging sample connection string"}]
+      resp = this.handleBucketList();
     } else if (path[0] == 'buckets') {
       if (path.length == 2) {
         // /buckets/:id
@@ -210,86 +141,7 @@ var TestingSupervisor = {
                   name: "default"};
       } else {
         // /buckets/:id/stats
-        var params = options["data"];
-        var opsPerSecondZoom = params['opsPerSecondZoom'] || "now";
-        var allSamples = {
-          '1hr': {"gets":[3,14,23,52,45,25,23,22,50,67,59,55,54,41,36,35,26,61,72,49,60],
-                  "misses":[23,14,45,64,41,45,43,25,14,11,18,36,64,76,86,86,79,78,55,59,49],
-                  "sets":[42,65,42,63,81,87,74,84,56,44,71,64,49,48,55,46,37,46,64,33,18],
-                  "ops":[61,65,64,75,77,57,68,76,64,61,66,63,68,37,32,60,72,54,43,41,55]},
-          'now': {"gets":[70,44,28,17,29,61,70,47,39,47,27,54,47,30,43,45,65,49,46,41,62],
-                  "misses":[67,48,45,29,18,53,57,59,78,57,41,41,29,34,34,43,51,58,63,71,78],
-                  "sets":[65,51,61,42,58,71,55,77,69,44,43,22,59,63,36,46,40,69,80,50,69],
-                  "ops":[63,55,27,30,35,57,39,38,32,17,38,49,61,78,82,71,41,35,25,44,68]},
-          '24hr': {"gets":[69,56,57,61,58,55,74,87,93,88,55,69,56,67,81,65,40,58,47,43,30],
-                   "misses":[14,45,56,45,42,43,29,23,47,23,40,60,45,54,64,40,28,19,59,48,60],
-                   "sets":[60,34,54,30,26,30,34,35,38,27,59,67,43,45,48,66,42,43,52,44,35],
-                   "ops":[6,24,53,64,35,30,45,50,31,32,29,50,28,30,30,40,30,54,39,37,58]}};
-        var samples = allSamples[opsPerSecondZoom];
-        var samplesSize = samples["gets"].length;
-
-        var samplesInterval = 5000;
-        if (opsPerSecondZoom == "24hr")
-          samplesInterval = 86400000 / samplesSize;
-        else if (opsPerSecondZoom == "1hr")
-          samplesInterval = 3600000 / samplesSize;
-
-        var now = (new Date()).valueOf();
-        var lastSampleTstamp = now;
-
-        if (samplesInterval == 5000) {
-          var rotates = ((now / 1000) >> 0) % samplesSize;
-          var newSamples = {};
-          for (var k in samples) {
-            var data = samples[k];
-            newSamples[k] = data.concat(data).slice(rotates, rotates + samplesSize);
-          }
-          samples = newSamples;
-        }
-
-        var startTstampParam = params["opsbysecondStartTStamp"];
-        if (startTstampParam !== undefined) {
-          var startTstamp = parseInt(startTstampParam, 10);
-
-          var intervals = Math.floor((now - startTstampParam) / samplesInterval);
-          if (intervals > samplesSize) {
-            throw new Error("should not happen");
-          }
-          lastSampleTstamp = startTstamp + intervals * samplesInterval;
-
-          var newSamples = {};
-          for (var k in samples) {
-            var data = samples[k];
-            var newData = data.slice(-intervals);
-            newSamples[k] = newData;
-          }
-          samples = newSamples;
-        }
-
-        var resp = {
-          hot_keys: [{name: "user:image:value",
-                      gets: 10000,
-                      bucket: "Excerciser application",
-                      misses: 100,
-                      type: "Persistent"},
-                     {name: "user:image:value2",
-                      gets: 10000,
-                      bucket: "Excerciser application",
-                      misses: 100,
-                      type: "Cache"},
-                     {name: "user:image:value3",
-                      gets: 10000,
-                      bucket: "Excerciser application",
-                      misses: 100,
-                      type: "Persistent"},
-                     {name: "user:image:value4",
-                      gets: 10000,
-                      bucket: "Excerciser application",
-                      misses: 100,
-                      type: "Cache"}],
-          op: _.extend({tstamp: lastSampleTstamp,
-                        'samplesInterval': samplesInterval},
-                       samples)};
+        resp = this.handleStats(options["data"]);
       }
     } else if (path[0] == 'alerts' && path.length == 1) {
       // /alerts
@@ -302,6 +154,165 @@ var TestingSupervisor = {
     _.defer(function () {
       fakeResponse(resp);
     });
+  },
+  handlePoolDetails: function () {
+    return {nodes: [{hostname: "mickey-mouse.disney.com",
+                     status: "healthy",
+                     ports: {proxy: 11211,
+                             direct: 11311},
+                     otp_node: "ns1@mickey-mouse.disney.com",
+                     otp_cookie: "SADFDFGDFG"},
+                    {hostname: "donald-duck.disney.com",
+                     status: "healthy",
+                     ports: {proxy: 11211,
+                             direct: 11311},
+                     otp_node: "ns1@donald-duck.disney.com",
+                     otp_cookie: "SADFDFGDFG"},
+                    {hostname: "goofy.disney.com",
+                     status: "healthy",
+                     ports: {proxy: 11211,
+                             direct: 11311},
+                     otp_node: "ns1@goofy.disney.com",
+                     otp_cookie: "SADFDFGDFG"}],
+            buckets: {
+              // GET returns first page of bucket details with link to next page
+              uri: "/buckets",
+              // returns just names and uris, but complete (i.e. without pagination)
+              shallowList: "/buckets?shallow=true"
+            },
+            stats: {uri: "/buckets/4/stats?really_for_pool=1"},
+            name: "Default Pool"}
+  },
+  handleBucketList: function () {
+    return [{name: "default",
+             uri: "/buckets/4",
+             flushCacheURI: "/buckets/4/flush",
+             passwordURI: "/buckets/4/password",
+             basicStats: {
+               cacheSize: 64, // in megs
+               opsPerSec: 100,
+               evictionsPerSec: 5,
+               cachePercentUsed: 50
+             },
+             sampleConnectionString: "default sample connection string"},
+            {name: "Excerciser Application",
+             uri: "/buckets/5",
+             testAppBucket: true,
+             testAppRunning: false,
+             testAppStatusURI: "/testappuri",
+             flushCacheURI: "/buckets/5/flush",
+             passwordURI: "/buckets/5/password",
+             basicStats: {
+               cacheSize: 65, // in megs
+               opsPerSec: 101,
+               evictionsPerSec: 6,
+               cachePercentUsed: 51
+             },
+             sampleConnectionString: "Excerciser Application sample connection string"},
+            {name: "new-year-site",
+             uri: "/buckets/6",
+             flushCacheURI: "/buckets/6/flush",
+             passwordURI: "/buckets/6/password",
+             basicStats: {
+               cacheSize: 66, // in megs
+               opsPerSec: 102,
+               evictionsPerSec: 7,
+               cachePercentUsed: 52
+             },
+             sampleConnectionString: "new-year-site sample connection string"},
+            {name: "new-year-site-staging",
+             uri: "/buckets/7",
+             flushCacheURI: "/buckets/7/flush",
+             passwordURI: "/buckets/7/password",
+             basicStats: {
+               cacheSize: 67, // in megs
+               opsPerSec: 103,
+               evictionsPerSec: 8,
+               cachePercentUsed: 53
+             },
+             sampleConnectionString: "new-year-site-staging sample connection string"}]
+  },
+  handleStats: function (params) {
+    var opsPerSecondZoom = params['opsPerSecondZoom'] || "now";
+    var allSamples = {
+      '1hr': {"gets":[3,14,23,52,45,25,23,22,50,67,59,55,54,41,36,35,26,61,72,49,60],
+              "misses":[23,14,45,64,41,45,43,25,14,11,18,36,64,76,86,86,79,78,55,59,49],
+              "sets":[42,65,42,63,81,87,74,84,56,44,71,64,49,48,55,46,37,46,64,33,18],
+              "ops":[61,65,64,75,77,57,68,76,64,61,66,63,68,37,32,60,72,54,43,41,55]},
+      'now': {"gets":[70,44,28,17,29,61,70,47,39,47,27,54,47,30,43,45,65,49,46,41,62],
+              "misses":[67,48,45,29,18,53,57,59,78,57,41,41,29,34,34,43,51,58,63,71,78],
+              "sets":[65,51,61,42,58,71,55,77,69,44,43,22,59,63,36,46,40,69,80,50,69],
+              "ops":[63,55,27,30,35,57,39,38,32,17,38,49,61,78,82,71,41,35,25,44,68]},
+      '24hr': {"gets":[69,56,57,61,58,55,74,87,93,88,55,69,56,67,81,65,40,58,47,43,30],
+               "misses":[14,45,56,45,42,43,29,23,47,23,40,60,45,54,64,40,28,19,59,48,60],
+               "sets":[60,34,54,30,26,30,34,35,38,27,59,67,43,45,48,66,42,43,52,44,35],
+               "ops":[6,24,53,64,35,30,45,50,31,32,29,50,28,30,30,40,30,54,39,37,58]}};
+    var samples = allSamples[opsPerSecondZoom];
+    var samplesSize = samples["gets"].length;
+
+    var samplesInterval = 5000;
+    if (opsPerSecondZoom == "24hr")
+      samplesInterval = 86400000 / samplesSize;
+    else if (opsPerSecondZoom == "1hr")
+      samplesInterval = 3600000 / samplesSize;
+
+    var now = (new Date()).valueOf();
+    var lastSampleTstamp = now;
+
+    if (samplesInterval == 5000) {
+      var rotates = ((now / 1000) >> 0) % samplesSize;
+      var newSamples = {};
+      for (var k in samples) {
+        var data = samples[k];
+        newSamples[k] = data.concat(data).slice(rotates, rotates + samplesSize);
+      }
+      samples = newSamples;
+    }
+
+    var startTstampParam = params["opsbysecondStartTStamp"];
+    if (startTstampParam !== undefined) {
+      var startTstamp = parseInt(startTstampParam, 10);
+
+      var intervals = Math.floor((now - startTstampParam) / samplesInterval);
+      if (intervals > samplesSize) {
+        throw new Error("should not happen");
+      }
+      lastSampleTstamp = startTstamp + intervals * samplesInterval;
+
+      var newSamples = {};
+      for (var k in samples) {
+        var data = samples[k];
+        newSamples[k] = data.slice(-intervals);
+      }
+      samples = newSamples;
+    }
+
+    return {hot_keys: [{name: "user:image:value",
+                        gets: 10000,
+                        bucket: "Excerciser application",
+                        misses: 100,
+                        type: "Persistent"},
+                       {name: "user:image:value2",
+                        gets: 10000,
+                        bucket: "Excerciser application",
+                        misses: 100,
+                        type: "Cache"},
+                       {name: "user:image:value3",
+                        gets: 10000,
+                        bucket: "Excerciser application",
+                        misses: 100,
+                        type: "Persistent"},
+                       {name: "user:image:value4",
+                        gets: 10000,
+                        bucket: "Excerciser application",
+                        misses: 100,
+                        type: "Cache"}],
+            op: _.extend({tstamp: lastSampleTstamp,
+                          'samplesInterval': samplesInterval},
+                         samples)};
+  },
+  interceptedPost: function (options) {
+    
   }
 };
 
