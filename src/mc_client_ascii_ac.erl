06f1a9e45b2df06548712223ce0561181105aa0f
Dustin Sallings <dustin@spy.net>
2010-01-18 23:57:17 -0800

    Getting rid of explicitly unimplemented code.
    It's going to crash either way.  Explicitly not implementing something
    means I have to fix each one of these lines when making a call
    signature change.

--- src/mc_client_ascii_ac.erl
+++ src/mc_client_ascii_ac.erl
@@ -27,25 +27,14 @@ cmd(Cmd, Sock, RecvCallback, Entry) ->
 %% For binary upstream talking to downstream ascii server.
 %% The RecvCallback function will receive ascii-oriented parameters.
 
-cmd_binary(?GET, _S, _RC, _E) -> exit(todo);
-
 cmd_binary(?SET, Sock, RecvCallback, Entry) ->
     cmd(set, Sock, RecvCallback, Entry);
-
-cmd_binary(?ADD, _S, _RC, _E) -> exit(todo);
-cmd_binary(?REPLACE, _S, _RC, _E) -> exit(todo);
 
 cmd_binary(?DELETE, Sock, RecvCallback, Entry) ->
     cmd(delete, Sock, RecvCallback, Entry);
 
-cmd_binary(?INCREMENT, _S, _RC, _E) -> exit(todo);
-cmd_binary(?DECREMENT, _S, _RC, _E) -> exit(todo);
-cmd_binary(?QUIT, _S, _RC, _E) -> exit(todo);
-
 cmd_binary(?FLUSH, Sock, RecvCallback, Entry) ->
     cmd(flush_all, Sock, RecvCallback, Entry);
-
-cmd_binary(?GETQ, _S, _RC, _E) -> exit(todo);
 
 cmd_binary(?NOOP, _Sock, RecvCallback, _Entry) ->
     % Assuming NOOP used to uncork GETKQ's.
@@ -57,39 +46,8 @@ cmd_binary(?NOOP, _Sock, RecvCallback, _Entry) ->
 cmd_binary(?VERSION, Sock, RecvCallback, Entry) ->
     cmd(version, Sock, RecvCallback, Entry);
 
-cmd_binary(?GETK, _S, _RC, _E) -> exit(todo);
-
 cmd_binary(?GETKQ, Sock, RecvCallback, Entry) ->
     cmd(get, Sock, RecvCallback, Entry);
-
-cmd_binary(?APPEND, _S, _RC, _E) -> exit(todo);
-cmd_binary(?PREPEND, _S, _RC, _E) -> exit(todo);
-
-cmd_binary(?STAT, _S, _RC, _E) -> exit(todo);
-
-cmd_binary(?SETQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?ADDQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?REPLACEQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?DELETEQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?INCREMENTQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?DECREMENTQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?QUITQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?FLUSHQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?APPENDQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?PREPENDQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RGET, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RSET, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RSETQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RAPPEND, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RAPPENDQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RPREPEND, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RPREPENDQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RDELETE, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RDELETEQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RINCR, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RINCRQ, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RDECR, _S, _RC, _E) -> exit(todo);
-cmd_binary(?RDECRQ, _S, _RC, _E) -> exit(todo);
 
 cmd_binary(Cmd, _S, _RC, _E) -> exit({unimplemented, Cmd}).
 
