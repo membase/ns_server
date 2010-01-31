3e77a77e3529db6eef59257f0c342aeb85a042b7
Steve Yen <steve.yen@gmail.com>
2010-01-31 12:05:06 -0800

    isasl.pw now generated correctly

--- src/ns_config_isasl_sync.erl
+++ src/ns_config_isasl_sync.erl
@@ -29,9 +29,11 @@ start_link(Path, AU, AP) when is_list(Path); is_list(AU); is_list(AP) ->
 setup_handler(Path, AU, AP) ->
     gen_event:add_handler(ns_config_events, ?MODULE, [Path, AU, AP]).
 
-init([Path, AU, AP]) ->
+init([Path, AU, AP] = Args) ->
+    error_logger:info_msg("isasl_sync init: ~p~n", [Args]),
     {value, Pools} = ns_config:search(ns_config:get(), pools),
     Buckets = extract_creds(Pools),
+    error_logger:info_msg("isasl_sync init buckets: ~p~n", [Buckets]),
     writeSASLConf(Path, Buckets, AU, AP),
     {ok, #state{buckets=Buckets, path=Path, updates=0,
                 admin_user=AU, admin_pass=AP},
@@ -47,9 +49,9 @@ handle_event({pools, V}, State) ->
             {ok, State, hibernate};
         NewBuckets ->
             writeSASLConf(State#state.path, NewBuckets,
-                         State#state.admin_user, State#state.admin_pass),
+                          State#state.admin_user, State#state.admin_pass),
             {ok, State#state{buckets=NewBuckets,
-                            updates=State#state.updates + 1}, hibernate}
+                             updates=State#state.updates + 1}, hibernate}
     end;
 handle_event(_, State) ->
     {ok, State, hibernate}.
@@ -67,31 +69,38 @@ handle_info(Info, State) ->
 %
 
 -spec extract_creds(list()) -> list().
-extract_creds(PropList) ->
+extract_creds(PoolConfigList) ->
     lists:sort(
       dict:to_list(
-        lists:foldl(fun examine_pool/2, dict:new(), PropList))).
+        lists:foldl(fun examine_pool/2, dict:new(), PoolConfigList))).
 
-examine_pool({_Name, Props}, D) ->
+examine_pool({_PoolName, PoolProps}, D) ->
     lists:foldl(fun examine_bucket/2, D,
-                proplists:get_value(buckets, Props, [])).
+                proplists:get_value(buckets, PoolProps, [])).
 
-examine_bucket({Name, Props}, D) ->
-    dict:store(Name, proplists:get_value(auth_plain, Props), D).
+examine_bucket({BucketName, BucketProps}, D) ->
+    dict:store(BucketName, BucketProps, D).
 
 %
 % Update the isasl stuff.
 %
 
-writeSASLConf(Path, NewBuckets, AU, AP) ->
+writeSASLConf(Path, Buckets, AU, AP) ->
     {ok, Pwd} = file:get_cwd(),
     PrivDir = filename:join(Pwd, "priv"),
     FullPath = filename:join(PrivDir, Path),
     error_logger:info_msg("Writing isasl passwd file: ~p~n", [FullPath]),
     {ok, F} = file:open(FullPath, [write]),
     io:format(F, "~s ~s~n", [AU, AP]),
-    lists:foreach(fun({U, undefined}) -> io:format(F, "~s~n", [U]);
-                     ({U, P})         -> io:format(F, "~s ~s~n", [U, P])
-                  end,
-                  NewBuckets),
+    lists:foreach(
+      fun({BucketName, BucketProps}) ->
+              SizePerNode = proplists:get_value(size_per_node, BucketProps, 64),
+              Config = "cache_size=" ++ integer_to_list(SizePerNode),
+              {User, Pass} = case proplists:get_value(auth_plain, BucketProps) of
+                                 undefined       -> {BucketName, ""};
+                                 {BucketName, P} -> {BucketName, P}
+                             end,
+              io:format(F, "~s ~s ~s~n", [User, Pass, Config])
+      end,
+      Buckets),
     file:close(F).
