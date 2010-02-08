ad4291909be78d6fa126a4693ba8d7eda3aeb7f0
Trond Norbye <Trond.Norbye@gmail.com>
2010-02-08 22:58:44 +0100

    Use "$@" instead of $1 $2 $3 etc

--- start_shell.sh
+++ start_shell.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2010, NorthScale, Inc.
 # All rights reserved.
 cd `dirname $0`
-exec erl -pa `find . -type d -name ebin` -boot start_sasl -setcookie nocookie -eval 'application:start(ns_server).' $1 $2 $3 $4 $5 $6 $7 $8 $9
+exec erl -pa `find . -type d -name ebin` -boot start_sasl -setcookie nocookie -eval 'application:start(ns_server).' "$@"
