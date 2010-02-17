ad1ebc2b0dc30b07cbffbf547234f894ef5c93cd
Steve Yen <steve.yen@gmail.com>
2010-02-16 20:45:23 -0800

    start_win entry point with inet_db:set_lookup for windows only.
    bug 320, related to bug 99, shows that the e-port implementation on
    windows to the erl5.7.4/erts-5.7.4/bin/inet_gethost.exe helper program
    looks broken.
    
    the nt-service/shell.bat start scripts on windows will start
    ns_server:start_win(), which is a wrapper for the usual
    ns_server:start() that's the entry point for other platforms.

--- src/ns_server.erl
+++ src/ns_server.erl
@@ -7,8 +7,21 @@
 
 -export([start_link/0, start/2, start/0, stop/1]).
 
-start() -> % Main entry point, mostly for windows/NT-service.
+-export([start_win/0]).
+
+start() ->
     application:start(ns_server).
+
+start_win() ->
+    % Main entry point for windows/NT-service.
+    %
+    % Also, native erl5.7.4/bin/inet_gethost.exe is broken on windows
+    % (XP, 2003 server, 7), so explicitly skip native inet lookup.
+    %
+    % See: http://osdir.com/ml/lang.erlang.general/2004-04/msg00155.html
+    %
+    inet_db:set_lookup([file, dns]),
+    start().
 
 start(_Type, _Args) ->
     start_link().
