db69a048a2802ee3fa521e4c352d2630a0f530e9
Dustin Sallings <dustin@spy.net>
2010-02-18 15:27:24 -0800

    Move pidfile and jointo stuff to misc.erl

--- src/ns_server.erl
+++ src/ns_server.erl
@@ -30,33 +30,6 @@ stop(_State) ->
     ok.
 
 start_link() ->
-    make_pidfile(),
-    ping_jointo(),
+    misc:make_pidfile(),
+    misc:ping_jointo(),
     ns_server_sup:start_link().
-
-% ----------------------------------
-
-make_pidfile() ->
-    case application:get_env(pidfile) of
-        {ok, PidFile} -> make_pidfile(PidFile);
-        X -> X
-    end.
-
-make_pidfile(PidFile) ->
-    Pid = os:getpid(),
-    ok = file:write_file(PidFile, list_to_binary(Pid)),
-    ok.
-
-ping_jointo() ->
-    case application:get_env(jointo) of
-        {ok, NodeName} -> ping_jointo(NodeName);
-        X -> X
-    end.
-
-ping_jointo(NodeName) ->
-    io:format("jointo: attempting to contact ~p~n", [NodeName]),
-    case net_adm:ping(NodeName) of
-        pong -> io:format("jointo: connected to ~p~n", [NodeName]);
-        pang -> {error, io:format("jointo: could not ping ~p~n", [NodeName])}
-    end.
-
