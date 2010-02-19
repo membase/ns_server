c45e24ffb0e211eba89f3a54ef7e0bc25b9012cb
Dustin Sallings <dustin@spy.net>
2010-02-19 11:41:01 -0800

    Added ns_boostrap function for overridding the resolver.

--- src/ns_bootstrap.erl
+++ src/ns_bootstrap.erl
@@ -1,8 +1,12 @@
 -module(ns_bootstrap).
 
--export([start/0]).
+-export([start/0, override_resolver/0]).
 
 start() ->
     ok = application:start(sasl),
     ok = application:start(dist_manager),
     ok = application:start(ns_server).
+
+override_resolver() ->
+    inet_db:set_lookup([file, dns]),
+    start().
