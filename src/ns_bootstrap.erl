9aff0d6eadbc0036a1548d50898b3ed37fd68007
Dustin Sallings <dustin@spy.net>
2010-02-19 20:55:11 -0800

    Added OS-specific network address change hooks.
    The windows hook looks for an application env var restart_script to be
    set and will execute that whenever the network address changes.
    
    unix currently does nothing.

--- src/ns_bootstrap.erl
+++ src/ns_bootstrap.erl
@@ -5,7 +5,9 @@
 start() ->
     ok = application:start(sasl),
     ok = application:start(dist_manager),
-    ok = application:start(ns_server).
+    ok = application:start(ns_server),
+    ok = gen_event:add_handler(ns_network_events,
+                               ns_address_change_handler, []).
 
 override_resolver() ->
     inet_db:set_lookup([file, dns]),
