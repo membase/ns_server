563269e9bf7ea7f8a4a4728365a20e6685fc0f03
Matt Ingenthron <ingenthr@cep.net>
2010-02-25 10:21:36 -0800

    Changing the mkdir argument, perhaps it should be -p?

--- start_shell.sh
+++ start_shell.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2010, NorthScale, Inc.
 # All rights reserved.
 cd `dirname $0`
-mkdir -f logs
+mkdir -p logs
 exec erl -pa `find . -type d -name ebin` \
     -setcookie nocookie \
     -run ns_bootstrap \
