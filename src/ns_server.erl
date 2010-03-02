c562897b9468d780ad3168bf1ea8e88f7d1e4653
Sean Lynch <seanl@literati.org>
2010-03-01 18:27:19 -0800

    Generate a ns_log entry when the server starts

--- src/ns_server.erl
+++ src/ns_server.erl
@@ -5,10 +5,15 @@
 
 -behavior(application).
 
--export([start/2, stop/1]).
+-export([start/2, stop/1, ns_log_cat/1]).
 
 start(_Type, _Args) ->
-    ns_server_sup:start_link().
+    Result = ns_server_sup:start_link(),
+    ns_log:log(?MODULE, 1, "NorthScale Memcached Server has started on port ~p.",
+               [ns_config:search_prop(ns_config:get(), rest, port, 8080)]),
+    Result.
 
 stop(_State) ->
     ok.
+
+ns_log_cat(_) -> info.
