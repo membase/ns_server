cdbb17bce1ef1a806ab8ce32f4c6b11a14e79acf
Sean Lynch <seanl@literati.org>
2010-03-12 16:52:02 -0800

    Bug 843: deduplicate ns_log messages
    Currently we garbage collect the duplicates once a minute, generating a
    log message with the number of repetitions for anything repeated more
    than once. Nothing is logged more than once per five minute interval.
    These are right at the top of the file so they can be changed easily,
    though it's important to note that the GC interval is in milliseconds
    while the time over which we suppress duplicates is in microseconds.
    
    Signed-off-by: Dustin Sallings <dustin@spy.net>

--- src/ns_log.erl
+++ src/ns_log.erl
@@ -8,6 +8,8 @@
 
 % Number of recent log entries.
 -define(RB_SIZE, 50).
+-define(DUP_TIME, 300000000). % 300 secs in microsecs
+-define(GC_TIME, 60000). % 60 secs in millisecs
 
 -behaviour(gen_server).
 -behavior(ns_log_categorizing).
@@ -27,7 +29,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--record(state, {recent}).
+-record(state, {recent, dedup}).
 
 start_link() ->
     Result = gen_server:start_link({global, ?MODULE}, ?MODULE, [], []),
@@ -35,7 +37,8 @@ start_link() ->
     Result.
 
 init([]) ->
-    {ok, #state{recent=emptyRecent()}}.
+    timer:send_interval(?GC_TIME, garbage_collect),
+    {ok, #state{recent=emptyRecent(), dedup=dict:new()}}.
 
 emptyRecent() ->
     ringbuffer:new(?RB_SIZE).
@@ -46,25 +49,31 @@ handle_call(recent, _From, State) ->
     {reply, Reply, State}.
 
 % Inbound logging request.
-handle_cast({log, Module, Code, Fmt, Args}, State) ->
-    Category = categorize(Module, Code),
-    NR = ringbuffer:add(#log_entry{module=Module, code=Code, msg=Fmt, args=Args,
-                                   cat=Category,
-                                   tstamp=erlang:now()},
-                        State#state.recent),
-    try gen_event:notify(ns_log_events, {ns_log, Category, Module, Code,
-                         Fmt, Args})
-    catch _:Reason ->
-        error_logger:info_msg("ns_log: message: ~p, ~p, ~p, ~p~n",
-                              [Module, Code, Fmt, Args])
-    end,
-    {noreply, State#state{recent=NR}};
+handle_cast({log, Module, Code, Fmt, Args}, State = #state{dedup=Dedup}) ->
+    Now = erlang:now(),
+    Key = {Module, Code, Fmt, Args},
+    case dict:find(Key, Dedup) of
+    {ok, {Count, FirstSeen, LastSeen}} ->
+        error_logger:info_msg("ns_log: suppressing duplicate log ~p:~p(~p) because it's been "
+                           "seen ~p times in the past ~p secs (last seen ~p secs ago~n",
+                           [Module, Code, lists:flatten(io_lib:format(Fmt, Args)),
+                            Count+1, timer:now_diff(Now, FirstSeen) / 1000000,
+                            timer:now_diff(Now, LastSeen) / 1000000]),
+        Dedup2 = dict:store(Key, {Count+1, FirstSeen, Now}, Dedup),
+        {noreply, State#state{dedup=Dedup2}};
+    error ->
+        State2 = do_log(Module, Code, Fmt, Args, State),
+        Dedup2 = dict:store(Key, {0, Now, Now}, Dedup),
+        {noreply, State2#state{dedup=Dedup2}}
+    end;
 handle_cast(clear, _State) ->
     error_logger:info_msg("Clearing log.~n", []),
-    {noreply,  #state{recent=emptyRecent()}}.
+    {noreply,  #state{recent=emptyRecent(), dedup=dict:new()}}.
 % Not handling any other state.
 
 % Nothing special.
+handle_info(garbage_collect, State) ->
+    {noreply, gc(State)};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -77,6 +86,40 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+do_log(Module, Code, Fmt, Args, State) ->
+    Category = categorize(Module, Code),
+    NR = ringbuffer:add(#log_entry{module=Module, code=Code, msg=Fmt, args=Args,
+                                   cat=Category,
+                                   tstamp=erlang:now()},
+                        State#state.recent),
+    try gen_event:notify(ns_log_events, {ns_log, Category, Module, Code,
+                         Fmt, Args})
+    catch _:Reason ->
+        error_logger:error_msg("ns_log: unable to notify listeners "
+                               "because of ~p~n", [Reason])
+    end,
+    State#state{recent=NR}.
+
+gc(State = #state{dedup=Dupes}) ->
+    {DupesList, State2} = gc(erlang:now(), dict:to_list(Dupes),
+                                           [], State),
+    State2#state{dedup=dict:from_list(DupesList)}.
+
+gc(_Now, [], DupesList, State) -> {DupesList, State};
+gc(Now, [{Key, Value} | Rest], DupesList, State) ->
+     {Count, FirstSeen, _LastSeen} = Value,
+     case timer:now_diff(Now, FirstSeen) >= ?DUP_TIME of
+     true ->
+         {Module, Code, Fmt, Args} = Key,
+         State2 = case Count of
+         0 -> State;
+         _ -> do_log(Module, Code, Fmt ++ " (repeated ~p times)",
+                     Args ++ [Count], State)
+         end,
+         gc(Now, Rest, DupesList, State2);
+     false -> gc(Now, Rest, [{Key, Value} | DupesList], State)
+     end.
 
 %% API
 
@@ -104,8 +147,8 @@ log(Module, Code, Msg) ->
 
 -spec log(atom(), integer(), string(), list()) -> ok.
 log(Module, Code, Fmt, Args) ->
-    error_logger:info_msg("Logging ~p:~p(~p, ~p)~n",
-                          [Module, Code, Fmt, Args]),
+    error_logger:info_msg("ns_log: logging ~p:~p(~p)~n",
+                          [Module, Code, lists:flatten(io_lib:format(Fmt, Args))]),
     gen_server:cast({global, ?MODULE}, {log, Module, Code, Fmt, Args}).
 
 -spec recent() -> list(#log_entry{}).
