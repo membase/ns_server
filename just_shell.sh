07ee9126d55d2f26da9537e8d12fef380e205b6a
Aliaksey Kandratsenka <alk@tut.by>
2010-06-20 13:35:11 +0300

    added just_shell.sh
    Change-Id: Ia8d82f2803d4f7b7a7fee4be17b53ec21be84889
    Reviewed-on: http://review.membase.org/4544
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Sean Lynch <seanlynch5@gmail.com>

---
+++ just_shell.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Copyright (c) 2010, NorthScale, Inc.
+# All rights reserved.
+cd `dirname $0`
+mkdir logs > /dev/null 2>&1
+
+# Initialize distributed erlang on the system (i.e. epmd)
+if [ -z "$DONT_START_EPMD" ]; then
+  erl -noshell -setcookie nocookie -sname init -run init stop 2>&1 > /dev/null
+fi
+
+exec erl -pa `find . -type d -name ebin` \
+    -setcookie nocookie \
+    -ns_server error_logger_mf_dir '"logs"' \
+    -ns_server error_logger_mf_maxbytes 10485760 \
+    -ns_server error_logger_mf_maxfiles 10 \
+    -- "$@"
