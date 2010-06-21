c8edd1562a85e5ee3c1ea23e137dbc23de4393b5
Aliaksey Kandratsenka <alk@tut.by>
2010-06-21 12:25:18 +0300

    workaround fake auth in only-web mode
    Change-Id: I79a03862701b414b946f53cd27708023604fc4ac
    Reviewed-on: http://review.northscale.com:8080/791
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

--- deps/menelaus/priv/js/hooks.js
+++ deps/menelaus/priv/js/hooks.js
@@ -778,6 +778,8 @@ var __hookParams = {};
   if (params['rebalanceStatus']) {
     MockedRequest.globalData.setRebalanceStatus(params['rebalanceStatus']);
   }
+
+  $.cookie('fakeauth', 1);
 })();
 
 //window.onerror = originalOnError;
