b57fe273f154bbdc2b086c5fd28d1bd207dc31ef
Sean Lynch <seanl@literati.org>
2010-07-09 16:41:25 -0700

    Add common include with logging macros.
    Change-Id: Ia8fd67d15365571f61ee1af82b665f2c7c053ede
    Reviewed-on: http://review.northscale.com/1072
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

---
+++ include/ns_common.hrl
@@ -0,0 +1,28 @@
+%% @author Northscale <info@northscale.com>
+%% @copyright 2010 NorthScale, Inc.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%      http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% @doc Macros used all over the place.
+%%
+
+-define(LOG(Fun, Format, Args),
+        error_logger:Fun("~p:~p:~p: " Format "~n",
+                         [node(), ?MODULE, ?LINE] ++ Args)).
+
+-define(log_info(Format, Args), ?LOG(info_msg, Format, Args)).
+-define(log_info(Msg), ?log_info(Msg, [])).
+-define(log_warning(Format, Args), ?LOG(warning_msg, Format, Args)).
+-define(log_warning(Msg), ?log_warning(Msg, [])).
+-define(log_error(Format, Args), ?LOG(error_msg, Format, Args)).
+-define(log_error(Msg), ?log_error(Msg, [])).
