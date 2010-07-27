d0934fcab4657106d30fb7d976414ab8e4850fbc
Sean Lynch <seanl@literati.org>
2010-07-27 15:14:00 -0700

    Log when we kill vbuckets.
    Change-Id: Idc78aa40309f165a211d62ebb650bf7506cb2854
    Reviewed-on: http://review.northscale.com/1364
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/ns_vbm_sup.erl
+++ src/ns_vbm_sup.erl
@@ -57,6 +57,8 @@ kill_vbuckets(Node, Bucket, VBuckets) ->
         [] ->
             ok;
         RemainingVBuckets ->
+            ?log_info("kill_vbuckets: remaining vbuckets: ~p",
+                      [RemainingVBuckets]),
             lists:foreach(fun ({V, dead}) ->
                                   ns_memcached:delete_vbucket(Node, Bucket, V);
                               ({V, _}) ->
