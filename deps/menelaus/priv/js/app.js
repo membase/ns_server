07f070f956a4543cb3b0e212f8f485007f183eac
Aliaksey Kandratsenka <alk@tut.by>
2010-08-05 20:36:02 -0700

    wired RAM gauge on overview screen
    Change-Id: I36acb8903ef3149f03de49e5cc9f4b8654c4a7be
    Reviewed-on: http://review.northscale.com/1649
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- deps/menelaus/priv/js/app.js
+++ deps/menelaus/priv/js/app.js
@@ -53,11 +53,9 @@ var LogoutTimer = {
 };
 
 var OverviewSection = {
-  init: function () {
-    _.defer(function () {
-      BucketsSection.cells.detailedBuckets.subscribeValue(function (buckets) {
-        $('#overview .buckets-number').text(buckets ? buckets.length : '??');
-      });
+  initLater: function () {
+    BucketsSection.cells.detailedBuckets.subscribeValue(function (buckets) {
+      $('#overview .buckets-number').text(buckets ? buckets.length : '??');
     });
 
     DAO.cells.serversCell.subscribeValue(function (servers) {
@@ -85,6 +83,41 @@ var OverviewSection = {
       updateCount('.pending-add-count', pendingAdd.length);
       updateCount('.pending-eject-count', pendingEject.length);
     });
+
+    var spinner;
+    BucketsSection.cells.detailedBuckets.subscribeValue(function (buckets) {
+      var poolDetails = DAO.cells.currentPoolDetailsCell.value;
+      if (!poolDetails || !buckets) {
+        if (!spinner)
+          spinner = overlayWithSpinner('#overview_clusters_block');
+        return;
+      }
+      if (spinner) {
+        spinner.remove();
+        spinner = null;
+      }
+
+      var item = $('#overview_clusters_block .ram-item');
+
+      var usedQuota = _.reduce(buckets, 0, function (acc, info) {
+        return acc + info.basicStats.memUsed;
+      });
+      var bucketsQuota = _.reduce(buckets, 0, function (acc, info) {
+        return acc + info.quota.ram;
+      });
+      var quotaTotal = poolDetails.storageTotals.ram.quotaTotal;
+      item.find('.cluster-total').text(ViewHelpers.formatQuantity(quotaTotal, null, null, ' '));
+      item.find('.used-quota').text(ViewHelpers.formatQuantity(usedQuota, null, null, ' '));
+      item.find('.buckets-quota').text(ViewHelpers.formatQuantity(bucketsQuota, null, null, ' '));
+      item.find('.unused-quota').text(ViewHelpers.formatQuantity(bucketsQuota - usedQuota, null, null, ' '));
+      item.find('.quota-free').text(ViewHelpers.formatQuantity(quotaTotal - bucketsQuota, null, null, ' '));
+
+      item.find('.used').css('width', calculatePercent(usedQuota, quotaTotal) + '%');
+      item.find('.free').css('width', calculatePercent(bucketsQuota - usedQuota, quotaTotal) + '%');
+    });
+  },
+  init: function () {
+    _.defer($m(this, 'initLater'));
   },
   onEnter: function () {
   }
