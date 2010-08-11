f4f5adb5b350e7d59ce3ab9b9a422801b9fd8dae
Aliaksey Kandratsenka <alk@tut.by>
2010-08-11 01:03:51 -0700

    update ns_bucket_sup childs according to fresh buckets list
    This makes sure that we always have stats_{collector,archiver} per
    bucket.
    
    Change-Id: I4336632775dc0c4e3bf35f0fc6d06b105b92e49a
    Reviewed-on: http://review.northscale.com/1825
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- src/ns_bucket_sup.erl
+++ src/ns_bucket_sup.erl
@@ -19,15 +19,27 @@
 
 -behaviour(supervisor).
 
+-include("ns_common.hrl").
+
 -export([start_link/0]).
 
--export([init/1]).
+-export([init/1, update_childs/0]).
 
 
 %% API
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    RV = supervisor:start_link({local, ?MODULE}, ?MODULE, []),
+    ns_pubsub:subscribe(ns_config_events,
+                        fun (Event, State) ->
+                                case Event of
+                                    {buckets, _} ->
+                                        update_childs();
+                                    _ -> ok
+                                end,
+                                State
+                        end, undefined),
+    RV.
 
 %% supervisor callbacks
 
@@ -35,17 +47,37 @@ init([]) ->
     {ok, {{one_for_one, 3, 10},
           child_specs()}}.
 
-
 %% Internal functions
 child_specs() ->
     Configs = ns_bucket:get_buckets(),
     ChildSpecs = child_specs(Configs),
-    error_logger:info_msg("~p:child_specs(): ChildSpecs = ~p~n",
-                          [?MODULE, ChildSpecs]),
+    ?log_info("~p:child_specs(): ChildSpecs = ~p~n",
+              [?MODULE, ChildSpecs]),
     ChildSpecs.
 
 child_specs(Configs) ->
     lists:append([child_spec(B) || {B, _} <- Configs]).
+
+update_childs() ->
+    NewSpecs = child_specs(),
+    NewIds = [element(1, X) || X <- NewSpecs],
+    OldSpecs = supervisor:which_children(?MODULE),
+    RunningIds = [element(1, X) || X <- OldSpecs],
+    ToStart = NewIds -- RunningIds,
+    ToStop = RunningIds -- NewIds,
+    lists:foreach(fun (StartId) ->
+                          Tuple = lists:keyfind(StartId, 1, NewSpecs),
+                          true = is_tuple(Tuple),
+                          ?log_info("Starting new ns_bucket_sup child: ~p~n", [Tuple]),
+                          supervisor:start_child(?MODULE, Tuple)
+                  end, ToStart),
+    lists:foreach(fun (StopId) ->
+                          Tuple = lists:keyfind(StopId, 1, OldSpecs),
+                          true = is_tuple(Tuple),
+                          ?log_info("Stopping ns_bucket_sup child for dead bucket: ~p~n", [Tuple]),
+                          supervisor:terminate_child(?MODULE, StopId),
+                          supervisor:delete_child(?MODULE, StopId)
+                  end, ToStop).
 
 child_spec(Bucket) ->
     [{{stats_collector, Bucket}, {stats_collector, start_link, [Bucket]},
