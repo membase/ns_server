0bf51c4aa7c2cab3df48ff8be239244e20361455
Aliaksey Kandratsenka <alk@tut.by>
2010-08-12 09:08:30 -0700

    moved overview into own file
    Change-Id: I86be1c44a9e014c7acec008b5325f81bae070410
    Reviewed-on: http://review.northscale.com/1869
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- deps/menelaus/priv/js/app.js
+++ deps/menelaus/priv/js/app.js
@@ -19,6 +19,7 @@
 //= require <settings.js>
 //= require <manage-buckets.js>
 //= require <monitor-buckets.js>
+//= require <overview.js>
 
 // TODO: doesn't work due to apparent bug in jqModal. Consider switching to another modal windows implementation
 // $(function () {
@@ -52,77 +53,6 @@ var LogoutTimer = {
   }
 };
 
-var OverviewSection = {
-  initLater: function () {
-    BucketsSection.cells.detailedBuckets.subscribeValue(function (buckets) {
-      $('#overview .buckets-number').text(buckets ? ViewHelpers.count(buckets.length, 'bucket') : '??');
-    });
-
-    DAO.cells.serversCell.subscribeValue(function (servers) {
-      $('.active-servers-count').text(servers ? servers.active.length : '??');
-
-      var block = $('#overview_servers_block');
-      if (!servers) {
-        block.find('.alert_num').hide();
-        return;
-      }
-      var pending = servers.pending;
-      var failedOver = _.select(servers.allNodes, function (node) {
-        return node.clusterMembership == 'inactiveFailed';
-      });
-      var down = _.select(servers.allNodes, function (node) {
-        return node.status != 'healthy';
-      });
-
-      function updateCount(selector, count) {
-        var span = block.find(selector).text(count);
-        span.parents('.alert_num')[count ? 'show' : 'hide']();
-        span.parents('.alert_num').parents('li')[count ? 'removeClass' : 'addClass']('is-zero');
-      }
-
-      updateCount('.failed-over-count', failedOver.length);
-      updateCount('.down-count', down.length);
-      updateCount('.pending-count', pending.length);
-    });
-
-    var spinner;
-    BucketsSection.cells.detailedBuckets.subscribeValue(function (buckets) {
-      var poolDetails = DAO.cells.currentPoolDetailsCell.value;
-      if (!poolDetails || !buckets) {
-        if (!spinner)
-          spinner = overlayWithSpinner('#overview_clusters_block');
-        return;
-      }
-      if (spinner) {
-        spinner.remove();
-        spinner = null;
-      }
-
-      var item = $('#overview_clusters_block .ram-item');
-
-      var usedQuota = _.reduce(buckets, 0, function (acc, info) {
-        return acc + info.basicStats.memUsed;
-      });
-      var bucketsQuota = _.reduce(buckets, 0, function (acc, info) {
-        return acc + info.quota.ram;
-      });
-      var quotaTotal = poolDetails.storageTotals.ram.quotaTotal;
-      item.find('.cluster-total').text(ViewHelpers.formatQuantity(quotaTotal, null, null, ' '));
-      item.find('.used-quota').text(ViewHelpers.formatQuantity(usedQuota, null, null, ' '));
-      item.find('.buckets-quota').text(ViewHelpers.formatQuantity(bucketsQuota, null, null, ' '));
-      item.find('.unused-quota').text(ViewHelpers.formatQuantity(bucketsQuota - usedQuota, null, null, ' '));
-      item.find('.quota-free').text(ViewHelpers.formatQuantity(quotaTotal - bucketsQuota, null, null, ' '));
-
-      item.find('.used').css('width', calculatePercent(usedQuota, quotaTotal) + '%');
-      item.find('.free').css('width', calculatePercent(bucketsQuota - usedQuota, quotaTotal) + '%');
-    });
-  },
-  init: function () {
-    _.defer($m(this, 'initLater'));
-  },
-  onEnter: function () {
-  }
-};
 
 ;(function () {
   var weekDays = "Sun Mon Tue Wed Thu Fri Sat".split(' ');
