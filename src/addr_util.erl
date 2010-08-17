029ee19f52dd6135c2a3bbdabe269a7c0354a81b
Sean Lynch <seanl@literati.org>
2010-08-16 19:04:16 -0700

    Bug 2032: Re-add addr_util
    Change-Id: Ib74dee7cd3f30f0643c2f43695d3d2414b694238
    Reviewed-on: http://review.northscale.com/1966
    Reviewed-by: Sean Lynch <seanl@literati.org>
    Tested-by: Sean Lynch <seanl@literati.org>

---
+++ src/addr_util.erl
@@ -0,0 +1,63 @@
+%% @author Northscale <info@northscale.com>
+%% @copyright 2010 NorthScale, Inc.
+%% All rights reserved.
+
+-module(addr_util).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([get_my_address/0, get_my_address/1]).
+
+%% Find the best IP address we can find for the current host.
+get_my_address() ->
+    get_my_address("255.255.255.255").
+
+get_my_address(CurrentAddress) ->
+    determine_address(CurrentAddress, list_all_addresses()).
+
+get_my_address_test() ->
+    %% Verify the result of this looks like an IP address.
+    4 = length(string:tokens(get_my_address(), ".")).
+
+determine_address(CurrentAddress, AddrList) ->
+    case lists:member(CurrentAddress, AddrList) of
+        true -> CurrentAddress;
+        _ -> extract_addr(AddrList)
+    end.
+
+determine_address_test() ->
+    "4.1.1.1" = determine_address("255.255.255.255", ["4.1.1.1", "1.2.3.4"]),
+    "1.2.3.4" = determine_address("1.2.3.4", ["4.1.1.1", "1.2.3.4"]),
+    "127.0.0.1" = determine_address("1.2.3.4", []).
+
+list_all_addresses() ->
+    {ok, AddrInfo} = inet:getif(),
+    lists:sort(
+      lists:map(fun({A,_,_}) -> addr_to_s(A) end, AddrInfo)
+      -- ["127.0.0.1"]).
+
+list_all_addresses_test() ->
+    %% I can't test too much here since this isn't functional, but
+    %% I'll verify we go through the code and return something.
+    true = is_list(list_all_addresses()).
+
+%% [X,...] -> X
+extract_addr([H|_Tl]) ->
+    H;
+%% [] -> "127.0.0.1"
+extract_addr([]) ->
+    "127.0.0.1".
+
+extract_addr_test() ->
+    "4.1.1.1" = extract_addr(["4.1.1.1", "1.2.3.4"]),
+    "4.1.1.1" = extract_addr(["4.1.1.1"]),
+    "127.0.0.1" = extract_addr([]).
+
+%% {1,2,3,4} -> "1.2.3.4"
+addr_to_s(T) ->
+    string:join(lists:map(fun erlang:integer_to_list/1,
+                          tuple_to_list(T)),
+                ".").
+
+addr_to_s_test() ->
+    "1.2.3.4" = addr_to_s({1,2,3,4}).
