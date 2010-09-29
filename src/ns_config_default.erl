f3564ca8879f13cd1649ab894fdfd406c30e8030
Aliaksey Kandratsenka <alk@tut.by>
2010-09-29 17:41:46 +0300

    setup initial server quota as specified in bug 2512
    Change-Id: I45ef522cf4ada681799b5374a6112d06daa3f079
    Reviewed-on: http://review.northscale.com/2857
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- src/ns_config_default.erl
+++ src/ns_config_default.erl
@@ -107,7 +107,8 @@ default() ->
                 _ -> RawDbDir
             end,
     InitQuota = case memsup:get_memory_data() of
-                    {Total, _, _} -> (Total * 4) div (1048576 * 5);
+                    {Total, _, _} -> lists:max([(Total * 4) div (1048576 * 5),
+                                                (Total div 1048576) - 512]);
                     _ -> undefined
                 end,
     [{directory, default_path("config")},
