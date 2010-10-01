5d9b9f35896d75893a4e5707248d3495e26ad0d7
Sean Lynch <seanl@literati.org>
2010-10-01 15:11:55 -0700

    Add diagnostic information to the heartbeat.
    Change-Id: I5fa4d55f87e8271199008e658f247ad4dc515a1b
    Reviewed-on: http://review.northscale.com/2961
    Tested-by: Steve Yen <steve.yen@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- src/ns_heart.erl
+++ src/ns_heart.erl
@@ -16,6 +16,9 @@
 -module(ns_heart).
 
 -behaviour(gen_server).
+
+-define(EXPENSIVE_CHECK_INTERVAL, 60000). % In ms
+
 -export([start_link/0, status_all/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
@@ -27,10 +30,11 @@ start_link() ->
 
 init([]) ->
     timer:send_interval(1000, beat),
-    {ok, empty_state}.
+    timer:send_interval(?EXPENSIVE_CHECK_INTERVAL, do_expensive_checks),
+    {ok, expensive_checks()}.
 
 handle_call(status, _From, State) ->
-    {reply, current_status(), State};
+    {reply, current_status(State), State};
 handle_call(Request, _From, State) ->
     {reply, {unhandled, ?MODULE, Request}, State}.
 
@@ -38,8 +42,10 @@ handle_cast(_Msg, State) -> {noreply, State}.
 
 handle_info(beat, State) ->
     misc:flush(beat),
-    ns_doctor:heartbeat(current_status()),
-    {noreply, State}.
+    ns_doctor:heartbeat(current_status(State)),
+    {noreply, State};
+handle_info(do_expensive_checks, _State) ->
+    {noreply, expensive_checks()}.
 
 terminate(_Reason, _State) -> ok.
 
@@ -50,7 +56,19 @@ status_all() ->
     {Replies, _} = gen_server:multi_call([node() | nodes()], ?MODULE, status, 5000),
     Replies.
 
+stats() ->
+    Stats = [wall_clock, context_switches, garbage_collection, io, reductions,
+             run_queue, runtime],
+    [{Stat, statistics(Stat)} || Stat <- Stats].
+
 %% Internal fuctions
-current_status() ->
-    [{active_buckets, ns_memcached:active_buckets()} |
-     element(2, ns_info:basic_info())].
+current_status(Expensive) ->
+    [{active_buckets, ns_memcached:active_buckets()},
+     {memory, erlang:memory()},
+     {memory_data, memsup:get_memory_data()},
+     {disk_data, disksup:get_disk_data()}|
+     Expensive].
+
+expensive_checks() ->
+    [{system_memory_data, memsup:get_system_memory_data()},
+     {statistics, stats()}].
