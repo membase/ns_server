3fc9462b107a0886942bb8ff742202cd225887cd
Sean Lynch <seanl@literati.org>
2010-10-03 13:04:16 -0700

    Bug 2647: Run ensure_table in calling process.
    We were running it in the ns_mnesia gen_server, but it can take an
    arbitrary period of time. It's atomic anyway, so there really wasn't
    any reason to do it in the gen_server.
    
    Change-Id: I0e57a373ba60d81c6200fa78619399096540702e
    Reviewed-on: http://review.northscale.com/3016
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

--- src/ns_mnesia.erl
+++ src/ns_mnesia.erl
@@ -57,7 +57,24 @@ delete_schema() ->
 %% @doc Make sure table exists and has a copy on this node, creating it or
 %% adding a copy if it does not.
 ensure_table(TableName, Opts) ->
-    gen_server:call(?MODULE, {ensure_table, TableName, Opts}).
+    try mnesia:table_info(TableName, disc_copies) of
+        Nodes when is_list(Nodes) ->
+            case lists:member(node(), Nodes) of
+                true ->
+                    ok;
+                false ->
+                    ?log_info("Creating local copy of ~p",
+                              [TableName]),
+                    {atomic, ok} = mnesia:add_table_copy(
+                                     TableName, node(), disc_copies)
+            end
+    catch exit:{aborted, {no_exists, _, _}} ->
+            {atomic, ok} =
+                mnesia:create_table(
+                  TableName,
+                  Opts ++ [{disc_copies, [node()]}]),
+            ?log_info("Created table ~p", [TableName])
+    end.
 
 
 %% @doc Back up the database in preparation for a node rename.
@@ -113,27 +130,6 @@ handle_call({add_node, Node}, _From, State) ->
             ?log_warning("Node ~p was already in cluster.", [Node])
     end,
     ?log_info("Mnesia config:~n~p", [mnesia:system_info(all)]),
-    {reply, ok, State};
-
-handle_call({ensure_table, TableName, Opts}, _From, State) ->
-    try mnesia:table_info(TableName, disc_copies) of
-        Nodes when is_list(Nodes) ->
-            case lists:member(node(), Nodes) of
-                true ->
-                    ok;
-                false ->
-                    ?log_info("Creating local copy of ~p",
-                              [TableName]),
-                    {atomic, ok} = mnesia:add_table_copy(
-                                     TableName, node(), disc_copies)
-            end
-    catch exit:{aborted, {no_exists, _, _}} ->
-            {atomic, ok} =
-                mnesia:create_table(
-                  TableName,
-                  Opts ++ [{disc_copies, [node()]}]),
-            ?log_info("Created table ~p", [TableName])
-    end,
     {reply, ok, State};
 
 handle_call(prepare_rename, _From, State) ->
