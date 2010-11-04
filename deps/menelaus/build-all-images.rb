6aeb4371e6328617539cfab8b94254d8982d0528
Aliaksey Kandratsenka <alk@tut.by>
2010-11-04 08:29:28 +0200

    extracted building of all-images.js into own script
    This should unbreak windows build
    
    Change-Id: I6de76840d472ee8c79287a419120c77da40bfac2
    Reviewed-on: http://review.membase.org/3550
    Tested-by: Steve Yen <steve.yen@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

---
+++ deps/menelaus/build-all-images.rb
@@ -0,0 +1,7 @@
+#!/bin/env ruby
+
+Dir.chdir(File.join(File.dirname(__FILE__), "priv/public"))
+
+print "var AllImages = ["
+print(Dir["images/**/*"].select {|p| File.file? p}.map {|p| p.inspect}.join(", "))
+puts "]"
