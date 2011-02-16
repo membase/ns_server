3b4d0321143790984566ebbff602f5eab2d07bf3
Aaron Miller <apage43@ninjawhale.com>
2011-02-16 01:32:31 -0500

    mb_grid stub fix crash on 0 or 1 nodes, add accumulator.
    Change-Id: Ie3e8422ff9894e3c1bcad53f75c77ebb091b4ba6
    Reviewed-on: http://review.membase.org/4593
    Reviewed-by: Benjamin Young <benjamin@couchbase.com>
    Tested-by: Dale Harvey <daleharvey@arandomurl.com>
    Reviewed-by: Dale Harvey <daleharvey@arandomurl.com>

--- src/mb_grid.erl
+++ src/mb_grid.erl
@@ -1,4 +1,3 @@
-%% 
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
@@ -18,18 +17,24 @@
 
 -export([aggregate_call/4,
          aggregate_call/5,
+         aggregate_call/6,
          abcast/3]).
 
 %% @doc Will broadcast request to named process on nodes
 abcast(Nodes, Name, Request) ->
     gen_server:abcast(Nodes, Name, Request).
 
-%% @doc Will make synchronous call to nodes and aggregate. Aggregate function must be a
-%% commutative function of arity 2.
+%% @doc Will make synchronous call to nodes and aggregate.
+aggregate_call(Nodes, Module, Request, AFun, Timeout, Accumulator) ->
+    {Replies, BadNodes} = gen_server:multi_call(Nodes, Module, Request, Timeout),
+    {lists:foldl(AFun, Accumulator, [Reply || {_Node, Reply} <- Replies]), BadNodes}.
+
+%% @doc Will make synchronous call to nodes and aggregate.
 aggregate_call(Nodes, Module, Request, AFun, Timeout) ->
     {Replies, BadNodes} = gen_server:multi_call(Nodes, Module, Request, Timeout),
-    [First | Rest] = [Reply || {_Node, Reply} <- Replies],
-    {lists:foldl(AFun, First, Rest), BadNodes}.
+    if  length(Replies) < 2 -> {Replies, BadNodes};
+        true -> [First | Rest] = [Reply || {_Node, Reply} <- Replies],
+             {lists:foldl(AFun, First, Rest), BadNodes} end.
 
 %% @doc Same as {@link aggregate_call/5} with timeout of infinity.
 aggregate_call(Nodes, Module, Request, AFun) ->
