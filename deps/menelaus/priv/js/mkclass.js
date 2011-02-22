49dceb3d2990d2dac0d1331fc19b3cea5ad22b34
Benjamin Young <byoung@bigbluehat.com>
2011-02-22 16:01:23 -0500

    removed eval from mkClass and delinted
    Change-Id: Ic3061cef12363e052af916b4c20d19686eaf6c3f
    Reviewed-on: http://review.membase.org/4683
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- deps/menelaus/priv/js/mkclass.js
+++ deps/menelaus/priv/js/mkclass.js
@@ -1,14 +1,10 @@
-function __topEval() {
-  return eval("(function () {return (" + String(arguments[0]) + ");})();");
-}
-
 function mkMethodWrapper (method, superClass, methodName) {
   return function () {
     var args = $.makeArray(arguments);
     var _super = $m(this, methodName, superClass);
     args.unshift(_super);
     return method.apply(this, args);
-  }
+  };
 }
 
 function mkClass(methods) {
@@ -16,13 +12,13 @@ function mkClass(methods) {
     var superclass = methods;
     var origMethods = arguments[1];
 
-    var meta = new Function();
+    var meta = function() {};
     meta.prototype = superclass.prototype;
 
-    methods = _.extend(new meta, origMethods);
+    methods = _.extend(new meta(), origMethods);
 
     _.each(origMethods, function (method, methodName) {
-      if (_.isFunction(method) && functionArgumentNames(method)[0] == '$super') {
+      if (_.isFunction(method) && functionArgumentNames(method)[0] === '$super') {
         methods[methodName] = mkMethodWrapper(method, superclass, methodName);
       }
     });
@@ -30,10 +26,11 @@ function mkClass(methods) {
     methods = _.extend({}, methods);
   }
 
-  var constructor = __topEval(function () {
-    if (this.initialize)
-      return this.initialize.apply(this, arguments);
-  });
+  var constructor = function () {
+      if (this.initialize) {
+        return this.initialize.apply(this, arguments);
+      }
+    };
 
   methods.constructor = constructor;
   constructor.prototype = methods;
