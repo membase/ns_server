e83ca5d666c22cc83434d1a7d393d6952b59bac0
Benjamin Young <byoung@bigbluehat.com>
2011-03-21 15:07:45 -0400

    all-images.js added back to index.html
    Change-Id: I7d85c63abb0d16a69d33e98726fc4b5bd1da28e6
    Reviewed-on: http://review.membase.org/5014
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- deps/menelaus/build-all-images.rb
+++ deps/menelaus/build-all-images.rb
@@ -4,4 +4,4 @@ Dir.chdir(File.join(File.dirname(__FILE__), "priv/public"))
 
 print "var AllImages = ["
 print(Dir["images/**/*"].select {|p| File.file? p}.map {|p| p.inspect}.join(", "))
-puts "]"
+puts "];"
