a19fb6ab156c46cfeb5a47aa29868937b1861dd4
Dale Harvey <daleharvey@arandomurl.com>
2011-04-13 15:30:22 -0700

    added alert for write failure
    Change-Id: I2457a0c3be4fe05b2ee7308dce36ab1099ae0c21
    Reviewed-on: http://review.membase.org/5361
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/menelaus_web_alerts_srv.erl
+++ src/menelaus_web_alerts_srv.erl
@@ -22,10 +22,16 @@
 %% Amount of time between sending users the same alert (s)
 -define(ALERT_TIMEOUT, 60 * 2).
 
--define(IP_ERR, "Cannot listen on hostname: ~p").
--define(OOM_ERR, "Bucket \"~s\" on node ~s is out of memory").
-
 -export([start_link/0, stop/0, local_alert/2, global_alert/2, fetch_alerts/0]).
+
+
+%% Error constants
+errors(ip) ->
+    "Cannot listen on hostname: ~p";
+errors(ep_oom_errors) ->
+    "Bucket \"~s\" on node ~s is out of memory";
+errors(ep_item_commit_failed) ->
+    "Bucket \"~s\" on node ~s failed to write an item".
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -127,7 +133,7 @@ alert_exists(Key, History, MsgQueue) ->
 %% @doc global checks for any server specific problems locally then
 %% broadcast alerts to clients connected to any particular node
 global_checks() ->
-    [oom, ip].
+    [oom, ip, write_fail].
 
 %% @doc fires off various checks
 check_alerts(Opaque, Hist) ->
@@ -141,37 +147,40 @@ check(ip, Opaque, _History) ->
     {_Name, Host} = misc:node_name_host(node()),
     case can_listen(Host) of
         false ->
-            global_alert({ip, node()}, fmt_to_bin(?IP_ERR, [node()]));
+            global_alert({ip, node()}, fmt_to_bin(errors(ip), [node()]));
         true ->
             ok
     end,
     Opaque;
 
+%% @doc check for write failures inside ep engine
+check(write_fail, Opaque, _History) ->
+    check_stat_increased(ep_item_commit_failed, Opaque);
+
 %% @doc check for any oom errors an any bucket
 check(oom, Opaque, _History) ->
-    New = oom_errors(),
-    case dict:is_key(oom, Opaque) of
+    check_stat_increased(ep_oom_errors, Opaque).
+
+%% @doc Check if the value of any statistic has increased since
+%% last check
+check_stat_increased(Stat, Opaque) ->
+    New = fetch_buckets_stat(Stat),
+    case dict:is_key(Stat, Opaque) of
         false ->
-            dict:store(oom, New, Opaque);
+            dict:store(Stat, New, Opaque);
         true ->
-            Old = dict:fetch(oom, Opaque),
-            case oom_increased(New, Old) of
+            Old = dict:fetch(Stat, Opaque),
+            case stat_increased(New, Old) of
                 [] ->
                     ok;
                 Buckets ->
                     {_Sname, Host} = misc:node_name_host(node()),
-                    Key = {oom, node()},
-                    [global_alert(Key, fmt_to_bin(?OOM_ERR, [Bucket, Host]))
+                    Key = {stat, node()},
+                    [global_alert(Key, fmt_to_bin(errors(Stat), [Bucket, Host]))
                      || Bucket <- Buckets]
             end,
-            dict:store(oom, New, Opaque)
+            dict:store(Stat, New, Opaque)
     end.
-
-
-%% @doc list of buckets thats number of oom errors have increased
--spec oom_increased(dict(), dict()) -> list().
-oom_increased(New, Old) ->
-    [Bucket || {Bucket, Val} <- dict:to_list(New), increased(Bucket, Val, Old)].
 
 
 %% @doc check that I can listen on the current host
@@ -191,24 +200,24 @@ can_listen(Host) ->
     end.
 
 
-%% Format the error message into a binary
-fmt_to_bin(Str, Args) ->
-    list_to_binary(lists:flatten(io_lib:format(Str, Args))).
+%% @doc list of buckets thats measured stats have increased
+-spec stat_increased(dict(), dict()) -> list().
+stat_increased(New, Old) ->
+    [Bucket || {Bucket, Val} <- dict:to_list(New), increased(Bucket, Val, Old)].
 
 
-%% @doc List of oom errors for each bucket
--spec oom_errors() -> dict().
-oom_errors() ->
+%% @doc fetch a list of a stat for all buckets
+fetch_buckets_stat(Stat) ->
     dict:from_list(
-      [{Bucket, bucket_oom_errors(Bucket)}
+      [{Bucket, fetch_bucket_stat(Bucket, Stat)}
        || Bucket <- ns_memcached:active_buckets()]
      ).
 
-%% @doc Total count of oom errors on a bucket
--spec bucket_oom_errors(string()) -> integer().
-bucket_oom_errors(Bucket) ->
-    case stats_archiver:latest(Bucket, minute, ep_oom_errors) of
-        {ep_oom_errors, X} -> X;
+
+%% @doc fetch latest value of stat for particular bucket
+fetch_bucket_stat(Bucket, Stat) ->
+    case stats_archiver:latest(Bucket, minute, Stat) of
+        {Stat, X} -> X;
         _ -> 0
     end.
 
@@ -231,6 +240,11 @@ increased(Key, Val, Dict) ->
         {ok, Prev} ->
             Val > Prev
     end.
+
+
+%% Format the error message into a binary
+fmt_to_bin(Str, Args) ->
+    list_to_binary(lists:flatten(io_lib:format(Str, Args))).
 
 
 %% Cant currently test the alert timeouts as would need to mock
