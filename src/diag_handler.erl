98458e6c0e1f8eb6271e8a204714d435cfe6e780
Aliaksey Kandratsenka <alk@tut.by>
2011-04-16 20:14:07 -0700

    read manifest from /opt/membase
    voltron is not very ready and looks like really right place is
    /opt/membase/lib/ns_server/erlang/lib/ns_server*/priv/
    
    Change-Id: I66df9ab9af8ed7c5cd8d68b7074a2608f54f80ac
    Reviewed-on: http://review.membase.org/5456
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- src/diag_handler.erl
+++ src/diag_handler.erl
@@ -64,10 +64,10 @@ diag_filter_out_config_password(Config) ->
 
 % Read the manifest.txt file
 manifest() ->
-    case file:read_file(filename:join(path_config:component_path(lib, "ns_server"), "manifest.txt")) of
+    case file:read_file(filename:join(path_config:component_path(bin, ".."), "manifest.txt")) of
         {ok, C} ->
             lists:sort(string:tokens(binary_to_list(C), "\n"));
-        [] -> []
+        _ -> []
     end.
 
 grab_process_info(Pid) ->
