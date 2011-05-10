ec08571f5a53f4b85ded61ef1367ebb2bf5b878f
Aliaksey Kandratsenka <alk@tut.by>
2011-05-09 17:26:00 -0700

    Revert "start per-bucket supervisor asynchronously"
    This reverts commit 51bf2dc8fd8c94e5cdc95fade0a0368bc6a43a52.
    
    ns_memcached doesn't wait for potentially slow warmup on init()
    anymore, so there's no need for single_bucket_sup trick.
    
    Change-Id: Ib888d4c00abbed0f5d28ffb15805c262d9e5e0ac
    Reviewed-on: http://review.membase.org/6118
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Dale Harvey <daleharvey@arandomurl.com>
    Tested-by: Dale Harvey <daleharvey@arandomurl.com>

--- src/ns_bucket_sup.erl
+++ src/ns_bucket_sup.erl
@@ -61,9 +61,18 @@ get_this_node_bucket_names(BucketsConfigs) ->
 
 %% @private
 %% @doc The child specs for each bucket.
-child_specs(Bucket) ->
-    [{{per_bucket_sup, Bucket}, {single_bucket_sup, start_link, [Bucket]},
-      permanent, infinity, supervisor, [single_bucket_sup]}].
+child_specs(BucketName) ->
+    [{{ns_memcached, BucketName}, {ns_memcached, start_link, [BucketName]},
+      %% ns_memcached waits for the bucket to sync to disk before exiting
+      permanent, 86400000, worker, [ns_memcached]},
+     {{ns_vbm_sup, BucketName}, {ns_vbm_sup, start_link, [BucketName]},
+      permanent, 1000, worker, [ns_vbm_sup]},
+     {{stats_collector, BucketName}, {stats_collector, start_link, [BucketName]},
+      permanent, 1000, worker, [stats_collector]},
+     {{stats_archiver, BucketName}, {stats_archiver, start_link, [BucketName]},
+      permanent, 1000, worker, [stats_archiver]},
+     {{stats_reader, BucketName}, {stats_reader, start_link, [BucketName]},
+      permanent, 1000, worker, [stats_reader]}].
 
 
 update_childs(Buckets) ->
