17f38eeab3a1eb20786b7a2b2c9766830ec7d21c
Benjamin Young <byoung@bigbluehat.com>
2011-05-17 09:37:31 -0400

    moved mkClass into misc.js; consolidating JS
    Change-Id: Ideda852e01d0cf0032b2d628f132119c622cc446
    Reviewed-on: http://review.membase.org/6304
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Benjamin Young <benjamin@couchbase.com>

--- priv/public/js/misc.js
+++ priv/public/js/misc.js
@@ -961,3 +961,43 @@ function naturalSort (a, b) {
   }
   return 0;
 }
+
+function mkMethodWrapper (method, superClass, methodName) {
+  return function () {
+    var args = $.makeArray(arguments);
+    var _super = $m(this, methodName, superClass);
+    args.unshift(_super);
+    return method.apply(this, args);
+  };
+}
+
+function mkClass(methods) {
+  if (_.isFunction(methods)) {
+    var superclass = methods;
+    var origMethods = arguments[1];
+
+    var meta = function() {};
+    meta.prototype = superclass.prototype;
+
+    methods = _.extend(new meta(), origMethods);
+
+    _.each(origMethods, function (method, methodName) {
+      if (_.isFunction(method) && functionArgumentNames(method)[0] === '$super') {
+        methods[methodName] = mkMethodWrapper(method, superclass, methodName);
+      }
+    });
+  } else {
+    methods = _.extend({}, methods);
+  }
+
+  var constructor = function () {
+      if (this.initialize) {
+        return this.initialize.apply(this, arguments);
+      }
+    };
+
+  methods.constructor = constructor;
+  constructor.prototype = methods;
+
+  return constructor;
+}
