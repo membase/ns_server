99b34a0becb68c4eb72d0175028eaf70119b2d97
Benjamin Young <byoung@bigbluehat.com>
2011-05-17 10:21:50 -0400

    moved multi-drawers-widget into app-misc.js; consolidating JS
    Change-Id: If8d556347ff93d388f82fc8d4f8ca36ea07c534d
    Reviewed-on: http://review.membase.org/6310
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Benjamin Young <benjamin@couchbase.com>

--- priv/public/js/app-misc.js
+++ priv/public/js/app-misc.js
@@ -1084,3 +1084,234 @@ $.plotSafe = function (placeholder/*, rest...*/) {
   }
   return $.plot.apply($, arguments);
 };
+
+var KeyedMap = mkClass({
+  initialize: function (itemKeyFunction) {
+    if (!(itemKeyFunction instanceof Function)) {
+      var keyAttr = itemKeyFunction;
+      itemKeyFunction = function (item) {return item[keyAttr]};
+    }
+    this.keyOf = itemKeyFunction;
+    this.map = {};
+  },
+  put: function (item, target) {
+    this.map[this.keyOf(item)] = target;
+  },
+  clear: function () {
+    this.map = {}
+  },
+  remove: function (item) {
+    if (item == null)
+      return;
+    var key = this.keyOf(item);
+    var rv = this.map[key];
+    delete this.map[key];
+    return rv;
+  },
+  get: function (item) {
+    if (item == null)
+      return;
+    return this.map[this.keyOf(item)];
+  }
+});
+
+// applies function to all items and forms KeyedMap with items ->
+// values association. Takes care of reusing old values when recomputing
+Cell.mapAllKeys = function (itemsCell, itemKeyFunction, valueFunction) {
+  return Cell.compute(function (v) {
+    var items = v.need(itemsCell);
+    var newMap = new KeyedMap(itemKeyFunction);
+    var oldMap = this.self.value || newMap;
+
+    _.each(items, function (item) {
+      var value = oldMap.get(item) || valueFunction(item);
+      newMap.put(item, value);
+    });
+
+    return newMap;
+  });
+}
+
+// TODO
+Cell.computeListDetails = function (itemsCell, detailsAttr, detailsFunction) {
+  return Cell.compute(function (v) {
+    return _.map(v.need(itemsCell), function (item) {
+      item = _.clone(item);
+      item[detailsAttr] = detailsFunction(item);
+      return item;
+    });
+  });
+}
+
+var MultiDrawersWidget = mkClass({
+  mandatoryOptions: "hashFragmentParam template elementKey listCell".split(" "),
+  initialize: function (options) {
+    options = this.options = _.extend({
+      placeholderCSS: '.settings-placeholder'
+    }, options);
+
+    var missingOptions = _.reject(this.mandatoryOptions, function (n) {
+      return (n in options);
+    });
+
+    if (missingOptions.length)
+      throw new Error("Missing mandatory option(s): " + missingOptions.join(','));
+
+    if (options.actionLink) {
+      configureActionHashParam(options.actionLink, $m(this, 'onActionLinkClick'));
+    }
+
+    this.knownKeys = {};
+
+    this.elementKey = this.options.elementKey;
+    if (!(this.elementKey instanceof Function)) {
+      this.elementKey = (function (itemAttr) {
+        return function (item) {return item[itemAttr]};
+      })(this.elementKey);
+    }
+
+    this.subscriptions = [];
+
+    var openedNamesCell = this.openedNames = new StringSetHashFragmentCell(options.hashFragmentParam);
+
+    var detailsCellProducer = (function (self) {
+      return function (item, key) {
+        console.log("producing details cell for key: ", key, ", item: ", item);
+        var detailsCell = Cell.compute(function (v) {
+          var url;
+
+          if (self.options.uriExtractor)
+            url = self.options.uriExtractor(item);
+          else
+            url = key;
+
+          return future.get({url: url});
+        });
+
+        var interested = Cell.compute(function (v) {
+          if (v.need(IOCenter.staleness))
+            return false;
+          return _.include(v(openedNamesCell) || [], key);
+        });
+
+        var rv = Cell.compute(function (v) {
+          if (!v(interested))
+            return;
+          return v(detailsCell);
+        });
+
+        rv.interested = interested;
+        detailsCell.keepValueDuringAsync = true;
+        rv.invalidate = $m(detailsCell, 'invalidate');
+
+        return rv;
+      }
+    })(this);
+
+    this.detailsMap = Cell.compute(function (v) {
+      var map = new KeyedMap(options.elementKey);
+      var oldMap = this.self.value || map;
+      _.each(v.need(options.listCell), function (item) {
+        var value = oldMap.get(item);
+        if (value) {
+          value.invalidate();
+        } else {
+          value = detailsCellProducer(item, map.keyOf(item));
+        }
+        map.put(item, value);
+      });
+      return map;
+    });
+  },
+  onActionLinkClick: function (uri, isMiddleClick) {
+    this.options.actionLinkCallback(uri);
+    if (isMiddleClick) {
+      this.openElement(uri);
+    } else
+      this.toggleElement(uri);
+  },
+  prepareDrawing: function () {
+    var subscriptions = this.subscriptions;
+    _.each(subscriptions, function (s) {
+      s.cancel();
+    });
+    subscriptions.length = 0;
+
+    $(this.options.placeholderCSS).hide();
+  },
+  subscribeDetailsRendering: function (parentNode, item) {
+    var self = this;
+    var options = self.options;
+    var subscriptions = self.subscriptions;
+
+    var q = $(parentNode);
+
+    var container = q[0];
+    if (!container) {
+      throw new Error("MultiDrawersWidget: bad markup!");
+    }
+
+    var detailsCell;
+    var valueSubscription = self.detailsMap.subscribeValue(function (detailsMap) {
+      if (detailsCell)
+        return;
+      if (!detailsMap)
+        return;
+      detailsCell = detailsMap.get(item);
+      if (!detailsCell)
+        return;
+
+      // we subscribe to detailsCell only once, which is first time we
+      // get it
+
+      var valueTransformer;
+      if (self.options.valueTransformer) {
+        valueTransformer = (function (transformer) {
+          return function (value) {
+            return transformer(item, value);
+          }
+        })(self.options.valueTransformer);
+      }
+
+      var renderer = mkCellRenderer([container, self.options.template], {
+        hideIf: function (cell) {
+          return !cell.interested.value;
+        },
+        valueTransformer: valueTransformer
+      }, detailsCell);
+
+      // this makes sure we render when any of interesting cells change
+      var s = Cell.compute(function (v) {
+        return [v(detailsCell), v(detailsCell.interested)];
+      }).subscribeValue(function (array) {
+        renderer();
+      });
+
+      subscriptions.push(s);
+    });
+
+    subscriptions.push(valueSubscription);
+  },
+  renderItemDetails: function (item) {
+    var self = this;
+    return ViewHelpers.thisElement(function (element) {
+      self.subscribeDetailsRendering(element, item);
+    });
+  },
+  toggleElement: function (name) {
+    if (_.include(this.openedNames.value, name)) {
+      this.closeElement(name);
+    } else {
+      this.openElement(name);
+    }
+  },
+  openElement: function (name) {
+    this.openedNames.addValue(name);
+  },
+  closeElement: function (name) {
+    this.openedNames.removeValue(name);
+  },
+  reset: function () {
+    this.openedNames.reset();
+  }
+});
