31ef4f1b210bfb5bc559d2b362a45d6a63ea1c49
Benjamin Young <byoung@bigbluehat.com>
2011-05-17 09:41:02 -0400

    moved callbacks.js into cells.js; consolidating JS
    Change-Id: I4727ed3c9257810887653e568f83d3a0517daa40
    Reviewed-on: http://review.membase.org/6305
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Benjamin Young <benjamin@couchbase.com>

--- priv/public/js/cells.js
+++ priv/public/js/cells.js
@@ -13,6 +13,91 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  **/
+var Slave = mkClass({
+  initialize: function (thunk) {
+    this.thunk = thunk
+  },
+  die: function () {this.dead = true;},
+  nMoreTimes: function (times) {
+    this.times = this.times || 0;
+    this.times += times;
+    var oldThunk = this.thunk;
+    this.thunk = function (data) {
+      oldThunk.call(this, data);
+      if (--this.times == 0)
+        this.die();
+    }
+    return this;
+  }
+});
+
+var CallbackSlot = mkClass({
+  initialize: function () {
+    this.slaves = [];
+    this.broadcasting = 0;
+  },
+  subscribeWithSlave: function (thunkOrSlave) {
+    var slave;
+    if (thunkOrSlave instanceof Slave)
+      slave = thunkOrSlave;
+    else
+      slave = new Slave(thunkOrSlave);
+    var wasEmpty = (this.slaves.length == 0);
+    this.slaves.push(slave);
+    if (wasEmpty)
+      this.__demandChanged(true);
+    return slave;
+  },
+  subscribeOnce: function (thunk) {
+    return this.subscribeWithSlave(thunk).nMoreTimes(1);
+  },
+  broadcast: function (data) {
+    this.broadcasting++;
+    _.each(this.slaves, function (slave) {
+      if (slave.dead)
+        return;
+      try {
+        slave.thunk(data);
+      } catch (e) {
+        console.log("got exception in CallbackSlot#broadcast", e, "for slave thunk", slave.thunk);
+        slave.die();
+        _.defer(function () {throw e;});
+      }
+    });
+    this.broadcasting--;
+    this.cleanup();
+  },
+  unsubscribeCallback: function (thunk) {
+    var slave = _.detect(this.slaves, function (candidate) {
+      return candidate.thunk == thunk;
+    });
+    if (slave)
+      this.unsubscribe(slave);
+    return slave;
+  },
+  unsubscribe: function (slave) {
+    slave.die();
+    if (this.broadcasting)
+      return;
+    var index = $.inArray(slave, this.slaves);
+    if (index >= 0) {
+      this.slaves.splice(index, 1);
+      if (!this.slaves.length)
+        this.__demandChanged(false);
+    }
+  },
+  cleanup: function () {
+    if (this.broadcasting)
+      return;
+    var oldLength = this.slaves.length;
+    this.slaves = _.reject(this.slaves, function (slave) {return slave.dead;});
+    if (oldLength && !this.slaves.length)
+      this.__demandChanged(false);
+  },
+  __demandChanged: function (haveDemand) {
+  }
+});
+
 // returns special value that when passed to Cell#setValue initiates async set
 // 'body' is a function that's passed cell-generated dataCallback
 // 'body' is assumed to arrange async process of computing/getting new value
