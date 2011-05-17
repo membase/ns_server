9b3084d5f7c1d79397c7f3e9cbd289f18eb90820
Benjamin Young <byoung@bigbluehat.com>
2011-05-17 09:57:27 -0400

    moved right-form-observer into app-ui-misc.js; consolidating JS
    Change-Id: I33c86c4d8c517334ba6a8dc29301f8101471af29
    Reviewed-on: http://review.membase.org/6307
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Benjamin Young <benjamin@couchbase.com>

--- priv/public/js/app-ui-misc.js
+++ priv/public/js/app-ui-misc.js
@@ -14,6 +14,111 @@
    limitations under the License.
  **/
 ;(function($){
+  $.fn.observePotentialChanges = (function () {
+    var intervalId;
+    var period = 20;
+    var maxIdlePeriods = 4;
+
+    var hadActivity;
+    var idlePeriods;
+
+    var idGen = 0;
+    var callbacks = {};
+    var callbacksSize = 0;
+
+    function timerCallback() {
+      for (var i in callbacks) {
+        (callbacks[i])();
+      }
+
+      if (!hadActivity) {
+        if (++idlePeriods >= maxIdlePeriods) {
+          suspendTimer();
+          idlePeriods = 0;
+        }
+      } else {
+        idlePeriods = 0;
+        hadActivity = undefined;
+      }
+    }
+    function activateTimer() {
+      hadActivity = true;
+      if (intervalId != null)
+        return;
+      intervalId = setInterval(timerCallback, period);
+    }
+    function suspendTimer() {
+      if (intervalId == null)
+        return;
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+    function requestTimer(callback) {
+      callbacks[++idGen] = callback;
+      callbacksSize++;
+
+      activateTimer();
+      return idGen;
+    }
+    function releaseTimer(id) {
+      delete callbacks[id];
+      if (--callbacksSize == 0)
+        suspendTimer();
+    }
+
+    return function (callback) {
+      var query = this;
+      var events = 'change mousemove click dblclick keyup keydown';
+      var boundF;
+      var id;
+
+      var instance = {
+        stopObserving: function () {
+          releaseTimer(id);
+          unbindEvents();
+        }
+      }
+
+      function cb() {
+        callback.call(null, instance);
+        if (!boundF)
+          bindEvents();
+      }
+
+      id = requestTimer(cb);
+
+      function bindEvents() {
+        query.bind(events, boundF = function (e) {
+          activateTimer();
+          unbindEvents();
+        });
+      }
+
+      function unbindEvents() {
+        query.unbind(events, boundF);
+        boundF = null;
+      }
+
+      bindEvents();
+      return instance;
+    }
+  })();
+
+  $.fn.observeInput = function (callback) {
+    var query = this;
+    var prevObject = query.prevObject || $('html > body');
+    var lastValue = query.val();
+
+    return prevObject.observePotentialChanges(function (instance) {
+      var newValue = query.val();
+      if (newValue == lastValue)
+        return;
+
+      lastValue = newValue;
+      callback.call(query, lastValue, instance);
+    });
+  };
+
   $.fn.boolAttr = function(attr, value) {
     var $this = $(this);
     if (value) {
