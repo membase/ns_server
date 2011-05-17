bda4e80855b77a2679b3ebfcdce881d5cf63baf1
Benjamin Young <byoung@bigbluehat.com>
2011-05-17 10:32:26 -0400

    combined JS for buckets sections; consolidating JS
    Change-Id: I32df75670b4bbab826ddfec5828bd955b6da1be2
    Reviewed-on: http://review.membase.org/6313
    Tested-by: Benjamin Young <benjamin@couchbase.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- priv/public/js/buckets.js
+++ priv/public/js/buckets.js
@@ -668,3 +668,46 @@ $(function () {
     dialog.find('.for-proxy-port input').boolAttr('disabled', isSasl);
   });
 });
+
+var MonitorBucketsSection = {
+  init: function () {
+    var bucketsListCell = DAL.cells.bucketsListCell;
+
+    var membaseBuckets = new Cell(function (bucketsList) {
+      return _.select(bucketsList, function (bucketInfo) {
+        return bucketInfo.bucketType == 'membase';
+      });
+    }, {
+      bucketsList: bucketsListCell
+    });
+    renderCellTemplate(membaseBuckets, 'monitor_persistent_buckets_list');
+
+    var memcachedBuckets = new Cell(function (bucketsList) {
+      return _.select(bucketsList, function (bucketInfo) {
+        return bucketInfo.bucketType != 'membase';
+      });
+    }, {
+      bucketsList: bucketsListCell
+    });
+    renderCellTemplate(memcachedBuckets, 'monitor_cache_buckets_list');
+
+    memcachedBuckets.subscribeValue(function (list) {
+      $('#monitor_buckets .memcached-buckets-subsection')[!list || list.length ? 'show' : 'hide']();
+    });
+    membaseBuckets.subscribeValue(function (list) {
+      $('#monitor_buckets .membase-buckets-subsection')[!list || list.length ? 'show' : 'hide']();
+    });
+
+    bucketsListCell.subscribeValue(function (list) {
+      var empty = (list && list.length == 0);
+      $('#monitor_buckets .no-buckets-subsection')[empty ? 'show' : 'hide']();
+    });
+
+    IOCenter.staleness.subscribeValue(function (stale) {
+      $('#monitor_buckets .staleness-notice')[stale ? 'show' : 'hide']();
+    });
+  },
+  onEnter: function () {
+    BucketsSection.refreshBuckets();
+  }
+};
