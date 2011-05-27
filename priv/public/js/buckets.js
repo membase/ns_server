09ad5c3c2fb990bb08cfb4298b7730a4a800b314
Aliaksey Kandratsenka <alk@tut.by>
2011-05-27 11:53:58 -0700

    ensure minimal size of arcs on bucket readiness pie chart
    Based of work of Benjamin Young.
    
    We increase sizes of too small args at the 'expense' of largest
    arc. With only 3 arcs max it seems acceptable approach, even though
    it's not entirely cleanest.
    
    Change-Id: I8fb19175c0557e7d6f1583c6edaa9752365a7676
    Reviewed-on: http://review.membase.org/6611
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- priv/public/js/buckets.js
+++ priv/public/js/buckets.js
@@ -508,10 +508,46 @@ var BucketsSection = {
       afterRendering: function () {
         _.each(bucketsListCell.value, function(bucketInfo) {
           var name = bucketInfo.name;
-          $($i(name+'_health')).sparkline(bucketInfo.healthStats, {
+
+          var healthStats = bucketInfo.healthStats;
+
+          var total = _.inject(healthStats, function (a,b) {return a+b}, 0);
+
+          var minimalAngle = Math.PI/180*30;
+          var nodeSize = Math.PI*2/total;
+
+          var stolenSize = 0;
+          var maxAngle = 0;
+          var maxIndex = -1;
+
+          for (var i = healthStats.length; i--;) {
+            var newValue = healthStats[i] * nodeSize;
+            if (newValue != 0 && newValue < minimalAngle) {
+              stolenSize += minimalAngle - newValue;
+              newValue = minimalAngle;
+            }
+            healthStats[i] = newValue;
+            if (newValue >= maxAngle) {
+              maxAngle = newValue;
+              maxIndex = i;
+            }
+          }
+
+          if (maxIndex < 0) {
+            throw new Error("BUG");
+          }
+
+          healthStats[maxIndex] -= stolenSize;
+
+          $($i(name+'_health')).sparkline(healthStats, {
             type: 'pie',
             sliceColors: ['#4A0', '#fac344', '#f00'],
             height: (isCanvasSupported ? '1.5em' : 'auto')
+          }).mouseover(function(ev) {
+            $(ev.target).attr('title',
+                bucketInfo.healthStats[0] + ' healthy, ' +
+                bucketInfo.healthStats[1] + ' unhealthy, ' +
+                bucketInfo.healthStats[2] + ' down');
           });
         });
       },
