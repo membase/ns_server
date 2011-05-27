217e75bd6af0b83af8ba41625c4838b4b9273865
Aliaksey Kandratsenka <alk@tut.by>
2011-05-27 11:57:58 -0700

    remove badarith diagnostics code in menelaus_stats
    Change-Id: Id281ac03860116b244e5e151ea0d4f71ae98d602
    Reviewed-on: http://review.couchbase.org/6889
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Bin Cui <bin.cui@gmail.com>

--- src/menelaus_stats.erl
+++ src/menelaus_stats.erl
@@ -54,26 +54,16 @@ bucket_ram_usage(BucketName) ->
 extract_stat(StatName, Sample) ->
     case orddict:find(StatName, Sample#stat_entry.values) of
         error -> 0;
-        {ok, V} when not is_number(V) ->
-            ?log_error("Got non-number (~p) for ~p~n~p~n", [V, StatName, Sample]),
-            V;
         {ok, V} -> V
     end.
 
 last_membase_sample(BucketName, Nodes) ->
     lists:foldl(fun ({_Node, []}, Acc) -> Acc;
                     ({_Node, [Sample|_]}, {AccMem, AccItems, AccOps, AccFetches}) ->
-                        try
-                            {extract_stat(mem_used, Sample) + AccMem,
-                             extract_stat(curr_items, Sample) + AccItems,
-                             extract_stat(ops, Sample) + AccOps,
-                             extract_stat(ep_bg_fetched, Sample) + AccFetches}
-                        catch error:badarith ->
-                                ?log_error("Badarith details:~nSample:~p~n,~p~n~p~n", [Sample,
-                                                                                       {AccMem, AccItems, AccOps, AccFetches},
-                                                                                       erlang:get_stacktrace()]),
-                                {0, 0, 0, 0}
-                        end
+                        {extract_stat(mem_used, Sample) + AccMem,
+                         extract_stat(curr_items, Sample) + AccItems,
+                         extract_stat(ops, Sample) + AccOps,
+                         extract_stat(ep_bg_fetched, Sample) + AccFetches}
                 end, {0, 0, 0, 0}, invoke_archiver(BucketName, Nodes, {1, minute, 1})).
 
 last_memcached_sample(BucketName, Nodes) ->
@@ -83,16 +73,11 @@ last_memcached_sample(BucketName, Nodes) ->
      CmdGet,
      GetHits} = lists:foldl(fun ({_Node, []}, Acc) -> Acc;
                                 ({_Node, [Sample|_]}, {AccMem, AccItems, AccOps, AccGet, AccGetHits}) ->
-                                    try {extract_stat(mem_used, Sample) + AccMem,
-                                         extract_stat(curr_items, Sample) + AccItems,
-                                         extract_stat(ops, Sample) + AccOps,
-                                         extract_stat(cmd_get, Sample) + AccGet,
-                                         extract_stat(get_hits, Sample) + AccGetHits}
-                                    catch error:badarith ->
-                                            ?log_error("Badarith details:~nSample:~p~n~p~n", [Sample,
-                                                                                              erlang:get_stacktrace()]),
-                                            {0, 0, 0, 0, 0}
-                                    end
+                                    {extract_stat(mem_used, Sample) + AccMem,
+                                     extract_stat(curr_items, Sample) + AccItems,
+                                     extract_stat(ops, Sample) + AccOps,
+                                     extract_stat(cmd_get, Sample) + AccGet,
+                                     extract_stat(get_hits, Sample) + AccGetHits}
                             end, {0, 0, 0, 0, 0}, invoke_archiver(BucketName, Nodes, {1, minute, 1})),
     {MemUsed,
      CurrItems,
