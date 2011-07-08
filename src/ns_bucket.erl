28f1e30fd76bef331e7c28398be037e50e30390b
Aliaksey Kandratsenka <alk@tut.by>
2011-07-09 00:53:16 +0300

    prevent creating port auth buckets on CAPI port
    Change-Id: If4ce28b77c8cdbc43a40ad24d6780eb64557eba8
    Reviewed-on: http://review.couchbase.org/7830
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Dale Harvey <daleharvey@arandomurl.com>
    Tested-by: Dale Harvey <daleharvey@arandomurl.com>

--- src/ns_bucket.erl
+++ src/ns_bucket.erl
@@ -432,6 +432,7 @@ is_port_free(BucketName, Port, Config) ->
     Port =/= ns_config:search_node_prop(Config, memcached, port)
         andalso Port =/= ns_config:search_node_prop(Config, moxi, port)
         andalso Port =/= ns_config:search_node_prop(Config, memcached, mccouch_port, 11213)
+        andalso Port =/= ns_config:search_node_prop(Config, rest, capi_port)
         andalso Port =/= proplists:get_value(port, menelaus_web:webconfig(Config))
         andalso is_open_proxy_port(BucketName, Port).
 
