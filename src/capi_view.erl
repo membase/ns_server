c2102ba8039f6ac9f0aa095b6de990d29ff94afc
Filipe David Manana <fdmanana@apache.org>
2011-07-13 19:01:34 +0100

    Add execution for /bucket/_all_docs
    This will merge the _all_docs result of each vbucket database,
    belonging to the given bucket, of all nodes in the cluster.
    Example:
    
    Bucket "default" as vbuckets 0 to 7 in node 0 (localhost:9500)
    and buckets 8 to 15 in node 1 (localhost:9501).
    
    Merged result of _all_docs is:
    
    $ curl http://localhost:9500/default/_all_docs
    {"total_rows":3,"rows":[
    {"id":"doc1","key":"doc1","value":{"rev":"1-967a00dff5e02add41819138abb3284d"}},
    {"id":"doc14","key":"doc14","value":{"rev":"1-967a00dff5e02add41819138abb3284d"}},
    {"id":"doc2","key":"doc2","value":{"rev":"1-967a00dff5e02add41819138abb3284d"}}
    ]}
    
    VBucket 0, node 0, has:
    
    $ curl http://localhost:9500/default%2F0/_all_docs
    {"total_rows":1,"offset":0,"rows":[
    {"id":"doc1","key":"doc1","value":{"rev":"1-967a00dff5e02add41819138abb3284d"}}
    ]}
    
    Vbucket 1, node 0, has:
    
    $ curl http://localhost:9500/default%2F1/_all_docs
    {"total_rows":1,"offset":0,"rows":[
    {"id":"doc2","key":"doc2","value":{"rev":"1-967a00dff5e02add41819138abb3284d"}}
    ]}
    
    Vbucket 14, node 1, has:
    
    $ curl http://localhost:9501/default%2F14/_all_docs
    {"total_rows":1,"offset":0,"rows":[
    {"id":"doc14","key":"doc14","value":{"rev":"1-967a00dff5e02add41819138abb3284d"}}
    ]}
    
    Change-Id: I820d7e938e4c3d61b316c9a94a208fccb7ca75a7
    Reviewed-on: http://review.couchbase.org/7951
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/capi_view.erl
+++ src/capi_view.erl
@@ -20,72 +20,110 @@
 -include("ns_common.hrl").
 
 -export([handle_view_req/3]).
+-export([all_docs_db_req/2]).
 
-design_doc_view(Req, #db{name=BucketName} = _Db, DName, ViewName, Keys) ->
-    DDId = <<"_design/", DName/binary>>,
-    {ok, BucketConfig} = ns_bucket:get_bucket(binary_to_list(BucketName)),
-    Map = proplists:get_value(map, BucketConfig),
-    true = is_list(Map),
-    {_, NodeToVBuckets} =
-        lists:foldl(fun ([Master | _], {Idx, Dict}) ->
-                            {Idx+1, dict:append(Master, Idx, Dict)}
-                    end, {0, dict:new()}, Map),
-    Config = ns_config:get(),
-    FullViewName = iolist_to_binary([DDId, $/, ViewName]),
-    ViewSpecs = dict:fold(fun (Node, VBuckets, Acc) when Node =:= node() ->
-                                  build_local_specs(BucketName, DDId, ViewName, VBuckets)
-                                      ++ Acc;
-                              (Node, VBuckets, Acc) ->
-                                  [build_remote_specs(Node, BucketName, FullViewName, VBuckets, Config)
-                                   | Acc]
-                          end, [], NodeToVBuckets),
-    MergeParams0 = #view_merge{
-      views = ViewSpecs,
-      keys = Keys
-     },
-    MergeParams = couch_httpd_view_merger:apply_http_config(Req, [], MergeParams0),
+-import(couch_util, [
+    get_value/2,
+    get_value/3
+]).
+
+design_doc_view(Req, Db, DesignName, ViewName) ->
+    DDocId = <<"_design/", DesignName/binary>>,
+    MergeParams = view_merge_params(Req, Db, DDocId, ViewName),
     couch_view_merger:query_view(Req, MergeParams).
 
-vbucket_db_name(BucketName, VBucket) ->
-    iolist_to_binary([BucketName, $/, integer_to_list(VBucket)]).
-
-build_local_specs(BucketName, DDId, ViewName, VBuckets) ->
-    lists:map(fun (VBucket) ->
-                      #simple_view_spec{database = vbucket_db_name(BucketName, VBucket),
-                                        ddoc_id = DDId,
-                                        view_name = ViewName}
-              end, VBuckets).
-
-build_remote_specs(Node, BucketName, FullViewName, VBuckets, Config) ->
-    MergeURL = iolist_to_binary(capi_utils:capi_url(Node, "/_view_merge", "127.0.0.1", Config)),
-    Props = {[{<<"views">>,
-               {[{vbucket_db_name(BucketName, VBId), FullViewName}
-                 || VBId <- VBuckets]}}]},
-    #merged_view_spec{url = MergeURL,
-                      ejson_spec = Props}.
 
 handle_view_req(Req, Db, DDoc) when Db#db.filepath =/= undefined ->
     couch_httpd_view:handle_view_req(Req, Db, DDoc);
 
 handle_view_req(#httpd{method='GET',
         path_parts=[_, _, DName, _, ViewName]}=Req, Db, _DDoc) ->
-    Keys = couch_httpd:qs_json_value(Req, "keys", nil),
-    design_doc_view(Req, Db, DName, ViewName, Keys);
+    design_doc_view(Req, Db, DName, ViewName);
 
 handle_view_req(#httpd{method='POST',
         path_parts=[_, _, DName, _, ViewName]}=Req, Db, _DDoc) ->
     couch_httpd:validate_ctype(Req, "application/json"),
-    {Fields} = couch_httpd:json_body_obj(Req),
-    case couch_util:get_value(<<"keys">>, Fields, nil) of
-        nil ->
-            ?log_info("POST to view ~p/~p in database ~p with no keys member.",
-                      [DName, ViewName, Db]),
-            design_doc_view(Req, Db, DName, ViewName, nil);
-        Keys when is_list(Keys) ->
-            design_doc_view(Req, Db, DName, ViewName, Keys);
-        _ ->
-            throw({bad_request, "`keys` member must be a array."})
-    end;
+    design_doc_view(Req, Db, DName, ViewName);
 
 handle_view_req(Req, _Db, _DDoc) ->
     couch_httpd:send_method_not_allowed(Req, "GET,POST,HEAD").
+
+
+all_docs_db_req(Req, #db{filepath = undefined} = Db) ->
+    MergeParams = view_merge_params(Req, Db, nil, <<"_all_docs">>),
+    couch_view_merger:query_view(Req, MergeParams);
+
+all_docs_db_req(Req, Db) ->
+    couch_httpd_db:db_req(Req, Db).
+
+
+node_vbuckets_dict(BucketName) ->
+    {ok, BucketConfig} = ns_bucket:get_bucket(BucketName),
+    Map = get_value(map, BucketConfig, []),
+    {_, NodeToVBuckets} =
+        lists:foldl(fun([Master | _], {Idx, Dict}) ->
+            {Idx + 1, dict:append(Master, Idx, Dict)}
+        end, {0, dict:new()}, Map),
+    NodeToVBuckets.
+
+
+view_merge_params(Req, #db{name = BucketName}, DDocId, ViewName) ->
+    NodeToVBuckets = node_vbuckets_dict(?b2l(BucketName)),
+    Config = ns_config:get(),
+    FullViewName = case DDocId of
+    nil ->
+        % _all_docs and other special builtin views
+        ViewName;
+    _ ->
+        iolist_to_binary([DDocId, $/, ViewName])
+    end,
+    ViewSpecs = dict:fold(
+        fun(Node, VBuckets, Acc) when Node =:= node() ->
+            build_local_specs(BucketName, DDocId, ViewName, VBuckets) ++ Acc;
+        (Node, VBuckets, Acc) ->
+           [build_remote_specs(Node, BucketName, FullViewName, VBuckets, Config) | Acc]
+        end, [], NodeToVBuckets),
+    case Req#httpd.method of
+    'GET' ->
+        Body = [],
+        Keys = validate_keys_param(couch_httpd:qs_json_value(Req, "keys", nil));
+    'POST' ->
+        {Body} = couch_httpd:json_body_obj(Req),
+        Keys = validate_keys_param(get_value(<<"keys">>, Body, nil))
+    end,
+    MergeParams0 = #view_merge{
+        views = ViewSpecs,
+        keys = Keys
+    },
+    couch_httpd_view_merger:apply_http_config(Req, Body, MergeParams0).
+
+
+validate_keys_param(nil) ->
+    nil;
+validate_keys_param(Keys) when is_list(Keys) ->
+    Keys;
+validate_keys_param(_) ->
+    throw({bad_request, "`keys` parameter is not an array."}).
+
+
+vbucket_db_name(BucketName, VBucket) ->
+    iolist_to_binary([BucketName, $/, integer_to_list(VBucket)]).
+
+
+build_local_specs(BucketName, DDocId, ViewName, VBuckets) ->
+    lists:map(fun(VBucket) ->
+            #simple_view_spec{
+                database = vbucket_db_name(BucketName, VBucket),
+                ddoc_id = DDocId,
+                view_name = ViewName
+            }
+        end, VBuckets).
+
+
+build_remote_specs(Node, BucketName, FullViewName, VBuckets, Config) ->
+    MergeURL = iolist_to_binary(capi_utils:capi_url(Node, "/_view_merge", "127.0.0.1", Config)),
+    Props = {[
+        {<<"views">>,
+            {[{vbucket_db_name(BucketName, VBId), FullViewName} || VBId <- VBuckets]}}
+    ]},
+    #merged_view_spec{url = MergeURL, ejson_spec = Props}.
