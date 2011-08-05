2adf018d65f5a8ae8b63d461c8ab153c7be41633
Bin Cui <bin.cui@gmail.com>
2011-08-05 11:16:09 -0700

    MB-3961 Work around inet:getiflist() bug under multi-home
    inet:getiflist() doesn't work well if multi-ipaddress are configured.
    Both uuid.erl and addr_utils.erl depend on this function. As such,
    either we use inet:getifadrs() or couch_uuid to remove this dependency.
    
    Change-Id: I2a69b8a146c741203311a614aed335e70aa22680
    Reviewed-on: http://review.couchbase.org/8646
    Reviewed-by: Filipe David Borba Manana <fdmanana@gmail.com>
    Tested-by: Filipe David Borba Manana <fdmanana@gmail.com>

--- src/menelaus_web.erl
+++ src/menelaus_web.erl
@@ -380,8 +380,7 @@ handle_complete_join(Req) ->
 get_uuid() ->
     case ns_config:search(uuid) of
         false ->
-            Uuid = list_to_binary(
-                uuid:uuid_to_string(uuid:get_v1(uuid:new(self())))),
+            Uuid = couch_uuids:new(),
             ns_config:set(uuid, Uuid),
             Uuid;
         {value, Uuid2} ->
