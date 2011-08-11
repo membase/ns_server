31e39c6b66fc3f2839a2f79855bc5ecb8e5ec796
Bin Cui <bin.cui@gmail.com>
2011-08-10 17:51:16 -0700

    Adjust the default vbucket number from 256 to 16
    Change-Id: I0b16d80025ea2c03c5b834fdc63cdeaa4e1428ce
    Reviewed-on: http://review.couchbase.org/8828
    Tested-by: Sharon Barr <sharonyb@gmail.com>
    Reviewed-by: Sharon Barr <sharonyb@gmail.com>

--- src/ns_bucket.erl
+++ src/ns_bucket.erl
@@ -458,7 +458,7 @@ getenv_int(VariableName, DefaultValue) ->
 
 new_bucket_default_params(membase) ->
     [{type, membase},
-     {num_vbuckets, getenv_int("MEMBASE_NUM_VBUCKETS", 256)},
+     {num_vbuckets, getenv_int("MEMBASE_NUM_VBUCKETS", 16)},
      {num_replicas, 1},
      {ram_quota, 0},
      {servers, []}];
