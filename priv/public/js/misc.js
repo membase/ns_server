a5a9f5cac8f945b5258deaeec41554e71d8dbaae
Aliaksey Kandratsenka <alk@tut.by>
2011-08-22 18:03:00 +0300

    unbreak serializeForm w.r.t. fields order
    This makes it's behavior consistent with what browsers do when they
    see duplicate input names in forms (preferring later inputs, as
    utilized by rails checkbox helpers).
    
    Change-Id: I148b5cc5b1baed7d9d36246999b76631f0ce621f
    Reviewed-on: http://review.couchbase.org/9181
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- priv/public/js/misc.js
+++ priv/public/js/misc.js
@@ -718,9 +718,10 @@ function serializeForm(f) {
   f = $(f);
   var array = f.serializeArray();
   var seenKeys = {};
-  array = _.filter(array, function (pair) {
-    if (seenKeys[pair.name])
+  array = _.filter(array.reverse(), function (pair) {
+    if (seenKeys[pair.name]) {
       return false;
+    }
     seenKeys[pair.name] = true;
     return true;
   });
