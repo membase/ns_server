975521f49876f7b4e689230a8fd1b4a8c07c7ac4
Aliaksey Kandratsenka <alk@tut.by>
2011-08-23 13:30:26 +0300

    refactored cb_config_couch_sync into separate gen_server process
    So that it doesn't block ns_config_events process.
    
    Change-Id: Ie664ab249066abba83965ec52751c7da2d5025ae
    Reviewed-on: http://review.couchbase.org/9183
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/cb_config_couch_sync.erl
+++ src/cb_config_couch_sync.erl
@@ -12,56 +12,112 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
+%%
+%% @doc This server syncs part of (replicated) ns_config to local
+%% couch config.  Actual sync is performed by worker process that's
+%% direct son of supervisor. ns_config changes are noted by gen_server
+%% process that's son of worker process. And it's using ns_pubsub to
+%% tap into ns_config changes.
+%%
+%% Such seemingly unusual design (with worker being parent of main
+%% process) is to ensure that supervisor shutdown synchronizes with
+%% worker death (which mutates world state). Main process does not
+%% mutate world's state, so worker can exit without waiting for main
+%% process death (it'll die soon but we don't care when).
 
 -module(cb_config_couch_sync).
 
--behaviour(gen_event).
+-behaviour(gen_server).
 
 -export([start_link/0]).
 
 %% gen_event callbacks
--export([init/1, handle_event/2, handle_call/2,
+-export([init/1, handle_call/3, handle_cast/2,
          handle_info/2, terminate/2, code_change/3]).
 
 -include("ns_common.hrl").
 
--record(state, {}).
+-record(state,
+        {
+          have_notable_change :: boolean(),
+          sync_started :: boolean(),
+          worker_pid :: pid()
+        }).
 
 start_link() ->
-    misc:start_event_link(
-      fun () ->
-              gen_event:add_sup_handler(ns_config_events,
-                                        {?MODULE, ns_config_events},
-                                        [])
-      end).
+    {ok, proc_lib:spawn_link(fun start_worker_loop/0)}.
 
-init([]) ->
-    {value, CouchConf} = ns_config:search_node(couchdb),
-    update_couchdb_config(CouchConf),
-    {ok, #state{}}.
+start_worker_loop() ->
+    erlang:process_flag(trap_exit, true),
+    {ok, _} = gen_server:start_link({local, ?MODULE}, ?MODULE, [self()], []),
+    worker_loop().
+
+worker_loop() ->
+    receive
+        {'EXIT', _From, Reason} ->
+            exit(Reason);
+        {sync_config, Pid} ->
+            do_config_sync(),
+            Pid ! sync_done;
+        X ->
+            ?log_error("couch sync worker got unknown message: ~p~n", [X]),
+            exit({unknown_message, X})
+    end,
+    worker_loop().
+
+init([WorkerPid]) ->
+    ns_pubsub:subscribe(ns_config_events, fun handle_config_event/2, []),
+    {ok, maybe_start_sync(#state{have_notable_change = true,
+                                 sync_started = false,
+                                 worker_pid = WorkerPid})}.
 
 terminate(_Reason, _State) ->
     ok.
-
 code_change(_OldVsn, State, _) ->
     {ok, State}.
 
-handle_call(Request, State) ->
-    ?log_info("handle_call(~p, ~p)", [Request, State]),
-    {ok, State}.
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
 
-handle_info(Info, State) ->
-    ?log_info("handle_info(~p, ~p)", [Info, State]),
-    {ok, State}.
+handle_cast(notable_config_change, State) ->
+    {noreply, maybe_start_sync(State#state{have_notable_change = true})};
+handle_cast(_Request, State) ->
+    {noreply, State}.
 
-handle_event({{node, Node, couchdb}, Config}, State) when Node =:= node() ->
-    update_couchdb_config(Config),
-    {ok, State};
-
-handle_event(_Event, State) ->
-    {ok, State}.
+handle_info(sync_done, State) ->
+    {noreply, maybe_start_sync(State#state{sync_started = false})};
+handle_info(_Event, State) ->
+    {noreply, State}.
 
 %% Auxiliary functions.
+
+is_notable_event({{node, Node, couchdb}, _}) when Node =:= node() -> true;
+is_notable_event(_) -> false.
+
+handle_config_event(KVPair, State) ->
+    case is_notable_event(KVPair) of
+        true -> gen_server:cast(?MODULE, notable_config_change);
+        _ -> ok
+    end,
+    State.
+
+maybe_start_sync(#state{have_notable_change = false} = State) ->
+    State;
+maybe_start_sync(#state{sync_started = true} = State) ->
+    State;
+maybe_start_sync(#state{worker_pid = Pid} = State) ->
+    Pid ! {sync_config, self()},
+    State#state{have_notable_change = false,
+                sync_started = true}.
+
+do_config_sync() ->
+    Config = ns_config:get(),
+    case ns_config:search(Config, {node, node(), couchdb}) of
+        {value, CouchDBCfg} ->
+            update_couchdb_config(CouchDBCfg);
+        _ -> ok
+    end.
+
 update_couchdb_config(Config) ->
     %% Updates couch config. Returns true if couchdb needs to be restarted.
     MaybeUpdate =
