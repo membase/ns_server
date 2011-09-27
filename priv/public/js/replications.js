39167a33198cb62f893ac27f1356e68d51af6563
Aliaksey Kandratsenka <alk@tut.by>
2011-09-27 23:11:52 +0300

    implemented UI for managing remote cluster references
    Change-Id: If31bb9981d97fb2d14d80d3ee8f4403797380ad1
    Reviewed-on: http://review.couchbase.org/9898
    Reviewed-by: Benjamin Young <benjamin@couchbase.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- priv/public/js/replications.js
+++ priv/public/js/replications.js
@@ -13,10 +13,25 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  **/
+var ReplicationsModel = {};
+
+(function () {
+  var model = ReplicationsModel;
+  var remoteClustersListURICell = model.remoteClustersListURICell = Cell.compute(function (v) {
+    if (v.need(DAL.cells.mode) != 'replications')
+      return;
+    return v.need(DAL.cells.currentPoolDetailsCell).remoteClusters.uri;
+  });
+  var remoteClustersListCell = model.remoteClustersListCell = Cell.compute(function (v) {
+    return future.get({url: v.need(remoteClustersListURICell)}, function (list) {
+      return _.sortBy(list, function (info) {return info.name});
+    });
+  });
+})();
+
 var ReplicationsSection = {
   init: function () {
-    renderTemplate('cluster_reference_list',
-        {rows:[{name:"london", ip:"127.0.0.1"}]});
+    renderCellTemplate(ReplicationsModel.remoteClustersListCell, 'cluster_reference_list');
     renderTemplate('ongoing_replications_list',
         {rows:[{bucket:"default", to:"london", status: "Completed",
           when: "on change",
@@ -25,9 +40,20 @@ var ReplicationsSection = {
         {rows:[{bucket:"default", to:"bangalore", status: "Failed",
           when: "one time sync",
           lastRun: 1315486775341}]});
-    
-    $('#create_cluster_reference, #cluster_reference_list_container .list_button').click(function() {
-      showDialog('create_cluster_reference_dialog');
+
+    $('#create_cluster_reference').click($m(this, 'startAddRemoteCluster'));
+    $('#cluster_reference_list_container').delegate('.list_button.edit-button', 'click', function() {
+      var name = $(this).closest('tr').attr('data-name');
+      if (!name) {
+        return;
+      }
+      ReplicationsSection.startEditRemoteCluster(name);
+    }).delegate('.list_button.delete-button', 'click', function() {
+      var name = $(this).closest('tr').attr('data-name');
+      if (!name) {
+        return;
+      }
+      ReplicationsSection.startDeleteRemoteCluster(name);
     });
     $('#create_replication, #ongoing_replications_list_container .list_button').click(function() {
       showDialog('create_replication_dialog');
@@ -35,6 +61,117 @@ var ReplicationsSection = {
     $('#create_replication_dialog .save_button').click(function() {
       overlayWithSpinner($('#create_replication_dialog'), null, 'Verifying...');
     });
+  },
+  getRemoteCluster: function (name, body) {
+    var self = this;
+    var generation = {};
+    self.getRemoteClusterGeneration = generation;
+    ReplicationsModel.remoteClustersListCell.getValue(function (remoteClustersList) {
+      if (generation !== self.getRemoteClusterGeneration) {
+        return;
+      }
+      var remoteCluster = _.detect(remoteClustersList, function (candidate) {
+        return (candidate.name === name)
+      });
+      body.call(self, remoteCluster);
+    });
+  },
+  startEditRemoteCluster: function (name) {
+    this.getRemoteCluster(name, function (remoteCluster) {
+      if (!remoteCluster) {
+        return;
+      }
+      editRemoteCluster(remoteCluster);
+    });
+    return;
+
+    function editRemoteCluster(remoteCluster) {
+      var form = $('#create_cluster_reference_dialog form');
+      setFormValues(form, remoteCluster);
+      $('#create_cluster_reference_dialog_errors_container').html('');
+      showDialog('create_cluster_reference_dialog', {
+        onHide: onHide
+      });
+      form.bind('submit', onSubmit);
+
+      function onSubmit(e) {
+        e.preventDefault();
+        ReplicationsSection.submitRemoteCluster(remoteCluster.uri, form);
+      }
+
+      function onHide() {
+        form.unbind('submit', onSubmit);
+      }
+    }
+  },
+  submitRemoteCluster: function (uri, form) {
+    var spinner = overlayWithSpinner(form);
+    postWithValidationErrors(uri, form, function (data, status) {
+      spinner.remove();
+      if (status == 'success') {
+        hideDialog('create_cluster_reference_dialog');
+        ReplicationsModel.remoteClustersListCell.invalidate();
+        return;
+      }
+      renderTemplate('create_cluster_reference_dialog_errors', _.values(data[0]).sort());
+    })
+  },
+  startAddRemoteCluster: function () {
+    var form = $('#create_cluster_reference_dialog form');
+    form.find('input[type=text], input[type=number], input[type=password], input:not([type])').val('');
+    $('#create_cluster_reference_dialog_errors_container').html('');
+    form.bind('submit', onSubmit);
+    showDialog('create_cluster_reference_dialog', {
+      onHide: onHide
+    });
+    return;
+
+    function onHide() {
+      form.unbind('submit', onSubmit);
+    }
+    var lastGen;
+    function onSubmit(e) {
+      e.preventDefault();
+      var gen = lastGen = {};
+      ReplicationsModel.remoteClustersListURICell.getValue(function (url) {
+        if (lastGen !== gen) {
+          return;
+        }
+        ReplicationsSection.submitRemoteCluster(url, form);
+      });
+    }
+  },
+  startDeleteRemoteCluster: function (name) {
+    var remoteCluster;
+    this.getRemoteCluster(name, function (_remoteCluster) {
+      remoteCluster = _remoteCluster;
+      if (!remoteCluster) {
+        return;
+      }
+      genericDialog({text: "Please, confirm deleting remote cluster reference '" + name + "'.",
+                     callback: dialogCallback});
+    });
+    return;
+
+    function dialogCallback(e, name, instance) {
+      instance.close();
+      if (name != 'ok') {
+        return;
+      }
+
+      ReplicationsModel.remoteClustersListCell.setValue(undefined);
+
+      $.ajax({
+        type: 'DELETE',
+        url: remoteCluster.uri,
+        success: ajaxCallback,
+        errors: ajaxCallback
+      });
+
+      function ajaxCallback() {
+        ReplicationsModel.remoteClustersListCell.invalidate();
+      }
+    }
   },
   onEnter: function() {
   }
