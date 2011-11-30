caaa649e3214f3ce4ec59bf186a316b0fbe086bc
Steve Yen <steve.yen@gmail.com>
2011-11-29 17:26:42 -0800

    misc:parse_version handles versions with 3 or more parts
    Change-Id: I7881e50ab31875638b5b540625469bb64a0561ce
    Reviewed-on: http://review.couchbase.org/11284
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

--- src/misc.erl
+++ src/misc.erl
@@ -1078,7 +1078,10 @@ init([X | Rest]) ->
     [X | init(Rest)].
 
 %% Parse version of the form 1.7.0r_252_g1e1c2c0 or 1.7.0r-252-g1e1c2c0 into a
-%% list {[1,7,0],candidate,252}.
+%% list {[1,7,0],candidate,252}.  1.8.0 introduces a license type suffix,
+%% like: 1.8.0r-25-g1e1c2c0-enterprise.  Note that we should never
+%% see something like 1.7.0-enterprise, as older nodes won't carry
+%% the license type information.
 -spec parse_version(string()) -> version().
 parse_version(VersionStr) ->
     Parts = string:tokens(VersionStr, "_-"),
@@ -1086,7 +1089,7 @@ parse_version(VersionStr) ->
         [BaseVersionStr] ->
             {BaseVersion, Type} = parse_base_version(BaseVersionStr),
             {BaseVersion, Type, 0};
-        [BaseVersionStr, OffsetStr, _Hash] ->
+        [BaseVersionStr, OffsetStr | _Rest] ->
             {BaseVersion, Type} = parse_base_version(BaseVersionStr),
             {BaseVersion, Type, list_to_integer(OffsetStr)}
     end.
@@ -1135,5 +1138,10 @@ parse_version_test() ->
                      parse_version("1.7.0")),
     ?assertEqual(true,
                  parse_version("1.7.1_252_g1e1c2c0") >
-                     parse_version("1.7.1_251_g1e1c2c1")).
+                     parse_version("1.7.1_251_g1e1c2c1")),
+    ?assertEqual({[1,8,0],release,25},
+                 parse_version("1.8.0_25_g1e1c2c0-enterprise")),
+    ?assertEqual({[1,8,0],release,25},
+                 parse_version("1.8.0-25-g1e1c2c0-enterprise")),
+    ok.
 -endif.
