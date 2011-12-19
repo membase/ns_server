c3b3f9cb1f3e716370a5bd557c1904d2107b872d
Aliaksey Kandratsenka <alk@tut.by>
2011-12-19 15:22:32 +0300

    don't preload images from no-preload directory
    Sometimes it's one-off images for setup wizard, for which there's no
    need to preload anything.
    
    Change-Id: I0a5d26d750e73d890cdbcb1499a65f5c4eefdfda
    Reviewed-on: http://review.couchbase.org/11758
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>

--- scripts/build-all-images.rb
+++ scripts/build-all-images.rb
@@ -3,5 +3,5 @@
 Dir.chdir(File.join(File.dirname(__FILE__), "..", "priv/public"))
 
 print "var AllImages = ["
-print(Dir["images/**/*"].select {|p| File.file? p}.map {|p| p.inspect}.join(", "))
+print(Dir["images/**/*"].select {|p| p !~ /no-preload/ && File.file?(p)}.map {|p| p.inspect}.join(", "))
 puts "];"
