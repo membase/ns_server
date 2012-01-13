e4a735442360d45a821f30d9b0fe350bbf75f279
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2012-01-12 22:55:19 -0800

    Split single_bucket_sup into two supervisors.
    So now there is a separate supervisor for ns_memcached and all the
    processes that depend on it. On the other hand capi_set_view_manager
    and capi_ddoc_replication_srv are independent of each other and
    ns_memcached_sup. Thus it's valid to use one_for_one restart
    strategy. With this change some transient crash in, for instance,
    capi_set_view_manager does not compel stats_reader and friends to
    restart.
    
    Change-Id: I32200f3bda241ddb2d6fa7999a106c9b75df9402
    Reviewed-on: http://review.couchbase.org/12330
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

---
+++ src/ns_memcached_sup.erl
@@ -0,0 +1,52 @@
+%% @author Couchbase <info@couchbase.com>
+%% @copyright 2012 Couchbase, Inc.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%      http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(ns_memcached_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/1]).
+-export([init/1]).
+
+start_link(BucketName) ->
+    Name = list_to_atom(?MODULE_STRING ++ "-" ++ BucketName),
+    supervisor:start_link({local, Name}, ?MODULE, [BucketName]).
+
+child_specs(BucketName) ->
+    [{{ns_memcached, stats, BucketName}, {ns_memcached, start_link,
+                                          [{BucketName, stats}]},
+      %% ns_memcached waits for the bucket to sync to disk before exiting
+      permanent, 86400000, worker, [ns_memcached]},
+     {{ns_memcached, data, BucketName}, {ns_memcached, start_link,
+                                         [{BucketName, data}]},
+      %% ns_memcached waits for the bucket to sync to disk before exiting
+      permanent, 86400000, worker, [ns_memcached]},
+     {{ns_vbm_sup, BucketName}, {ns_vbm_sup, start_link, [BucketName]},
+      permanent, 1000, worker, [ns_vbm_sup]},
+     {{stats_collector, BucketName}, {stats_collector, start_link, [BucketName]},
+      permanent, 1000, worker, [stats_collector]},
+     {{stats_archiver, BucketName}, {stats_archiver, start_link, [BucketName]},
+      permanent, 1000, worker, [stats_archiver]},
+     {{stats_reader, BucketName}, {stats_reader, start_link, [BucketName]},
+      permanent, 1000, worker, [stats_reader]},
+     {{failover_safeness_level, BucketName},
+      {failover_safeness_level, start_link, [BucketName]},
+      permanent, 1000, worker, [failover_safeness_level]}].
+
+init([BucketName]) ->
+    {ok, {{rest_for_one,
+           misc:get_env_default(max_r, 3),
+           misc:get_env_default(max_t, 10)},
+          child_specs(BucketName)}}.
--- src/single_bucket_sup.erl
+++ src/single_bucket_sup.erl
@@ -34,34 +34,21 @@ top_loop(ParentPid, Pid, BucketName) ->
     end.
 
 child_specs(BucketName) ->
-    [{{ns_memcached, stats, BucketName}, {ns_memcached, start_link,
-                                          [{BucketName, stats}]},
-      % ns_memcached waits for the bucket to sync to disk before exiting
-      permanent, 86400000, worker, [ns_memcached]},
-     {{ns_memcached, data, BucketName}, {ns_memcached, start_link,
-                                         [{BucketName, data}]},
-      % ns_memcached waits for the bucket to sync to disk before exiting
-      permanent, 86400000, worker, [ns_memcached]},
-     {{ns_vbm_sup, BucketName}, {ns_vbm_sup, start_link, [BucketName]},
-      permanent, 1000, worker, [ns_vbm_sup]},
+    [{{ns_memcached_sup, BucketName},
+      {ns_memcached_sup, start_link, [BucketName]},
+      %% ns_memcached can take a long time to terminate; but it will do it in
+      %% a finite amount of time since all the shutdown timeouts in
+      %% ns_memcached_sup are finite
+      permanent, infinity, supervisor, [ns_memcached_sup]},
      {{capi_ddoc_replication_srv, BucketName},
       {capi_ddoc_replication_srv, start_link, [BucketName]},
       permanent, 1000, worker, [capi_ddoc_replication_srv]},
      {{capi_set_view_manager, BucketName},
       {capi_set_view_manager, start_link, [BucketName]},
-      permanent, 1000, worker, [capi_set_view_manager]},
-     {{stats_collector, BucketName}, {stats_collector, start_link, [BucketName]},
-      permanent, 1000, worker, [stats_collector]},
-     {{stats_archiver, BucketName}, {stats_archiver, start_link, [BucketName]},
-      permanent, 1000, worker, [stats_archiver]},
-     {{stats_reader, BucketName}, {stats_reader, start_link, [BucketName]},
-      permanent, 1000, worker, [stats_reader]},
-     {{failover_safeness_level, BucketName},
-      {failover_safeness_level, start_link, [BucketName]},
-      permanent, 1000, worker, [failover_safeness_level]}].
+      permanent, 1000, worker, [capi_set_view_manager]}].
 
 init([BucketName]) ->
-    {ok, {{rest_for_one,
+    {ok, {{one_for_one,
            misc:get_env_default(max_r, 3),
            misc:get_env_default(max_t, 10)},
           child_specs(BucketName)}}.
