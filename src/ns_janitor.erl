8064704422e25270e0cf470e67f1e7c2805c84c8
Aliaksey Kandratsenka <alk@tut.by>
2012-01-13 19:40:58 +0300

    [bp] moved vbucket map visualization code out of janitor
    Originally reviewed on http://review.couchbase.org/12346
    
    Change-Id: I80817673927efff3d86a442f56ddbd85f5316e48
    Reviewed-on: http://review.couchbase.org/13953
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/ns_janitor.erl
+++ src/ns_janitor.erl
@@ -21,7 +21,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([cleanup/2, current_states/2, graphviz/1]).
+-export([cleanup/2, current_states/2]).
 
 -define(WAIT_FOR_MEMCACHED_TRIES, 5).
 
@@ -92,60 +92,6 @@ do_cleanup(Bucket, Options, Config) ->
                     ok
             end
     end.
-
-
-state_color(active) ->
-    "color=green";
-state_color(pending) ->
-    "color=blue";
-state_color(replica) ->
-    "color=yellow";
-state_color(dead) ->
-    "color=red".
-
-
--spec node_vbuckets(non_neg_integer(), node(), [{node(), vbucket_id(),
-                                                 vbucket_state()}], map()) ->
-                           iolist().
-node_vbuckets(I, Node, States, Map) ->
-    GState = lists:keysort(1,
-               [{VBucket, state_color(State)} || {N, VBucket, State} <- States,
-                                                 N == Node]),
-    GMap = [{VBucket, "color=gray"} || {VBucket, Chain} <- misc:enumerate(Map, 0),
-                                         lists:member(Node, Chain)],
-    [io_lib:format("n~Bv~B [style=filled label=\"~B\" group=g~B ~s];~n",
-                   [I, V, V, V, Style]) ||
-        {V, Style} <- lists:ukeymerge(1, GState, GMap)].
-
-graphviz(Bucket) ->
-    {ok, Config} = ns_bucket:get_bucket(Bucket),
-    Map = proplists:get_value(map, Config, []),
-    Servers = proplists:get_value(servers, Config, []),
-    {ok, States, Zombies} = current_states(Servers, Bucket),
-    Nodes = lists:sort(Servers),
-    NodeColors = lists:map(fun (Node) ->
-                                   case lists:member(Node, Zombies) of
-                                       true -> {Node, "red"};
-                                       false -> {Node, "black"}
-                                   end
-                           end, Nodes),
-    SubGraphs = [io_lib:format("subgraph cluster_n~B {~ncolor=~s;~nlabel=\"~s\";~n~s}~n",
-                              [I, Color, Node, node_vbuckets(I, Node, States, Map)]) ||
-                    {I, {Node, Color}} <- misc:enumerate(NodeColors)],
-    Replicants = lists:sort(ns_bucket:map_to_replicas(Map)),
-    Replicators = lists:sort(ns_vbm_sup:replicators(Nodes, Bucket)),
-    AllRep = lists:umerge(Replicants, Replicators),
-    Edges = [io_lib:format("n~pv~B -> n~pv~B [color=~s];~n",
-                            [misc:position(Src, Nodes), V,
-                             misc:position(Dst, Nodes), V,
-                             case {lists:member(R, Replicants), lists:member(R, Replicators)} of
-                                 {true, true} -> "black";
-                                 {true, false} -> "red";
-                                 {false, true} -> "blue"
-                             end]) ||
-                R = {Src, Dst, V} <- AllRep],
-    ["digraph G { rankdir=LR; ranksep=6;", SubGraphs, Edges, "}"].
-
 
 -spec sanify(string(), map(), [atom()], [atom()]) -> map().
 sanify(Bucket, Map, Servers, DownNodes) ->
