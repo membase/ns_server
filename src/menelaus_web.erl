1595f2339cdd6630d6028a106a9376d75b7580d2
Aliaksey Kandratsenka <alk@tut.by>
2012-04-14 20:38:20 -0700

    MB-5307: menelaus_web:handle_streaming now eats all notifications
    I've noticed positive effect on CPU during rebalance because we have
    significant amount of config change due to vbucket map updates. And it
    makes sense to consume batch of changes before sending out updated
    stuff.
    
    Change-Id: If4e85f6d25367283226534c6e190030d7e1865e9
    Reviewed-on: http://review.couchbase.org/16773
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/menelaus_web.erl
+++ src/menelaus_web.erl
@@ -693,6 +693,14 @@ streaming_inner(F, HTTPRes, LastRes) ->
     end,
     Res.
 
+consume_watcher_notifies() ->
+    receive
+        {notify_watcher, _} ->
+            consume_watcher_notifies()
+    after 0 ->
+            ok
+    end.
+
 handle_streaming(F, Req, HTTPRes, LastRes) ->
     Res =
         try streaming_inner(F, HTTPRes, LastRes)
@@ -702,7 +710,10 @@ handle_streaming(F, Req, HTTPRes, LastRes) ->
                 exit(normal)
         end,
     receive
-        {notify_watcher, _} -> ok;
+        {notify_watcher, _} ->
+            timer:sleep(50),
+            consume_watcher_notifies(),
+            ok;
         _ ->
             ale:debug(?MENELAUS_LOGGER,
                       "menelaus_web streaming socket closed by client"),
