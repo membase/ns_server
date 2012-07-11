9186e8ea814c306d03f7de03d53539a1b93c196f
Junyi Xie <junyi.couchbase@gmail.com>
2012-07-10 19:25:03 -0700

    CBD-399: refactoring capi_replication code
    This change-set simplifies capi_replication code used by XDCR.
    It also removes some unused code from retired couchdb replicator.
    
    Change-Id: I7de8c448ccb8f82cdd2772dafc8019ca1a623971
    Reviewed-on: http://review.couchbase.org/18193
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Junyi Xie <junyi.couchbase@gmail.com>

--- src/capi_replication.erl
+++ src/capi_replication.erl
@@ -18,32 +18,11 @@
 
 -export([get_missing_revs/2, update_replicated_docs/3]).
 
--include("couch_db.hrl").
--include("mc_entry.hrl").
--include("mc_constants.hrl").
+-include("xdc_replicator.hrl").
 
-get_missing_revs(#db{name = BucketBin,
-                     filepath = undefined}, JsonDocIdRevs) ->
-    Bucket = binary_to_list(BucketBin),
-
-    Results =
-        lists:foldr(
-          fun ({Id, Rev}, Acc) ->
-                  {VBucket, _Node} = cb_util:vbucket_from_id(Bucket, Id),
-
-                  case is_missing_rev(Bucket, VBucket, Id, Rev) of
-                      false ->
-                          Acc;
-                      true ->
-                          [{Id, Rev} | Acc]
-                  end;
-              (_, _) ->
-                  throw(unsupported)
-          end, [], JsonDocIdRevs),
-    {ok, Results};
+%% public functions
 get_missing_revs(#db{name = DbName}, JsonDocIdRevs) ->
     {Bucket, VBucket} = capi_utils:split_dbname(DbName),
-
     Results =
         lists:foldr(
           fun ({Id, Rev}, Acc) ->
@@ -56,68 +35,14 @@ get_missing_revs(#db{name = DbName}, JsonDocIdRevs) ->
               (_, _) ->
                   throw(unsupported)
           end, [], JsonDocIdRevs),
+
+    NumCandidates = length(JsonDocIdRevs),
+    RemoteWinners = length(Results),
+    ?xdcr_debug("after conflict resolution for ~p docs, num of remote winners is ~p and "
+                "number of local winners is ~p.",
+                [NumCandidates, RemoteWinners, (NumCandidates-RemoteWinners)]),
     {ok, Results}.
 
-is_missing_rev(Bucket, VBucket, Id, RemoteMeta) ->
-     case capi_utils:get_meta(Bucket, VBucket, Id) of
-         {error, enoent, _CAS} ->
-             true;
-         {error, not_my_vbucket} ->
-             throw({bad_request, not_my_vbucket});
-         {ok, LocalMeta, _Deleted, _Props} ->
-             %% we do not have any information about deletedness of
-             %% the remote side thus we use only revisions to
-             %% determine a winner
-             case max(LocalMeta, RemoteMeta) of
-                 %% if equal, prefer LocalMeta since in this case, no need
-                 %% to replicate the remote item, hence put LocalMeta before
-                 %% RemoteMeta.
-                 LocalMeta ->
-                     false;
-                 RemoteMeta ->
-                     true
-             end
-     end.
-
-
-%% In case of one or more errors, just return the first one. Otherwise,
-%% return ok. Also notice that in case of error, we return {ok, Error}. This is
-%% per the Couch's update_docs() semantics.
-make_return_tuple({ok, Errors}) ->
-    case Errors of
-        [] ->
-            ok;
-        [Error | _] ->
-            {ok, Error}
-    end.
-
-update_replicated_docs(#db{name = BucketBin,
-                           filepath = undefined},
-                       Docs, Options) ->
-    Bucket = binary_to_list(BucketBin),
-
-    case proplists:get_value(all_or_nothing, Options, false) of
-        true ->
-            throw(unsupported);
-        false ->
-            ok
-    end,
-
-    Errors =
-        lists:foldr(
-          fun (#doc{id = Id, rev = Rev} = Doc, ErrorsAcc) ->
-                  {VBucket, _Node} = cb_util:vbucket_from_id(Bucket, Id),
-
-                  case do_update_replicated_doc(Bucket, VBucket, Doc) of
-                      ok ->
-                          ErrorsAcc;
-                      {error, Error} ->
-                          [{{Id, Rev}, Error} | ErrorsAcc]
-                  end
-          end,
-          [], Docs),
-
-    make_return_tuple({ok, Errors});
 update_replicated_docs(#db{name = DbName}, Docs, Options) ->
     {Bucket, VBucket} = capi_utils:split_dbname(DbName),
 
@@ -131,7 +56,7 @@ update_replicated_docs(#db{name = DbName}, Docs, Options) ->
     Errors =
         lists:foldr(
           fun (#doc{id = Id, rev = Rev} = Doc, ErrorsAcc) ->
-                  case do_update_replicated_doc(Bucket, VBucket, Doc) of
+                  case do_update_replicated_doc_loop(Bucket, VBucket, Doc) of
                       ok ->
                           ErrorsAcc;
                       {error, Error} ->
@@ -142,23 +67,32 @@ update_replicated_docs(#db{name = DbName}, Docs, Options) ->
 
     make_return_tuple({ok, Errors}).
 
-do_update_replicated_doc(_Bucket, _VBucket,
-                         #doc{id = <<?LOCAL_DOC_PREFIX, _/binary>>}) ->
-    ok;
-do_update_replicated_doc(Bucket, VBucket,
-                         #doc{id = Id, rev = Rev,
-                              body = Value0, deleted = Deleted} = _Doc) ->
-    case Value0 of
-    {_} ->
-        Value = ?JSON_ENCODE(filter_out_mccouch_fields(Value0));
-    _ ->
-        Value = Value0
-    end,
-    do_update_replicated_doc_loop(Bucket, VBucket, Id, Rev, Value, Deleted).
+%% helper functions
+is_missing_rev(Bucket, VBucket, Id, RemoteMeta) ->
+    case capi_utils:get_meta(Bucket, VBucket, Id) of
+        {error, enoent, _CAS} ->
+            true;
+        {error, not_my_vbucket} ->
+            throw({bad_request, not_my_vbucket});
+        {ok, LocalMeta, _Deleted, _Props} ->
+            %% we do not have any information about deletedness of
+            %% the remote side thus we use only revisions to
+            %% determine a winner
+            case max(LocalMeta, RemoteMeta) of
+                %% if equal, prefer LocalMeta since in this case, no need
+                %% to replicate the remote item, hence put LocalMeta before
+                %% RemoteMeta.
+                LocalMeta ->
+                    false;
+                RemoteMeta ->
+                    true
+            end
+    end.
 
-do_update_replicated_doc_loop(Bucket, VBucket, DocId,
-                              {DocSeqNo, DocRevId} = DocRev,
-                              DocValue, DocDeleted) ->
+do_update_replicated_doc_loop(Bucket, VBucket,
+                              #doc{id = DocId, rev = DocRev,
+                                   body = DocValue, deleted = DocDeleted} = Doc) ->
+    {DocSeqNo, DocRevId} = DocRev,
     RV =
         case capi_utils:get_meta(Bucket, VBucket, DocId) of
             {error, enoent, CAS} ->
@@ -199,12 +133,24 @@ do_update_replicated_doc_loop(Bucket, VBucket, DocId,
 
     case RV of
         retry ->
-            do_update_replicated_doc_loop(Bucket, VBucket, DocId,
-                                          DocRev, DocValue, DocDeleted);
+            do_update_replicated_doc_loop(Bucket, VBucket, Doc);
         _Other ->
             RV
     end.
 
+%% In case of one or more errors, just return the first one. Otherwise,
+%% return ok. Also notice that in case of error, we return {ok, Error}. This is
+%% per the Couch's update_docs() semantics.
+make_return_tuple({ok, Errors}) ->
+    case Errors of
+        [] ->
+            ok;
+        [Error | _] ->
+            {ok, Error}
+    end.
+
+
+%% ep_engine operation functions
 do_set_with_meta(Bucket, DocId, VBucket, DocValue, DocRev, CAS) ->
     case ns_memcached:set_with_meta(Bucket, DocId,
                                     VBucket, DocValue,
@@ -234,11 +180,3 @@ do_delete_with_meta(Bucket, DocId, VBucket, DocRev, CAS) ->
             {error, {bad_request, einval}}
     end.
 
-filter_out_mccouch_fields({Props}) ->
-    FilteredProps = lists:filter(
-                      fun ({<<$$, _/binary>>, _Value}) ->
-                              false;
-                          ({_, _}) ->
-                              true
-                      end, Props),
-    {FilteredProps}.
