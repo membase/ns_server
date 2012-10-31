9b04199559fe2159f398b2bd3beed22ba0ee477c
Aliaksey Kandratsenka <alk@tut.by>
2012-10-31 15:26:07 -0700

    MB-5308: fixed indentation in default spatial map function
    Closing if brace was not properly indented.
    
    Change-Id: Ifa7f78978d825a419f4ad91ad20b3c2f085c22ed
    Reviewed-on: http://review.couchbase.org/22183
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- priv/public/js/views.js
+++ priv/public/js/views.js
@@ -1613,7 +1613,7 @@ var ViewsSection = {
         return callback("conflict", "already_exists", "Spatial Function with given name already exists");
       }
       spatial[spatialName] = spatialDef ||
-          "function (doc) {\n  if (doc.geometry) {\n    emit(doc.geometry, null);\n}\n}";
+          "function (doc) {\n  if (doc.geometry) {\n    emit(doc.geometry, null);\n  }\n}";
 
       $('#spatial_code_errors').text('').attr('title','');
 
