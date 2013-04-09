765672bf9d081ac74318b833346c52767a853091
Aliaksey Kandratsenka <alk@tut.by>
2013-04-09 12:32:05 -0700

    MB-100: feed binaries to ns_log for some memory savings
    I've seen some avalance of crashes that we log into ns_log,
    overloading ns_log quite easily. Part of that was some uneeded error
    logging to standard output which is fixed in another commit. But I
    also saw that ns_log doesn't store it's messages very
    efficiently. I.e. it's using stringish-iolists (mostly lists of chars
    with occasional atoms). Clearly, converting it to more efficient
    iolist is a win.
    
    Change-Id: Iabdd2aff36c66ed56400120fa775e723b4b1810b
    Reviewed-on: http://review.couchbase.org/25574
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/ns_log_sink.erl
+++ src/ns_log_sink.erl
@@ -63,14 +63,20 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+pre_format_msg(Msg) ->
+    %% our code demands msg to be a list, but it's more efficient to
+    %% store and pass around binaries rather than mostly strings (but
+    %% in fact iolists)
+    [iolist_to_binary(Msg)].
+
 do_log(#log_info{loglevel=LogLevel, time=Time, module=Module,
                  node=Node, user_data=undefined} = _Info, Msg) ->
     Category = loglevel_to_category(LogLevel),
-    ns_log:log(Module, Node, Time, Category, Msg, []);
+    ns_log:log(Module, Node, Time, Category, pre_format_msg(Msg), []);
 do_log(#log_info{loglevel=LogLevel, time=Time,
                  node=Node, user_data={Module, Code}} = _Info, Msg) ->
     Category = loglevel_to_category(LogLevel),
-    ns_log:log(Module, Node, Time, Code, Category, Msg, []).
+    ns_log:log(Module, Node, Time, Code, Category, pre_format_msg(Msg), []).
 
 -spec loglevel_to_category(loglevel()) -> log_classification().
 loglevel_to_category(debug) ->
