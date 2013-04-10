2016128dd127e8096c22adf7d9be55d200948966
Aliaksey Kandratsenka <alk@tut.by>
2013-04-10 10:01:25 -0700

    MB-8056: log reason of kill_memcached failures
    Change-Id: I7c94a878b8ef765d651db81c52c7f41d13684f36
    Reviewed-on: http://review.couchbase.org/25595
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/ns_server_testrunner_api.erl
+++ src/ns_server_testrunner_api.erl
@@ -1,14 +1,22 @@
 -module(ns_server_testrunner_api).
 
+-include("ns_common.hrl").
+
 -compile(export_all).
 
 restart_memcached(Timeout) ->
     {ok, _} = rpc:call(ns_server:get_babysitter_node(), ns_child_ports_sup, restart_port_by_name, [memcached], Timeout).
 
 kill_memcached(Timeout) ->
-    Pid = rpc:call(ns_server:get_babysitter_node(), ns_child_ports_sup, find_port, [memcached]),
-    Pid ! {send_to_port, <<"die!\n">>},
-    ok = misc:wait_for_process(Pid, Timeout).
+    try
+        Pid = rpc:call(ns_server:get_babysitter_node(), ns_child_ports_sup, find_port, [memcached]),
+        Pid ! {send_to_port, <<"die!\n">>},
+        ok = misc:wait_for_process(Pid, Timeout)
+    catch E:T ->
+            ST = erlang:get_stacktrace(),
+            ?log_error("Got exception in kill_memcached: ~p~n~p", [{E,T}, ST]),
+            erlang:raise(E, T, ST)
+    end.
 
 eval_string(String) ->
     {value, Value, _} = eshell:eval(String, erl_eval:new_bindings()),
