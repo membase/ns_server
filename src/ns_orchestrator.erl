d59c9eacfb4d42e529f8c75e3025b231ac5968a3
Aliaksey Kandratsenka <alk@tut.by>
2013-04-10 17:12:06 -0700

    MB-100: don't consider failed over nodes for recovery
    It was pointed out that previous behavior of taking all nodes is not
    very desirable. We now take all nodes except failed over (and not yet
    added back).
    
    Change-Id: I8dbf1c2c7d7bf80df3dabfa39a2a614474c1a17e
    Reviewed-on: http://review.couchbase.org/25611
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- src/ns_orchestrator.erl
+++ src/ns_orchestrator.erl
@@ -639,7 +639,8 @@ idle({start_recovery, Bucket}, _From, State) ->
                 throw(not_needed)
         end,
 
-        Servers = ns_node_disco:nodes_wanted(),
+        FailedOverNodes = [N || {N, inactiveFailed} <- ns_cluster_membership:get_nodes_cluster_membership()],
+        Servers = ns_node_disco:nodes_wanted() -- FailedOverNodes,
         BucketConfig = misc:update_proplist(BucketConfig0, [{servers, Servers}]),
         ns_cluster_membership:activate(Servers),
         ns_config:sync_announcements(),
