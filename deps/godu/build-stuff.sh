68fddcc88f69c176cd40552fc753d50c3cc9f04c
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2013-08-02 12:03:37 -0700

    CBD-1061 Build linux version of godu with build-id.
    Change-Id: I8a5886660025b1821026d519574c7ed09003c999
    Reviewed-on: http://review.couchbase.org/27898
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Philip Labee <philip@couchbase.com>

--- deps/godu/build-stuff.sh
+++ deps/godu/build-stuff.sh
@@ -10,4 +10,4 @@ set -x
 
 GOOS=windows GOARCH=386 go build -o ../../priv/i386-win32-godu.exe || exit $?
 
-GOOS=linux GOARCH=386 go build -o ../../priv/i386-linux-godu || exit $?
+GOOS=linux GOARCH=386 go build -ldflags "-B 0x$(sed -e 's/-//g' /proc/sys/kernel/random/uuid)" -o ../../priv/i386-linux-godu || exit $?
