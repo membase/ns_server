487089cdf448f3ea94b423a8f86cfe2e5f23d7af
Aliaksey Kandratsenka <alk@tut.by>
2013-11-21 20:00:00 -0800

    MB-9339: added pooled_memcached_client and memcached_clients_pool
    Change-Id: I64473163306b12fb9a1b5ef1fba33d841801da86

---
+++ src/memcached_clients_pool.erl
@@ -0,0 +1,55 @@
+-module(memcached_clients_pool).
+
+-include("ns_common.hrl").
+-include("remote_clusters_info.hrl").
+
+-export([start_link/0,
+         make_loc_by_remote_cluster_info_ref/3,
+         make_loc/4]).
+
+-export([take_socket/1, put_socket/2]).
+
+start_link() ->
+    Options = [{name, memcached_clients_pool},
+               {connection_timeout, 30000},
+               {pool_size, 200}],
+    ns_connection_pool:start_link(Options).
+
+take_socket({?MODULE, {Host, Port, Bucket, Auth}}) ->
+    case ns_connection_pool:maybe_take_socket(memcached_clients_pool, {Host, Port, Bucket}) of
+        {ok, _S} = Ok ->
+            Ok;
+        no_socket ->
+            establish_connection(Host, Port, Bucket, Auth)
+    end.
+
+establish_connection(Host, Port, Bucket, Password) ->
+    case gen_tcp:connect(Host, Port, [binary, {packet, 0}, {active, false}]) of
+        {ok, S} ->
+            case mc_client_binary:auth(S, {<<"PLAIN">>,
+                                           {list_to_binary(Bucket),
+                                            list_to_binary(Password)}}) of
+                ok ->
+                    {ok, S};
+                {memcached_error, _, _} = McdError ->
+                    {error, {auth_failed, McdError}}
+            end;
+        {error, _ } = Error ->
+            Error
+    end.
+
+put_socket(Socket, {?MODULE, {Host, Port, Bucket, _Auth}}) ->
+    ns_connection_pool:put_socket(memcached_clients_pool, {Host, Port, Bucket}, Socket).
+
+make_loc_by_remote_cluster_info_ref(TargetRef, Through, VBucket) ->
+    case remote_clusters_info:get_memcached_vbucket_info_by_ref(TargetRef, Through, VBucket) of
+        {ok, {Host, Port}, BucketInfo} ->
+            {ok, {_ClusterUUID, BucketName}} = remote_clusters_info:parse_remote_bucket_reference(TargetRef),
+            Password = binary_to_list(BucketInfo#remote_bucket.password),
+            {ok, {?MODULE, {Host, Port, BucketName, Password}}};
+        Error ->
+            Error
+    end.
+
+make_loc(Host, Port, Bucket, Password) ->
+    {?MODULE, {Host, Port, Bucket, Password}}.
---
+++ src/pooled_memcached_client.erl
@@ -0,0 +1,140 @@
+-module(pooled_memcached_client).
+
+-include("ns_common.hrl").
+-include("mc_constants.hrl").
+-include("mc_entry.hrl").
+-include("couch_db.hrl").
+
+-export([find_missing_revs/3, bulk_set_metas/3]).
+
+-spec find_missing_revs(any(), vbucket_id(), [{binary(), rev()}]) ->
+                               {ok,
+                                [{binary(), rev()}],
+                                [{atom(), {binary(), rev()}}]}.
+find_missing_revs(DestRef, Vb, IdRevs) ->
+    execute(DestRef, fun find_missing_revs_inner/3, [Vb, IdRevs]).
+
+find_missing_revs_inner(S, Vb, IdRevs) ->
+    SenderPid = spawn_link(
+                  fun () ->
+                          [begin
+                               H = #mc_header{vbucket = Vb,
+                                              opcode = ?CMD_GET_META},
+                               E = #mc_entry{key = Key},
+                               ok = mc_binary:send(S, req, H, E)
+                           end || {Key, _Rev} <- IdRevs],
+                          ok
+                  end),
+    RV = fetch_missing_revs_loop(S, IdRevs, [], []),
+    erlang:unlink(SenderPid),
+    erlang:exit(SenderPid, kill),
+    misc:wait_for_process(SenderPid, infinity),
+    RV.
+
+fetch_missing_revs_loop(_S, [], Acc, AccErr) ->
+    {ok, lists:reverse(Acc), lists:reverse(AccErr)};
+fetch_missing_revs_loop(S, [{_Key, Rev} = Pair | Rest], Acc, AccErr) ->
+    Missing =
+        case mc_binary:recv(S, res, infinity) of
+            %% get meta of key successfully
+            {ok, #mc_header{status=?SUCCESS}, #mc_entry{ext = Ext, cas = CAS}} ->
+                <<_MetaFlags:32/big, ItemFlags:32/big,
+                  Expiration:32/big, SeqNo:64/big>> = Ext,
+                RevId = <<CAS:64/big, Expiration:32/big, ItemFlags:32/big>>,
+                RemoteRev = {SeqNo, RevId},
+                RemoteRev < Rev;
+            %% key not found, which is Ok if replicating new items
+            {ok, #mc_header{status=?KEY_ENOENT}, _} ->
+                true;
+            %% unexpected response returned by remote memcached, treat it as error
+            {ok, #mc_header{status=OtherResponse}, _} ->
+                {error, mc_client_binary:map_status(OtherResponse)}
+        end,
+    case Missing of
+        {error, Err} ->
+            fetch_missing_revs_loop(S, Rest, Acc, [{Err, Pair} | AccErr]);
+        _Bool ->
+            NewAcc = if Missing -> [Pair | Acc];
+                        true -> Acc
+                     end,
+            fetch_missing_revs_loop(S, Rest, NewAcc, AccErr)
+    end.
+
+
+bulk_set_metas(_DestRef, _Vb, [] = _DocsList) ->
+    {ok, []};
+bulk_set_metas(DestRef, Vb, DocsList) ->
+    execute(DestRef, fun bulk_set_metas_inner/3, [Vb, DocsList]).
+
+bulk_set_metas_inner(S, Vb, DocsList) ->
+    RecverPid = erlang:spawn_link(erlang, apply, [fun bulk_set_metas_recv_replies/3, [S, self(), length(DocsList)]]),
+    [ok = send_single_set_meta(S, Vb, Doc) || Doc <- DocsList],
+    receive
+        {RecverPid, RV} ->
+            {ok, RV}
+    end.
+
+send_single_set_meta(S, Vb,
+                     #doc{id = Key, rev = Rev, deleted = Deleted,
+                          body = DocValue}) ->
+    {OpCode, Data} = case Deleted of
+                         true ->
+                             {?CMD_DEL_WITH_META, <<>>};
+                         _ ->
+                             {?CMD_SET_WITH_META, DocValue}
+                     end,
+    Ext = mc_client_binary:rev_to_mcd_ext(Rev),
+    McHeader = #mc_header{vbucket = Vb, opcode = OpCode},
+    %% CAS does not matter since remote ep_engine has capability
+    %% to do getMeta internally before doing setWithMeta or delWithMeta
+    CAS  = 0,
+    McBody = #mc_entry{key = Key, data = Data, ext = Ext, cas = CAS},
+    mc_binary:send(S, req, McHeader, McBody).
+
+bulk_set_metas_recv_replies(S, Parent, Count) ->
+    RVs = bulk_set_metas_replies_loop(S, Count, []),
+    Parent ! {self(), RVs}.
+
+bulk_set_metas_replies_loop(_S, 0, Acc) ->
+    lists:reverse(Acc);
+bulk_set_metas_replies_loop(S, Count, Acc) ->
+    case mc_binary:recv(S, res, infinity) of
+        {ok, #mc_header{status=Status}, _} ->
+            NewAcc = [mc_client_binary:map_status(Status) | Acc],
+            bulk_set_metas_replies_loop(S, Count - 1, NewAcc)
+    end.
+
+execute(DestRef, Body, Args) ->
+    Parent = self(),
+    Pid =
+        proc_lib:spawn_link(
+          fun () ->
+                  RV = execute_on_socket(DestRef, Body, Args),
+                  Parent ! {self(), RV}
+          end),
+    receive
+        {'EXIT', Pid, Reason} ->
+            ?log_error("Child memcached client process died: ~p", [Reason]),
+            erlang:exit(Reason);
+        {Pid, RV} ->
+            RV
+    end.
+
+execute_on_socket(DestRef, Body, Args) ->
+    case DestRef:take_socket() of
+        {ok, S} ->
+            execute_with_socket(S, Body, Args, DestRef);
+        {error, _} = Error ->
+            Error
+    end.
+
+execute_with_socket(S, Body, Args, DestRef) ->
+    RV = erlang:apply(Body, [S | Args]),
+    case RV of
+        {ok, _} ->
+            DestRef:put_socket(S),
+            RV;
+        Error ->
+            %% no need to close socket, it'll be autoclosed
+            Error
+    end.
