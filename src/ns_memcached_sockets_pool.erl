254eadef9b8d06045f9f69545c3ca487df4d2870
Artem Stemkovski <artem@couchbase.com>
2014-01-22 15:51:00 -0800

    bp:MB-100 use sockets pool for long blocking memcached calls
    so far it is just wait_for_checkpoint_persistence but there
    will be more of them
    
    Change-Id: Iaeca8739de6358e2309d07c04d2a9a18dca63be6
    Orig-Reviewed-on: http://review.couchbase.org/32748
    Reviewed-on: http://review.couchbase.org/34029
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

---
+++ src/ns_memcached_sockets_pool.erl
@@ -0,0 +1,55 @@
+%% @author Couchbase <info@couchbase.com>
+%% @copyright 2013 Couchbase, Inc
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%      http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% @doc pool of sockets that are used for long blocking memcached calls
+%%
+-module(ns_memcached_sockets_pool).
+
+-include("ns_common.hrl").
+
+-export([start_link/0]).
+
+-export([take_socket/1, put_socket/1]).
+
+start_link() ->
+    Options = [{name, ?MODULE},
+               {connection_timeout, 30000},
+               {pool_size, 10000}],
+    ns_connection_pool:start_link(Options).
+
+
+take_socket(Bucket) ->
+    Result =
+        case ns_connection_pool:maybe_take_socket(?MODULE, ns_memcached) of
+            {ok, Sock} ->
+                {ok, Sock};
+            no_socket ->
+                ns_memcached:connect()
+        end,
+
+    case Result of
+        {ok, Socket} ->
+            case mc_client_binary:select_bucket(Socket, Bucket) of
+                ok ->
+                    {ok, Socket};
+                Err ->
+                    {error, {select_bucket_failed, Err}}
+            end;
+        Error ->
+            Error
+    end.
+
+put_socket(Socket) ->
+    ns_connection_pool:put_socket(?MODULE, ns_memcached, Socket).
