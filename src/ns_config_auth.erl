776f2be396d9b73e81b8de3dc5b7c775d4fbf42f
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-03-27 14:36:12 -0700

    Use erlang:apply in hash_password to silence deprecation warning.
    Change-Id: Id12c0442158b4da28637637b0e2e285b8a051d51
    Reviewed-on: http://review.couchbase.org/35016
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>

--- src/ns_config_auth.erl
+++ src/ns_config_auth.erl
@@ -160,9 +160,12 @@ hash_password(Password) ->
     {Salt, hash_password(Salt, Password)}.
 
 hash_password(Salt, Password) ->
-    case erlang:system_info(otp_release) < "R16" of
-        true ->
-            crypto:sha_mac(Salt, list_to_binary(Password));
-        false ->
-            crypto:hmac(sha, Salt, list_to_binary(Password))
-    end.
+    {F, A} =
+        case erlang:system_info(otp_release) < "R16" of
+            true ->
+                {sha_mac, [Salt, list_to_binary(Password)]};
+            false ->
+                {hmac, [sha, Salt, list_to_binary(Password)]}
+        end,
+
+    erlang:apply(crypto, F, A).
