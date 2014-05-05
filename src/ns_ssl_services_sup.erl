4d64f7e4dc749778d46149a24d77e8bb111c1528
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-05-05 15:56:52 -0700

    Revert "MB-10953 Clear ssl PEM cache after saving certificates."
    This reverts commit ad9ff254ce8414aeba55fceca1c472da9cff9412.
    
    Because ssl_manager:clear_pem_cache is not available in r14b04.
    
    Change-Id: I31f47be97a7d6acf53df87bfb548cf5a5b4b897e
    Reviewed-on: http://review.couchbase.org/36726
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/ns_ssl_services_sup.erl
+++ src/ns_ssl_services_sup.erl
@@ -4,7 +4,8 @@
 
 -include("ns_common.hrl").
 
--export([init/1, start_link/0, restart_ssl_services/0]).
+-export([init/1, start_link/0,
+         stop_ssl_services/0, start_ssl_services/0]).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
@@ -15,9 +16,14 @@ init([]) ->
            misc:get_env_default(max_t, 10)},
           child_specs()}}.
 
-restart_ssl_services() ->
+stop_ssl_services() ->
     [begin
-         ok = supervisor2:terminate_child(?MODULE, Child),
+         ok = supervisor2:terminate_child(?MODULE, Child)
+     end || Child <- [ns_rest_ssl_service, ns_capi_ssl_service]],
+    ok.
+
+start_ssl_services() ->
+    [begin
          {ok, _} = supervisor2:restart_child(?MODULE, Child)
      end || Child <- [ns_rest_ssl_service, ns_capi_ssl_service]],
     ok.
