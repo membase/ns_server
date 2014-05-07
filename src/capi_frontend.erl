31e9e848b9acf0117389596e643cdb93ded70cf7
Aliaksey Kandratsenka <alk@tut.by>
2014-05-07 14:58:27 -0700

    MB-9915: extracted send_no_active_vbuckets to capi_frontend
    For later reuse
    
    Change-Id: Ia4a66510cfbe937186cc0054c98899c157f98ab4
    Reviewed-on: http://review.couchbase.org/36804
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/capi_frontend.erl
+++ src/capi_frontend.erl
@@ -85,6 +85,38 @@ continue_do_db_req(#httpd{user_ctx=UserCtx,
             couch_db_frontend:do_db_req(Req1, Fun)
     end.
 
+find_node_with_vbuckets(BucketBin) ->
+    Bucket = erlang:binary_to_list(BucketBin),
+    VBucketsDict = vbucket_map_mirror:node_vbuckets_dict(Bucket),
+    Nodes = dict:fetch_keys(VBucketsDict),
+    Len = erlang:length(Nodes),
+    case Len of
+        0 ->
+            undefined;
+        _ ->
+            random:seed(erlang:now()),
+            lists:nth(random:uniform(Len), Nodes)
+    end.
+
+send_no_active_vbuckets(CouchReq, Bucket0) ->
+    Req = CouchReq#httpd.mochi_req,
+    Bucket = iolist_to_binary(Bucket0),
+    LocalAddr = menelaus_util:local_addr(Req),
+    Headers0 = [{"Content-Type", "application/json"},
+                {"Cache-Control", "must-revalidate"}],
+    RedirectNode = find_node_with_vbuckets(Bucket),
+    Headers = case RedirectNode of
+                  undefined -> Headers0;
+                  _ ->
+                      Path = erlang:iolist_to_binary(Req:get(raw_path)),
+                      [{"Location", capi_utils:capi_url_bin(RedirectNode, Path, LocalAddr)}
+                       | Headers0]
+              end,
+    Tuple = {302,
+             Headers,
+             <<"{\"error\":\"no_active_vbuckets\",\"reason\":\"Cannot execute view query since the node has no active vbuckets\"}">>},
+    {ok, Req:respond(Tuple)}.
+
 verify_bucket_auth(#httpd{mochi_req=MochiReq}, BucketName) ->
     ListBucketName = ?b2l(BucketName),
     BucketConfig = case ns_bucket:get_bucket_light(ListBucketName) of
