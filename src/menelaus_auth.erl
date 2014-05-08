38dc7ad0f58febf31b07aacd1e271a23521ff8c8
Aliaksey Kandratsenka <alk@tut.by>
2014-05-07 17:16:35 -0700

    MB-10942: only allow non-GETs token auth for UI
    Our UI always adds header to it's requests. Which CSRF is unable to
    duplicate.
    
    So we still do UI auth via cookie. Which is super-convenient
    (particularly for /diag link in UI). And we cover our ass from CSRF by
    requiring that all non-GET requests (only type of requests that are
    vulnerable to CSRF) have ui header added.
    
    Change-Id: I48098dab04855109f270e2a01809cd5f726ebe8b
    Reviewed-on: http://review.couchbase.org/36814
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/menelaus_auth.erl
+++ src/menelaus_auth.erl
@@ -230,9 +230,15 @@ extract_auth(Req) ->
                 "Basic " ++ Value ->
                     parse_user_password(base64:decode_to_string(Value));
                 _ ->
-                    case extract_ui_auth_token(Req) of
-                        undefined -> undefined;
-                        Token -> {token, Token}
+                    Method = Req:get(method),
+                    case Method =:= 'GET' orelse Method =:= 'HEAD' of
+                        true ->
+                            case extract_ui_auth_token(Req) of
+                                undefined -> undefined;
+                                Token -> {token, Token}
+                            end;
+                        _ ->
+                            undefined
                     end
             end
     end.
