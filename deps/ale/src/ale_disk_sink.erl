59eb702fe0547805560f1115e622bbaa1aeb403d
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-05-28 14:53:50 -0700

    [ale] Reimplement disk sink without using disk_log.
    Rotation is currently not supported. And sync can still block callers.
    
    Change-Id: Ifa894debf30650a7daf95cdd4e963bf69b2f7b49
    Reviewed-on: http://review.couchbase.org/37746
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- deps/ale/src/ale_disk_sink.erl
+++ deps/ale/src/ale_disk_sink.erl
@@ -26,70 +26,115 @@
 
 -include("ale.hrl").
 
--record(state, {name      :: atom(),
-                path      :: string()}).
+-record(state, {
+          path :: string(),
+          file :: file:io_device(),
+          buffer :: binary(),
+          buffer_size :: integer(),
+          flush_timer :: undefined | reference(),
 
--define(DEFAULT_MAX_SIZE, 10 * 1024 * 1024).
--define(DEFAULT_MAX_FILES, 10).
+          batch_size :: pos_integer(),
+          batch_timeout :: pos_integer()
+         }).
 
--define(DISK_LOG_DEFAULTS, [{format, external},
-                            {type, wrap},
-                            {size, {?DEFAULT_MAX_SIZE, ?DEFAULT_MAX_FILES}}]).
-
--spec start_link(atom(), string()) -> {ok, pid()} | ignore | {error, any()}.
 start_link(Name, Path) ->
     start_link(Name, Path, []).
 
--spec start_link(atom(), string(),
-                 [{size, {pos_integer(), pos_integer()} | infinity}]) ->
-                        {ok, pid()} | ignore | {error, any()}.
 start_link(Name, Path, Opts) ->
-    gen_server:start_link({local, Name}, ?MODULE, [Name, Path, Opts], []).
+    gen_server:start_link({local, Name}, ?MODULE, [Path, Opts], []).
 
 sink_type() ->
     preformatted.
 
-init([Name, Path, Opts]) ->
+init([Path, Opts]) ->
     process_flag(trap_exit, true),
+    File = open_log(Path),
 
-    case ale_utils:supported_opts(Opts, [size]) of
-        false ->
-            {stop, invalid_opts};
-        true ->
-            DiskLogOptions = disk_log_opts(Name, Path, Opts),
+    BatchSize = proplists:get_value(batch_size, Opts, 524288),
+    BatchTimeout = proplists:get_value(batch_timeout, Opts, 1000),
 
-            case disk_log:open(DiskLogOptions) of
-                {ok, Name} ->
-                    {ok, #state{name=Name, path=Path}};
-                Error ->
-                    {stop, Error}
-            end
-    end.
+    {ok, #state{path = Path,
+                file = File,
+                buffer = <<>>,
+                buffer_size = 0,
+                batch_size = BatchSize,
+                batch_timeout = BatchTimeout}}.
 
-handle_call({log, Bytes}, _From, #state{name=Name} = State) ->
-    RV = disk_log:blog(Name, Bytes),
-    {reply, RV, State};
-
-handle_call(sync, _From, #state{name=Name} = State) ->
-    {reply, disk_log:sync(Name), State};
-
+handle_call({log, Msg}, _From, State) ->
+    {reply, ok, log_msg(Msg, State)};
+handle_call(sync, _From, State) ->
+    {reply, ok, sync(State)};
 handle_call(Request, _From, State) ->
     {stop, {unexpected_call, Request}, State}.
 
 handle_cast(Msg, State) ->
     {stop, {unexpected_cast, Msg}, State}.
 
+handle_info(flush_buffer, State) ->
+    {noreply, flush_buffer(State)};
 handle_info(Info, State) ->
     {stop, {unexpected_info, Info}, State}.
 
-terminate(_Reason, #state{name=Name} = _State) ->
-    ok = disk_log:close(Name).
+terminate(_Reason, State) ->
+    flush_buffer(State),
+    ok.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-disk_log_opts(Name, Path, Opts) ->
-    UserOpts = ale_utils:interesting_opts(Opts, [size]),
-    UserOpts1 = [{name, Name}, {file, Path} | UserOpts],
-    %% this prefers user supplied values
-    ale_utils:proplists_merge(UserOpts1, ?DISK_LOG_DEFAULTS).
+%% internal functions
+open_log(Path) ->
+    {ok, F} = file:open(Path, [raw, append, binary]),
+    F.
+
+log_msg(Msg, #state{buffer = Buffer,
+                    buffer_size = BufferSize,
+                    batch_size = BatchSize} = State) ->
+    NewBuffer = <<Buffer/binary, Msg/binary>>,
+    NewBufferSize = BufferSize + byte_size(Msg),
+
+    NewState = State#state{buffer = NewBuffer,
+                           buffer_size = NewBufferSize},
+
+    case NewBufferSize >= BatchSize of
+        true ->
+            flush_buffer(NewState);
+        false ->
+            maybe_arm_flush_timer(NewState)
+    end.
+
+flush_buffer(#state{file = File,
+                    buffer = Buffer,
+                    buffer_size = BufferSize} = State) ->
+    case BufferSize =:= 0 of
+        true ->
+            State;
+        false ->
+            ok = file:write(File, Buffer),
+            NewState = State#state{buffer = <<>>,
+                                   buffer_size = 0},
+            cancel_flush_timer(NewState)
+    end.
+
+maybe_arm_flush_timer(#state{flush_timer = undefined,
+                             batch_timeout = Timeout} = State) ->
+    TRef = erlang:send_after(Timeout, self(), flush_buffer),
+    State#state{flush_timer = TRef};
+maybe_arm_flush_timer(State) ->
+    State.
+
+cancel_flush_timer(#state{flush_timer = TRef} = State) when TRef =/= undefined ->
+    erlang:cancel_timer(TRef),
+    receive
+        flush_buffer -> ok
+    after
+        0 -> ok
+    end,
+    State#state{flush_timer = undefined};
+cancel_flush_timer(State) ->
+    State.
+
+sync(State) ->
+    NewState = flush_buffer(State),
+    file:datasync(NewState#state.file),
+    NewState.
