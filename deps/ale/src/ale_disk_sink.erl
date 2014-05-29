d456950f42e2032c273c289643ed4a51285ab728
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2014-05-29 13:37:22 -0700

    [ale] Add size based rotation support to disk sink.
    Change-Id: Ia8458611926ec5bf4b35ae002ef1549153fb6674
    Reviewed-on: http://review.couchbase.org/37747
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>

--- deps/ale/src/ale_disk_sink.erl
+++ deps/ale/src/ale_disk_sink.erl
@@ -24,17 +24,21 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-include_lib("kernel/include/file.hrl").
 -include("ale.hrl").
 
 -record(state, {
           path :: string(),
-          file :: file:io_device(),
+          file :: undefined | file:io_device(),
+          file_size :: integer(),
           buffer :: binary(),
           buffer_size :: integer(),
           flush_timer :: undefined | reference(),
 
           batch_size :: pos_integer(),
-          batch_timeout :: pos_integer()
+          batch_timeout :: pos_integer(),
+          rotation_size :: non_neg_integer(),
+          rotation_num_files :: pos_integer()
          }).
 
 start_link(Name, Path) ->
@@ -48,17 +52,23 @@ sink_type() ->
 
 init([Path, Opts]) ->
     process_flag(trap_exit, true),
-    File = open_log(Path),
 
     BatchSize = proplists:get_value(batch_size, Opts, 524288),
     BatchTimeout = proplists:get_value(batch_timeout, Opts, 1000),
 
-    {ok, #state{path = Path,
-                file = File,
-                buffer = <<>>,
-                buffer_size = 0,
-                batch_size = BatchSize,
-                batch_timeout = BatchTimeout}}.
+    RotationConf = proplists:get_value(rotation, Opts, []),
+    RotSize = proplists:get_value(size, RotationConf, 10485760),
+    RotNumFiles = proplists:get_value(num_files, RotationConf, 20),
+
+    State = #state{path = Path,
+                   buffer = <<>>,
+                   buffer_size = 0,
+                   batch_size = BatchSize,
+                   batch_timeout = BatchTimeout,
+                   rotation_size = RotSize,
+                   rotation_num_files = RotNumFiles},
+
+    {ok, open_log_file(State)}.
 
 handle_call({log, Msg}, _From, State) ->
     {reply, ok, log_msg(Msg, State)};
@@ -83,10 +93,6 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 %% internal functions
-open_log(Path) ->
-    {ok, F} = file:open(Path, [raw, append, binary]),
-    F.
-
 log_msg(Msg, #state{buffer = Buffer,
                     buffer_size = BufferSize,
                     batch_size = BatchSize} = State) ->
@@ -103,15 +109,21 @@ log_msg(Msg, #state{buffer = Buffer,
             maybe_arm_flush_timer(NewState)
     end.
 
-flush_buffer(#state{file = File,
-                    buffer = Buffer,
-                    buffer_size = BufferSize} = State) ->
+flush_buffer(State) ->
+    NewState = do_flush_buffer(State),
+    maybe_rotate_files(NewState).
+
+do_flush_buffer(#state{file = File,
+                       file_size = FileSize,
+                       buffer = Buffer,
+                       buffer_size = BufferSize} = State) ->
     case BufferSize =:= 0 of
         true ->
             State;
         false ->
             ok = file:write(File, Buffer),
-            NewState = State#state{buffer = <<>>,
+            NewState = State#state{file_size = FileSize + BufferSize,
+                                   buffer = <<>>,
                                    buffer_size = 0},
             cancel_flush_timer(NewState)
     end.
@@ -138,3 +150,102 @@ sync(State) ->
     NewState = flush_buffer(State),
     file:datasync(NewState#state.file),
     NewState.
+
+rotate_files(#state{path = Path,
+                    rotation_num_files = NumFiles}) ->
+    rotate_files_loop(Path, NumFiles - 1).
+
+rotate_files_loop(Path, 0) ->
+    case file:delete(Path) of
+        ok ->
+            ok;
+        Error ->
+            Error
+    end;
+rotate_files_loop(Path, 1) ->
+    To = Path ++ ".1",
+    case do_rotate_file(Path, To) of
+        ok ->
+            ok;
+        Error ->
+            Error
+    end;
+rotate_files_loop(Path, N) ->
+    From = Path ++ "." ++ integer_to_list(N - 1),
+    To = Path ++ "." ++ integer_to_list(N),
+
+    case do_rotate_file(From, To) of
+        ok ->
+            rotate_files_loop(Path, N - 1);
+        Error ->
+            Error
+    end.
+
+do_rotate_file(From, To) ->
+    case file:read_file_info(From) of
+        {error, enoent} ->
+            ok;
+        {error, _} = Error ->
+            Error;
+        {ok, _} ->
+            case file:rename(From, To) of
+                ok ->
+                    ok;
+                Error ->
+                    Error
+            end
+    end.
+
+maybe_rotate_files(#state{file_size = FileSize,
+                          rotation_size = RotSize} = State)
+  when RotSize =/= 0, FileSize >= RotSize ->
+    ok = rotate_files(State),
+    open_log_file(State);
+maybe_rotate_files(State) ->
+    State.
+
+open_log_file(#state{path = Path,
+                     file = OldFile} = State) ->
+    case OldFile of
+        undefined ->
+            ok;
+        _ ->
+            file:close(OldFile)
+    end,
+
+    {ok, File, #file_info{size = Size}} = open_file(Path),
+    State#state{file = File,
+                file_size = Size}.
+
+open_file(Path) ->
+    case file:read_file_info(Path) of
+        {ok, Info} ->
+            do_open_file(Path, Info);
+        {error, enoent} ->
+            case file:open(Path, [raw, append, binary]) of
+                {ok, File} ->
+                    file:close(File),
+                    open_file(Path);
+                Error ->
+                    Error
+            end;
+        Error ->
+            Error
+    end.
+
+do_open_file(Path, #file_info{inode = Inode}) ->
+    case file:open(Path, [raw, append, binary]) of
+        {ok, File} ->
+            case file:read_file_info(Path) of
+                {ok, #file_info{inode = Inode} = Info} -> % Inode is bound
+                    {ok, File, Info};
+                {ok, OtherInfo} ->
+                    file:close(File),
+                    do_open_file(Path, OtherInfo);
+                Error ->
+                    file:close(File),
+                    Error
+            end;
+        Error ->
+            Error
+    end.
