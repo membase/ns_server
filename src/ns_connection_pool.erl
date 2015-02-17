f7a5123cab22b3e0f7c50c23d2b88d7d3ba98099
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2015-02-17 12:00:28 -0800

    Add a hint about rest of the dicts used in ns_connection_pool.
    Change-Id: Idf9f9f32452f2303fffdda5172574876aa53d18f
    Reviewed-on: http://review.couchbase.org/46987
    Reviewed-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/ns_connection_pool.erl
+++ src/ns_connection_pool.erl
@@ -53,8 +53,8 @@
 -behaviour(gen_server).
 
 -record(ns_connection_pool, {
-          destinations = dict:new(),
-          sockets = dict:new(),
+          destinations = dict:new(), % Dest => [Socket]
+          sockets = dict:new(), % Socket => {Dest, Timer}
           clients = dict:new(), % Pid => {Dest, MonRef}
           queues = dict:new(),  % Dest => queue of Froms
           max_pool_size = 50 :: non_neg_integer(),
