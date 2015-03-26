9f11e393dd4063be363c43d54a1185cdc253488b
Pavel Blagodov <stochmail@gmail.com>
2015-03-26 20:53:18 +0300

    index tooling: removed index nodes tab
    Change-Id: I81e4d2c947793899ae75b1e7cf83e11fd2a7fe00
    Reviewed-on: http://review.couchbase.org/48817
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- priv/public/js/indexes.js
+++ priv/public/js/indexes.js
@@ -14,59 +14,27 @@
    limitations under the License.
  **/
 
-function createIndexesSectionCells(ns, modeCell, modeTabsCell, tasksProgressCell) {
+function createIndexesSectionCells(ns, modeCell, tasksProgressCell) {
   ns.isAtIndexesTabCell = Cell.compute(function (v) {
-    return v.need(modeCell) === "indexes" && v.need(modeTabsCell) === "indexes";
+    return v.need(modeCell) === "indexes";
   }).name('isAtIndexesTabCell');
-  ns.isAtNodesTabCell = Cell.compute(function (v) {
-    return v.need(modeCell) === "indexes" && v.need(modeTabsCell) === "nodes";
-  }).name('isAtNodesTabCell');
   ns.indexesCell = Cell.compute(function (v) {
     if (!v.need(ns.isAtIndexesTabCell)) {
       return;
     }
     return future.get({url: "/indexStatus"});
   });
-  ns.indexNodesCell = Cell.compute(function (v) {
-    if (!v.need(ns.isAtNodesTabCell)) {
-      return;
-    }
-    return future.get({url: "/indexStatus"}, valueTransformer);
-    function valueTransformer(statusRows) {
-      var knownHosts = {};
-      _.each(statusRows, function (is) {
-        knownHosts[is.hostname] = 1;
-      });
-      var hostnames = _.keys(knownHosts).sort(naturalSort);
-      return _.map(hostnames, function (h) {
-        return {hostname: h,
-                currentQuota: 'TODO',
-                pendingQuota: 'TODO'};
-      });
-    }
-  });
 }
 var IndexesSection = {
   init: function () {
     var self = IndexesSection;
-    self.modeTabsCell = new TabsCell("indexesTab",
-                                 "#js_indexes .tabs.switcher",
-                                 "#js_indexes .panes > div",
-                                 ["indexes", "nodes"]);
-    createIndexesSectionCells(IndexesSection, DAL.cells.mode, self.modeTabsCell, DAL.cells.tasksProgressCell);
+    createIndexesSectionCells(IndexesSection, DAL.cells.mode, DAL.cells.tasksProgressCell);
     prepareTemplateForCell('js_indexes_list', self.indexesCell);
-    prepareTemplateForCell('js_index_nodes_list', self.indexNodesCell);
     self.indexesCell.subscribeValue(function (list) {
       if (!list) {
         return;
       }
       renderTemplate('js_indexes_list', list);
-    });
-    self.indexNodesCell.subscribeValue(function (list) {
-      if (!list) {
-        return;
-      }
-      renderTemplate('js_index_nodes_list', list);
     });
   },
   onEnter: function () {
