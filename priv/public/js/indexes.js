93f6c4d8edc30c880aedeeafbfb4ffc14f803ae2
Pavel Blagodov <stochmail@gmail.com>
2015-04-01 18:30:05 +0300

    index tooling: sortable indexes table
    Change-Id: I9d14e42ef97174a9aa30c6c2ce4e2b687d1fe7c0
    Reviewed-on: http://review.couchbase.org/49028
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- priv/public/js/indexes.js
+++ priv/public/js/indexes.js
@@ -14,7 +14,7 @@
    limitations under the License.
  **/
 
-function createIndexesSectionCells(ns, modeCell, tasksProgressCell) {
+function createIndexesSectionCells(ns, modeCell, indexesTableSortByCell, indexesTableSortDescendingCell) {
   ns.isAtIndexesTabCell = Cell.compute(function (v) {
     return v.need(modeCell) === "indexes";
   }).name('isAtIndexesTabCell');
@@ -23,19 +23,69 @@ function createIndexesSectionCells(ns, modeCell, tasksProgressCell) {
       return;
     }
     return future.get({url: "/indexStatus"});
-  });
+  }).name("indexesCell");
+  ns.sortedIndexesCell = Cell.compute(function (v) {
+    var indexes = v.need(ns.indexesCell);
+    var sortBy = v.need(indexesTableSortByCell);
+    var sortDescending = v.need(indexesTableSortDescendingCell);
+    if (indexes[0] && !indexes[0][sortBy]) {
+      sortBy = "hostname";
+    }
+    var rv = {
+      list: indexes,
+      descending: sortDescending,
+      sortBy: sortBy
+    };
+    if (!indexes[0]) {
+      return rv;
+    }
+    indexes.sort(mkComparatorByProp(sortBy, naturalSort));
+    if (sortDescending === "true") {
+      indexes.reverse();
+    }
+    return rv;
+  }).name("sortedIndexesCell");
+  ns.sortedIndexesCell.equality = function (a, b) {return false;};
 }
 var IndexesSection = {
   init: function () {
     var self = IndexesSection;
-    createIndexesSectionCells(IndexesSection, DAL.cells.mode, DAL.cells.tasksProgressCell);
+    var indexesListContainer = $("#js_indexes_list_container");
+    var indexesTableSortByCell = new Cell();
+    var indexesTableSortDescendingCell = new Cell();
+    createIndexesSectionCells(
+      IndexesSection,
+      DAL.cells.mode,
+      indexesTableSortByCell,
+      indexesTableSortDescendingCell
+    );
     prepareTemplateForCell('js_indexes_list', self.indexesCell);
-    self.indexesCell.subscribeValue(function (list) {
-      if (!list) {
+    var headers
+    self.sortedIndexesCell.subscribeValue(function (config) {
+      if (!config) {
         return;
       }
-      renderTemplate('js_indexes_list', list);
+      renderTemplate('js_indexes_list', config.list);
+      headers = $("[data-sortby]", indexesListContainer);
+      headers.click(function () {
+        var header = jQuery(this);
+        var sortBy = header.data("sortby");
+        if (sortBy === indexesTableSortByCell.value) {
+          indexesTableSortDescendingCell.setValue(config.descending === "true" ? "false" : "true");
+        } else {
+          indexesTableSortDescendingCell.setValue("false");
+        }
+        indexesTableSortByCell.setValue(sortBy);
+      });
+      indexesListContainer.toggleClass("dynamic_descending", config.descending === "true");
+      $("[data-sortby=" + config.sortBy + "]", indexesListContainer).addClass("dynamic_active");
     });
+    self.isAtIndexesTabCell.subscribeValue(function (isIndexesTab) {
+      if (isIndexesTab) {
+        !indexesTableSortByCell.value && indexesTableSortByCell.setValue("hostname");
+        !indexesTableSortDescendingCell.value && indexesTableSortDescendingCell.setValue("false");
+      }
+    })
   },
   onEnter: function () {
   },
