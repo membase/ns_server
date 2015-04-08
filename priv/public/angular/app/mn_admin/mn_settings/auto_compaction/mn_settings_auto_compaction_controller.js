816f17f367bba48c696b0684bd1d5660208967c1
Pavel Blagodov <stochmail@gmail.com>
2015-04-08 15:53:04 +0300

    MB-12222: catchErrors of auto-compaction validation
    Change-Id: Ic38819b01011cdcc2057261a0a24b8878b5705b3
    Reviewed-on: http://review.couchbase.org/49375
    Reviewed-by: Eben Haber <eben@couchbase.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/angular/app/mn_admin/mn_settings/auto_compaction/mn_settings_auto_compaction_controller.js
+++ priv/public/angular/app/mn_admin/mn_settings/auto_compaction/mn_settings_auto_compaction_controller.js
@@ -1,12 +1,10 @@
 angular.module('mnSettingsAutoCompaction').controller('mnSettingsAutoCompactionController',
   function($scope, mnHelper, mnSettingsAutoCompactionService, autoCompactionSettings) {
     $scope.autoCompactionSettings = autoCompactionSettings;
-    function getErrors(resp) {
-      $scope.errors = resp.data.errors;
-    }
     $scope.$watch('autoCompactionSettings', function (autoCompactionSettings) {
-      mnSettingsAutoCompactionService
-        .saveAutoCompaction(autoCompactionSettings, {just_validate: 1}).then(getErrors, getErrors);
+      mnHelper.promiseHelper($scope, mnSettingsAutoCompactionService
+        .saveAutoCompaction(autoCompactionSettings, {just_validate: 1}))
+        .catchErrors();
     }, true);
     $scope.submit = function () {
       if ($scope.viewLoading) {
