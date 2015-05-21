4a2490b2df32ff52fc148b28c43cc4258b6b710c
Artem Stemkovski <astemkov@gmail.com>
2015-05-20 19:06:18 -0700

    enable email for audit_dropped_events alert by default
    Change-Id: Ib9e2b0c44f693da6c8ac6e16a5cc86e4d403ba9c
    Reviewed-on: http://review.couchbase.org/51313
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Poonam D <poonam@couchbase.com>

--- src/ns_config_default.erl
+++ src/ns_config_default.erl
@@ -355,7 +355,7 @@ default() ->
                        {encrypt, false}]},
        {alerts, [auto_failover_node,auto_failover_maximum_reached,
                  auto_failover_other_nodes_down,auto_failover_cluster_too_small,ip,
-                 disk,overhead,ep_oom_errors,ep_item_commit_failed]}
+                 disk,overhead,ep_oom_errors,ep_item_commit_failed, audit_dropped_events]}
       ]},
      {alert_limits, [
        %% Maximum percentage of overhead compared to max bucket size (%)
