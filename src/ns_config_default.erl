e75deeb4a45482939810926f56630d73c78df540
Artem Stemkovski <astemkov@gmail.com>
2015-05-20 19:09:39 -0700

    formatting
    Change-Id: Iaa0702d043f1cf7d5ff149ad898560aeb569e6cd
    Reviewed-on: http://review.couchbase.org/51314
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Poonam D <poonam@couchbase.com>

--- src/ns_config_default.erl
+++ src/ns_config_default.erl
@@ -353,9 +353,9 @@ default() ->
                        {host, "localhost"},
                        {port, 25},
                        {encrypt, false}]},
-       {alerts, [auto_failover_node,auto_failover_maximum_reached,
-                 auto_failover_other_nodes_down,auto_failover_cluster_too_small,ip,
-                 disk,overhead,ep_oom_errors,ep_item_commit_failed, audit_dropped_events]}
+       {alerts, [auto_failover_node, auto_failover_maximum_reached,
+                 auto_failover_other_nodes_down, auto_failover_cluster_too_small, ip,
+                 disk, overhead, ep_oom_errors, ep_item_commit_failed, audit_dropped_events]}
       ]},
      {alert_limits, [
        %% Maximum percentage of overhead compared to max bucket size (%)
