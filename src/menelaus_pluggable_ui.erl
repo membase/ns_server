d974ffd661c5d1c8a73ffb3138f90a5ada0fece8
Anders Nygren <anders.nygren@couchbase.com>
2015-10-28 11:21:31 -0600

    MB-16754: Add menelaus_pluggable_ui.erl
    menelaus_pluggable_ui add suport for proxying requests to service
    nodes that support pluggable UI.
    
    Change-Id: If9581e9a94a21ecea211337390c402e979aba59c
    Reviewed-on: http://review.couchbase.org/56488
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>
    Tested-by: Dave Finlay <dave.finlay@couchbase.com>

---
+++ src/menelaus_pluggable_ui.erl
@@ -0,0 +1,274 @@
+%% @author Couchbase, Inc <info@couchbase.com>
+%% @copyright 2015 Couchbase, Inc.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%      http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+-module(menelaus_pluggable_ui).
+
+-export([find_plugins/0,
+         is_plugin/2,
+         proxy_req/4,
+         maybe_serve_file/4]).
+
+-include("ns_common.hrl").
+
+-define(CONFIG_DIR, etc).
+-define(PLUGIN_FILE_PATTERN, "pluggable-ui-*.json").
+-define(TIMEOUT, 60000).
+-define(PART_SIZE, 100000).
+-define(WINDOW_SIZE, 5).
+
+-type service_name()   :: atom().
+-type rest_api_prefix():: string().
+-type proxy_strategy() :: local.
+-record(plugin, { name            :: service_name(),
+                  proxy_strategy  :: proxy_strategy(),
+                  rest_api_prefix :: rest_api_prefix(),
+                  doc_root        :: binary()}).
+-type plugin()  :: #plugin{}.
+-type plugins() :: [plugin()].
+
+-spec find_plugins() -> plugins().
+find_plugins() ->
+    SpecFiles = find_plugin_spec_files(),
+    read_and_validate_plugin_specs(SpecFiles).
+
+find_plugin_spec_files() ->
+    filelib:wildcard(filename:join(path_config:component_path(?CONFIG_DIR),
+                                   ?PLUGIN_FILE_PATTERN)).
+
+read_and_validate_plugin_specs(SpecFiles) ->
+    lists:foldl(fun read_and_validate_plugin_spec/2, [], SpecFiles).
+
+read_and_validate_plugin_spec(File, Acc) ->
+    {ok, Bin} = file:read_file(File),
+    {KVs} = ejson:decode(Bin),
+    validate_plugin_spec(KVs, Acc).
+
+-spec validate_plugin_spec([{binary(), binary()}], plugins()) -> plugins().
+validate_plugin_spec(KVs, Plugins) ->
+    ServiceName = binary_to_atom(get_element(<<"service">>, KVs), latin1),
+    ProxyStrategy = decode_proxy_strategy(get_element(<<"proxy-strategy">>,
+                                                      KVs)),
+    RestApiPrefix = binary_to_list(get_element(<<"rest-api-prefix">>, KVs)),
+    case {valid_service(ServiceName),
+          find_plugin_by_prefix(RestApiPrefix, Plugins)} of
+        {true, false} ->
+            ?log_info("Loaded pluggable UI specification for ~p~n",
+                      [ServiceName]),
+            [#plugin{name = ServiceName,
+                     proxy_strategy = ProxyStrategy,
+                     rest_api_prefix = RestApiPrefix,
+                     doc_root = get_element(<<"doc-root">>, KVs)} | Plugins];
+        {true, #plugin{}} ->
+            ?log_info("Pluggable UI specification for ~p not loaded, "
+                      "duplicate REST API prefix ~p~n",
+                      [ServiceName, RestApiPrefix]),
+            Plugins;
+        {false, _} ->
+            ?log_info("Pluggable UI specification for ~p not loaded~n",
+                      [ServiceName]),
+            Plugins
+    end.
+
+valid_service(ServiceName) ->
+    lists:member(ServiceName,
+                 ns_cluster_membership:supported_services()).
+
+get_element(Key, KVs) ->
+    {Key, Val} = lists:keyfind(Key, 1, KVs),
+    Val.
+
+decode_proxy_strategy(<<"local">>) ->
+    local.
+
+%%% =============================================================
+%%%
+proxy_req(RestPrefix, Path, Plugins, Req) ->
+    Service = prefix_to_service(RestPrefix, Plugins),
+    case address_and_port_for(Service, Plugins) of
+        HostPort when is_tuple(HostPort) ->
+            do_proxy_req(HostPort, Path, Req);
+        Error ->
+            server_error(Req, Service, Error)
+    end.
+
+address_and_port_for(Service, Plugins) ->
+    case proxy_strategy(Service, Plugins) of
+        false ->
+            undefined;
+        {found, ProxyStrategy} ->
+            pick_address_and_port_for(Service, ProxyStrategy)
+    end.
+
+proxy_strategy(Service, Plugins) ->
+    case find_plugin_by_name(Service, Plugins) of
+        #plugin{proxy_strategy = ProxyStrategy} ->
+            {found, ProxyStrategy};
+        false ->
+            false
+    end.
+
+pick_address_and_port_for(Service, local) ->
+    case ns_cluster_membership:should_run_service(Service, node()) of
+        true ->
+            address_and_port(Service, node());
+        false ->
+            service_not_running
+    end.
+
+address_and_port(Service, Node) ->
+    Addr = node_address(Node),
+    Port = port_for(Service, Node),
+    {Addr, Port}.
+
+node_address(Node) ->
+    {_Node, Addr} = misc:node_name_host(Node),
+    Addr.
+
+port_for(fts, Node) ->
+    {value, Port} = ns_config:search(ns_config:latest(),
+                                     {node, Node, fts_http_port}),
+    Port;
+
+port_for(n1ql, Node) ->
+    {value, Port} = ns_config:search(ns_config:latest(),
+                                     {node, Node, query_port}),
+    Port.
+
+do_proxy_req({Host, Port}, Path, Req) ->
+    Method = Req:get(method),
+    Headers = convert_headers(Req),
+    Body = get_body(Req),
+    Options = [{partial_download, [{window_size, ?WINDOW_SIZE},
+                                   {part_size, ?PART_SIZE}]}],
+    Resp = lhttpc:request(Host, Port, false, Path, Method, Headers, Body,
+                          ?TIMEOUT, Options),
+    handle_resp(Resp, Req).
+
+convert_headers(MochiReq) ->
+    HeadersList = mochiweb_headers:to_list(MochiReq:get(headers)),
+    Headers = lists:filtermap(fun ({'Content-Length', _Value}) ->
+                                      false;
+                                  ({'Transfer-Encoding', _Value}) ->
+                                      false;
+                                  ({Name, Value}) ->
+                                      {true, {convert_header_name(Name), Value}}
+                              end, HeadersList),
+    case menelaus_auth:extract_ui_auth_token(MochiReq) of
+        undefined ->
+            Headers;
+        Token ->
+            [{"ns-server-auth-token", Token} | Headers]
+    end.
+
+convert_header_name(Header) when is_atom(Header) ->
+    atom_to_list(Header);
+convert_header_name(Header) when is_list(Header) ->
+    Header.
+
+get_body(Req) ->
+    case Req:recv_body() of
+        Body when is_binary(Body) ->
+            Body;
+        undefined ->
+            <<>>
+    end.
+
+handle_resp({ok, {{StatusCode, _ReasonPhrase}, RcvdHeaders, Pid}}, Req)
+  when is_pid(Pid) ->
+    SendHeaders = filter_resp_headers(RcvdHeaders),
+    Resp = start_response(Req, StatusCode, SendHeaders),
+    stream_body(Pid, Resp);
+handle_resp({ok, {{StatusCode, _ReasonPhrase}, RcvdHeaders, undefined = _Body}},
+            Req) ->
+    SendHeaders = filter_resp_headers(RcvdHeaders),
+    menelaus_util:respond(Req, {StatusCode, SendHeaders, <<>>});
+handle_resp({error, _Reason}=Error, Req) ->
+    ?log_error("http client error ~p~n", [Error]),
+    menelaus_util:respond(Req, {500, [], <<"Unexpected server error">> }).
+
+filter_resp_headers(Headers) ->
+    lists:filter(fun is_safe_response_header/1, Headers).
+
+is_safe_response_header({"Content-Length", _}) ->
+    false;
+is_safe_response_header({"Transfer-Encoding", _}) ->
+    false;
+is_safe_response_header(_) ->
+    true.
+
+start_response(Req, StatusCode, Headers) ->
+    menelaus_util:respond(Req, {StatusCode, Headers, chunked}).
+
+stream_body(Pid, Resp) ->
+    case lhttpc:get_body_part(Pid) of
+        {ok, Part} when is_binary(Part) ->
+            Resp:write_chunk(Part),
+            stream_body(Pid, Resp);
+        {ok, {http_eob, _Trailers}} ->
+            Resp:write_chunk(<<>>)
+    end.
+
+server_error(Req, Service, service_not_running) ->
+    Msg = list_to_binary("Service " ++ atom_to_list(Service)
+                         ++ " not running on this node"),
+    send_server_error(Req, Msg).
+
+send_server_error(Req, Msg) ->
+    menelaus_util:respond(Req, {404, [], Msg}).
+
+%%% =============================================================
+%%%
+maybe_serve_file(RestPrefix, Plugins, Req, Path) ->
+    case doc_root(RestPrefix, Plugins) of
+        DocRoot when is_binary(DocRoot) ->
+            menelaus_util:serve_file(Req,
+                                     Path,
+                                     DocRoot,
+                                     [{"Cache-Control", "max-age=10"}]);
+        undefined ->
+            menelaus_util:reply_not_found(Req)
+    end.
+
+doc_root(RestPrefix, Plugins) ->
+    case find_plugin_by_prefix(RestPrefix, Plugins) of
+        #plugin{doc_root = DocRoot} ->
+            DocRoot;
+        false ->
+            undefined
+    end.
+
+%%% =============================================================
+%%%
+-spec is_plugin(string(), plugins()) -> boolean().
+is_plugin(Prefix, Plugins) ->
+    case find_plugin_by_prefix(Prefix, Plugins) of
+        #plugin{} ->
+            true;
+        _ ->
+            false
+    end.
+
+prefix_to_service(Prefix, Plugins) ->
+    #plugin{name = Service} = find_plugin_by_prefix(Prefix, Plugins),
+    Service.
+
+find_plugin_by_name(Name, Plugins) ->
+    find_plugin(Name, Plugins, #plugin.name).
+
+find_plugin_by_prefix(Prefix, Plugins) ->
+    find_plugin(Prefix, Plugins, #plugin.rest_api_prefix).
+
+find_plugin(Prefix, Plugins, KeyPos) ->
+    lists:keyfind(Prefix, KeyPos, Plugins).
