1c11723c8f707ba4b88bbd7046f0eb98d94cc51b
Pavel Blagodov <stochmail@gmail.com>
2015-11-24 16:49:51 +0300

    MB-12222: constants refactor to align with standard style guide
    Change-Id: I46ff5fc555f227099c80fec2b4ebbb425ed8fcae
    Reviewed-on: http://review.couchbase.org/57213
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>
    Tested-by: Dave Finlay <dave.finlay@couchbase.com>

--- priv/public/ui/app/constants/constants.js
+++ priv/public/ui/app/constants/constants.js
@@ -1,39 +1,46 @@
-angular.module('app').constant('bucketsFormConfiguration', {
-  authType: 'sasl',
-  name: '',
-  saslPassword: '',
-  bucketType: 'membase',
-  evictionPolicy: 'valueOnly',
-  replicaNumber: "1",
-  replicaIndex: "0",
-  threadsNumber: "3",
-  flushEnabled: "0",
-  ramQuotaMB: "0",
+(function () {
+  "use strict";
 
-  uri: '/pools/default/buckets'
-}).constant('knownAlerts', [
-  'auto_failover_node',
-  'auto_failover_maximum_reached',
-  'auto_failover_other_nodes_down',
-  'auto_failover_cluster_too_small',
-  'ip',
-  'disk',
-  'overhead',
-  'ep_oom_errors',
-  'ep_item_commit_failed'
-]).constant('timeUnitToSeconds', {
-  minute: 60,
-  hour: 3600,
-  day: 86400,
-  week: 691200,
-  month: 2678400,
-  year: 31622400
-})
-.constant("docsLimit", 1000)
-.constant("docBytesLimit", 2500)
-.constant("viewsPerPageLimit", 6)
-.constant("IEC", {
-  Ki: 1024,
-  Mi: 1048576,
-  Gi: 1073741824
-});
+  angular
+    .module('app')
+    .constant('bucketsFormConfiguration', {
+      authType: 'sasl',
+      name: '',
+      saslPassword: '',
+      bucketType: 'membase',
+      evictionPolicy: 'valueOnly',
+      replicaNumber: "1",
+      replicaIndex: "0",
+      threadsNumber: "3",
+      flushEnabled: "0",
+      ramQuotaMB: "0",
+      uri: '/pools/default/buckets'
+    })
+    .constant('knownAlerts', [
+      'auto_failover_node',
+      'auto_failover_maximum_reached',
+      'auto_failover_other_nodes_down',
+      'auto_failover_cluster_too_small',
+      'ip',
+      'disk',
+      'overhead',
+      'ep_oom_errors',
+      'ep_item_commit_failed'
+    ])
+    .constant('timeUnitToSeconds', {
+      minute: 60,
+      hour: 3600,
+      day: 86400,
+      week: 691200,
+      month: 2678400,
+      year: 31622400
+    })
+    .constant("docsLimit", 1000)
+    .constant("docBytesLimit", 2500)
+    .constant("viewsPerPageLimit", 6)
+    .constant("IEC", {
+      Ki: 1024,
+      Mi: 1048576,
+      Gi: 1073741824
+    });
+})();
