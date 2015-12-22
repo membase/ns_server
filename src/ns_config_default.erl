1b8e0a7dc26bc705d720438f1948009887a47165
Artem Stemkovski <astemkov@gmail.com>
2015-12-22 13:14:03 -0800

    MB-11314 add preconfigured roles to default config
    we do not make them editable in watson, but in future releases
    we might to add possibility to reconfigure roles
    
    Change-Id: I1fc07247b8e4a098d92f9b653017b537ef0ffa03
    Reviewed-on: http://review.couchbase.org/58476
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/ns_config_default.erl
+++ src/ns_config_default.erl
@@ -373,7 +373,8 @@ default() ->
      %% everything is unlimited by default
      {{request_limit, rest}, undefined},
      {{request_limit, capi}, undefined},
-     {drop_request_memory_threshold_mib, undefined}].
+     {drop_request_memory_threshold_mib, undefined},
+     {roles_definitions, menelaus_roles:preconfigured_roles()}].
 
 %% Recursively replace all strings in a hierarchy that start
 %% with a given Prefix with a ReplacementPrefix.  For example,
