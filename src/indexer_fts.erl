78a4d77b64755ee2c931c95f82222b255f3082ab
Marty Schoch <marty.schoch@gmail.com>
2016-02-02 15:19:53 -0500

    MB-17754: update stat names to match those used by cbft
    Change-Id: Ie0d7d3751e32447f748cffa947abc933110ea426
    Reviewed-on: http://review.couchbase.org/59415
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Marty Schoch <marty.schoch@gmail.com>

--- src/indexer_fts.erl
+++ src/indexer_fts.erl
@@ -56,10 +56,11 @@ get_gauges() ->
     [doc_count, num_pindexes].
 
 get_counters() ->
-    [timer_batch_execute_count, timer_batch_merge_count, timer_batch_store_count,
-     timer_iterator_next_count, timer_iterator_seek_count, timer_iterator_seek_first_count,
-     timer_reader_get_count, timer_reader_iterator_count, timer_writer_delete_count,
-     timer_writer_get_count, timer_writer_iterator_count, timer_writer_set_count,
+    [timer_batch_merge_count, timer_batch_store_count,
+     timer_iterator_next_count, timer_iterator_seek_count,
+     timer_reader_get_count, timer_reader_multi_get_count,
+     timer_reader_prefix_iterator_count, timer_reader_range_iterator_count,
+     timer_writer_execute_batch_count,
      timer_opaque_set_count, timer_rollback_count, timer_data_update_count,
      timer_data_delete_count, timer_snapshot_start_count, timer_opaque_get_count].
 
