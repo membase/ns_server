46e2a335a7af8f0f625a65d83d6fe2f5098a07c3
dave-finlay <dave.finlay@couchbase.com>
2016-02-10 12:18:24 -0800

    MB-18026: if not enterprise set storagemode to forestdb in wizard
    Change-Id: Ic0d342ca4616bab3fe076932e066df995fd2a711
    Reviewed-on: http://review.couchbase.org/59762
    Tested-by: Dave Finlay <dave.finlay@couchbase.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- priv/public/ui/app/mn_wizard/step1/mn_wizard_step1_controller.js
+++ priv/public/ui/app/mn_wizard/step1/mn_wizard_step1_controller.js
@@ -22,6 +22,9 @@
       mnPromiseHelper(vm, mnWizardStep1Service.getConfig())
       .applyToScope("config")
       .onSuccess(function (config) {
+        if (!vm.isEnterprise) {
+          config.startNewClusterConfig.indexSettings.storageMode = 'forestdb';
+        }
         vm.onDbPathChange();
         vm.onIndexPathChange();
       });
