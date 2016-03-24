4f0474486da0177db443de06f943f676813c3e2c
Pavel Blagodov <stochmail@gmail.com>
2016-03-24 13:31:12 +0300

    extract errors with null value in extractErrors function
    in some cases that indicates that there are no errors
    
    Change-Id: Ic25a02ec4b9a68eaa7af5320a8f6fd5a13abbbf2
    Reviewed-on: http://review.couchbase.org/61936
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>

--- priv/public/ui/app/components/mn_promise_helper.js
+++ priv/public/ui/app/components/mn_promise_helper.js
@@ -155,7 +155,7 @@
         if (resp.status === 0) {
           return false;
         }
-        var errors = resp.data && resp.data.errors && _.keys(resp.data).length === 1 ? resp.data.errors : resp.data || resp ;
+        var errors = resp.data && resp.data.errors !== undefined && _.keys(resp.data).length === 1 ? resp.data.errors : resp.data || resp ;
         return _.isEmpty(errors) ? false : errors;
       }
       function clearSpinnerTimeout() {
