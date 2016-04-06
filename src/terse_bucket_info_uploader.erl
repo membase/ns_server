3a7f690763ebbb6315d7a19c7b9b0e8e7121888e
Anders Nygren <anders.nygren@couchbase.com>
2016-04-06 14:10:39 -0500

    Flush bucket_info_cache_invalidations to avoid unneeded cluster updates
    terse_bucket_info_uploader is converted to a gen_server, and flushes
    bucket_info_cache_invalidation messages in order to avoid repeted
    memcached cluster configuration updates with identical data.
    
    Signed-off-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    [aliaksiej.artamonau@gmail.com: removed generic gen_server comments]
    
    Change-Id: I935600d5ed098f93028bf46b754a63c2b9acb91e
    Reviewed-on: http://review.couchbase.org/62505
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/terse_bucket_info_uploader.erl
+++ src/terse_bucket_info_uploader.erl
@@ -16,18 +16,32 @@
 %% @doc This service watches changes of terse bucket info and uploads
 %% it to ep-engine
 -module(terse_bucket_info_uploader).
+
+-behaviour(gen_server).
+
 -include("ns_common.hrl").
 
 -export([start_link/1,
          refresh_cluster_config/1,
          get_pid/1]).
 
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
 start_link(BucketName) ->
     ns_bucket_sup:ignore_if_not_couchbase_bucket(
       BucketName,
       fun (_) ->
               Name = server_name(BucketName),
-              work_queue:start_link(Name, fun () -> init(BucketName) end)
+              gen_server:start_link({local, Name}, ?MODULE, BucketName, [])
       end).
 
 get_pid(BucketName) ->
@@ -36,10 +50,45 @@ get_pid(BucketName) ->
 server_name(BucketName) ->
     list_to_atom("terse_bucket_info_uploader-" ++ BucketName).
 
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
 init(BucketName) ->
     Self = self(),
     ns_pubsub:subscribe_link(bucket_info_cache_invalidations, fun invalidation_loop/2, {BucketName, Self}),
-    submit_refresh(BucketName, Self).
+    submit_refresh(BucketName, Self),
+    {ok, #state{}}.
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info({refresh, BucketName}, State) ->
+    flush_refresh_msgs(BucketName),
+    refresh_cluster_config(BucketName),
+    {noreply, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+flush_refresh_msgs(BucketName) ->
+    case misc:flush({refresh, BucketName}) of
+        N when N > 0 ->
+            ?log_debug("Flushed ~p refresh messages", [N]);
+        _ ->
+            ok
+    end.
 
 refresh_cluster_config(BucketName) ->
     case bucket_info_cache:terse_bucket_info(BucketName) of
@@ -66,10 +115,7 @@ set_cluster_config(BucketName, JSON) ->
     end.
 
 submit_refresh(BucketName, Process) ->
-    work_queue:submit_work(Process,
-                           fun () ->
-                                   refresh_cluster_config(BucketName)
-                           end).
+    Process ! {refresh, BucketName}.
 
 invalidation_loop(BucketName, {BucketName, Parent}) ->
     submit_refresh(BucketName, Parent),
