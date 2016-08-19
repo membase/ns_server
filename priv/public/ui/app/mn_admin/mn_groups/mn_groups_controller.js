c113e734aa8561fb740d82eaf59433e98d347633
Pavel Blagodov <stochmail@gmail.com>
2016-08-19 15:24:21 +0300

    spock-ui: update look and functionality of groups tab
    - old drag and drop functionality replaced on select box
      due to the fact that it has bad usability on large cluster.
      The issue was disscussed in Node Scalability UI document under
      Server Groups Tab section.
    
    - added new styles
    
    Change-Id: I2e75e1c213b2d720d8e7ebb3cb599c8a456f6d78
    Reviewed-on: http://review.couchbase.org/66912
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- priv/public/ui/app/mn_admin/mn_groups/mn_groups_controller.js
+++ priv/public/ui/app/mn_admin/mn_groups/mn_groups_controller.js
@@ -16,18 +16,15 @@
     ])
     .controller('mnGroupsController', mnGroupsController);
 
-    function mnGroupsController($scope, $uibModal, mnGroupsService, mnPromiseHelper, mnHelper, mnPoller, jQuery, mnMakeSafeForCSSFilter, mnNaturalSortingFilter, $window, mnAlertsService, mnPoolDefault) {
+    function mnGroupsController($scope, $uibModal, mnGroupsService, mnPromiseHelper, mnHelper, mnPoller, mnMakeSafeForCSSFilter, mnNaturalSortingFilter, $window, mnAlertsService, mnPoolDefault) {
       var vm = this;
 
       vm.createGroup = createGroup;
       vm.deleteGroup = deleteGroup;
       vm.applyChanges = applyChanges;
       vm.reloadState = mnHelper.reloadState;
-
-      vm.onItemTaken = onItemTaken;
-      vm.onItemDropped = onItemDropped;
-      vm.onItemMoved = onItemMoved;
-
+      vm.changeNodeGroup = changeNodeGroup;
+      vm.groupsModel = {};
       vm.disableApplyChangesBtn = true;
 
       activate();
@@ -42,16 +39,17 @@
               vm.disableApplyChangesBtn = true;
               vm.revisionMismatch = true;
             } else {
-              mnAlertsService.showAlertInPopup(resp.data, 'error', timeout);
+              mnAlertsService.showAlertInPopup(resp.data, 'error');
             }
           });
       }
 
-      function deleteGroup(group) {
-        var initialGroups = vm.state.initialGroups;
-        var currentGroups = vm.state.currentGroups;
+      function isGroupsEqual() {
+        return _.isEqual(vm.state.initialGroups, vm.state.currentGroups);
+      }
 
-        if (_.isEqual(initialGroups, currentGroups)) {
+      function deleteGroup(group) {
+        if (isGroupsEqual()) {
           return $uibModal.open({
             templateUrl: 'mn_admin/mn_groups/delete_dialog/mn_groups_delete_dialog.html',
             controller: 'mnGroupsDeleteDialogController as groupsDeleteDialogCtl',
@@ -76,151 +74,26 @@
         });
       }
 
+      function changeNodeGroup(server, currentGroupName) {
+        var fromGroup = _.find(vm.state.currentGroups, function (cGroup) {
+          return cGroup.name === currentGroupName;
+        });
+        var toGroup = _.find(vm.state.currentGroups, function (cGroup) {
+          return cGroup.name === vm.groupsModel[server.hostname].name;
+        });
+
+        _.remove(fromGroup.nodes, function (node) {
+          return node.hostname === server.hostname;
+        });
+
+        toGroup.nodes.push(server);
+
+        vm.disableApplyChangesBtn = false;
+      }
+
       function activate() {
         mnPromiseHelper(vm, mnGroupsService.getGroupsState())
           .applyToScope("state");
       }
-
-
-      //this part of code should be reworked in angular style later
-      //or we can use something like https://github.com/marceljuenemann/angular-drag-and-drop-lists
-      var dragSelector = ".js_draggable";
-      var overSelector = ".js_group_wrapper";
-      var putIntoSelector = ".js_group_servers";
-      var overActiveSelector = "dynamic_over";
-      var emplyListSelector = "dynamic_empty";
-      var contextSelector = "#js_groups";
-      var placeholderSelector = "js_drag_placeholder drag_placeholder";
-      var placeholder = jQuery("<span />").addClass(placeholderSelector).get(0);
-      var prevRecipientGroupDomElement;
-      var jQApplicantDomElements;
-      var takenDomElement;
-      var takenGroupDomElement;
-      var recipientDomElement;
-      var hostnameComparator = mkComparatorByProp('hostname', mnNaturalSortingFilter);
-
-      function onItemTaken(serverHostname) {
-        jQApplicantDomElements = jQuery(overSelector, contextSelector);
-        takenDomElement = jQuery(dragSelector + "_" + mnMakeSafeForCSSFilter(serverHostname));
-        takenGroupDomElement = detectAcceptingTarget(jQApplicantDomElements, takenDomElement);
-
-        jQuery(takenDomElement).after(placeholder);
-        jQuery(takenGroupDomElement).addClass(overActiveSelector);
-      }
-      function onItemDropped() {
-        recipientDomElement = detectAcceptingTarget(jQApplicantDomElements, takenDomElement) || prevRecipientGroupDomElement;
-
-        jQuery(placeholder).detach();
-        jQuery(takenDomElement).css({left: "", top: ""});
-        jQApplicantDomElements.removeClass(overActiveSelector);
-
-        if (recipientDomElement === takenGroupDomElement) {
-          return;
-        }
-
-        var takenDomElementIndex = jQuery(takenDomElement).index();
-
-        var currentGroups = vm.state.currentGroups;
-        var takenFromGroup = currentGroups[jQuery(takenGroupDomElement).index()].nodes;
-        var takenServer = takenFromGroup[takenDomElementIndex];
-        var recipientGroup = currentGroups[jQuery(recipientDomElement).index()].nodes;
-
-        recipientGroup.push(takenServer);
-        recipientGroup.sort(hostnameComparator);
-
-        var indexInRecipientGroup = arrayObjectIndexOf(recipientGroup, takenServer.hostname, "hostname");
-        insertAt(jQuery(recipientDomElement).find(putIntoSelector), takenDomElement, indexInRecipientGroup);
-
-        takenFromGroup.splice(takenDomElementIndex, 1);
-
-
-        vm.disableApplyChangesBtn = _.isEqual(currentGroups, vm.state.initialGroups);
-        vm.disableAddGroupBtn = false;
-        vm.serverGroupsWarnig = false;
-        $scope.$apply();
-
-        takenGroupDomElement = undefined;
-        takenDomElement = undefined;
-      }
-      function onItemMoved() {
-        if (!takenDomElement) {
-          return;
-        }
-
-        recipientDomElement = detectAcceptingTarget(jQApplicantDomElements, takenDomElement) || prevRecipientGroupDomElement;
-        var recipientGroupIsChanged = prevRecipientGroupDomElement && recipientDomElement !== prevRecipientGroupDomElement;
-
-        if (recipientGroupIsChanged) {
-          jQuery(prevRecipientGroupDomElement).removeClass(overActiveSelector);
-        }
-
-        jQuery(recipientDomElement).addClass(overActiveSelector);
-
-        if (takenGroupDomElement === recipientDomElement) {
-          jQuery(takenDomElement).after(placeholder);
-        } else {
-          var currentGroups = vm.state.currentGroups;
-          var takenFromGroup = currentGroups[jQuery(takenGroupDomElement).index()].nodes;
-          var takenServer = takenFromGroup[jQuery(takenDomElement).index()];
-          var recipientGroup = _.cloneDeep(currentGroups[jQuery(recipientDomElement).index()].nodes);
-
-          recipientGroup.push(takenServer);
-          recipientGroup.sort(hostnameComparator);
-
-          var indexInRecipientGroup = arrayObjectIndexOf(recipientGroup, takenServer.hostname, "hostname");
-          insertAt(jQuery(recipientDomElement).find(putIntoSelector), placeholder, indexInRecipientGroup);
-        }
-
-        prevRecipientGroupDomElement = recipientDomElement;
-      }
-
-      function insertAt(node, elements, index) {
-        var array = jQuery.makeArray(node.children());
-        array.splice(index, 0, elements);
-        node.append(array);
-        return this;
-      }
-
-      function fullOffset(node) {
-        var offset = jQuery(node).offset();
-        offset.bottom = offset.top + jQuery(node).outerHeight();
-        offset.right = offset.left + jQuery(node).outerWidth();
-        return offset;
-      }
-
-      function arrayObjectIndexOf(array, searchTerm, property) {
-        var i;
-        var length = array.length;
-        for (i = 0; i < length; i++) {
-          if (array[i][property] === searchTerm) {
-            return i;
-          }
-        }
-      }
-
-      function mkComparatorByProp(propName, propComparator) {
-        propComparator = propComparator || defaultComparator;
-        return function (a,b) {
-          return propComparator(a[propName], b[propName]);
-        }
-      }
-
-      function detectAcceptingTarget(jQApplicantsList, node) {
-        var nodeOffset = fullOffset(node);
-
-        return (_.chain(jQApplicantsList).map(function (applicant) {
-          var applicantOffset = fullOffset(applicant);
-          return {
-            self: applicant,
-            sideA: Math.min(nodeOffset.right, applicantOffset.right) - Math.max(nodeOffset.left, applicantOffset.left),
-            sideB: Math.min(nodeOffset.bottom, applicantOffset.bottom) - Math.max(nodeOffset.top, applicantOffset.top)
-          }
-        }).filter(function (elementC) {
-          return elementC.sideA > 0 && elementC.sideB > 0;
-        }).max(function (elementC) {
-          return elementC.sideA  * elementC.sideB;
-        }).value() || {}).self;
-      }
     }
-
 })();
