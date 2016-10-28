59feda6fdb6fee543af1e5f7e39ffa1dae513b3d
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2016-10-28 14:56:41 -0700

    MB-21511 Stop dcp_replicator non-normally when socket closes.
    Since normal doesn't get propagated via links (in case the other
    process doesn't trap exits). Specifically, it breaks the logic in
    janitor_agent:wait_seqno_persisted where we link to replicator process
    to ensure that we hang rebalance forever if replicator dies.
    
    Change-Id: I311a3255607a39d29329c03747718f1a6059c275
    Reviewed-on: http://review.couchbase.org/69307
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>

--- src/dcp_proxy.erl
+++ src/dcp_proxy.erl
@@ -90,7 +90,7 @@ handle_info({tcp, Socket, Data}, #state{sock = Socket} = State) ->
 
 handle_info({tcp_closed, Socket}, State) ->
     ?log_error("Socket ~p was closed. Closing myself. State = ~p", [Socket, State]),
-    {stop, normal, State};
+    {stop, socket_closed, State};
 
 handle_info({'EXIT', _Pid, _Reason} = ExitSignal, State) ->
     ?log_error("killing myself due to exit signal: ~p", [ExitSignal]),
