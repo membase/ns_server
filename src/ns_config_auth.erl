3c7c8c5641531e6e1734a5158d6fd8bc68e58f8f
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2016-11-08 17:05:39 -0800

    Drop 2.5 support from ns_config_auth.
    Change-Id: Ife75e1ec94e48f360588f28b9842a2b7f2e60b91
    Reviewed-on: http://review.couchbase.org/70204
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>

--- src/ns_config_auth.erl
+++ src/ns_config_auth.erl
@@ -25,7 +25,6 @@
          get_password/1,
          credentials_changed/3,
          unset_credentials/1,
-         upgrade/1,
          get_creds/2,
          is_system_provisioned/0,
          is_system_provisioned/1,
@@ -38,21 +37,7 @@ get_key(ro_admin) ->
     read_only_user_creds.
 
 set_credentials(Role, User, Password) ->
-    case cluster_compat_mode:is_cluster_30() of
-        true ->
-            set_credentials_30(Role, User, Password);
-        false ->
-            set_credentials_old(Role, User, Password)
-    end.
-
-set_credentials_30(Role, User, Password) ->
     ns_config:set(get_key(Role), {User, {password, hash_password(Password)}}).
-
-set_credentials_old(admin, User, Password) ->
-    ns_config:set(rest_creds, [{creds,
-                                [{User, [{password, Password}]}]}]);
-set_credentials_old(ro_admin, User, Password) ->
-    ns_config:set(read_only_user_creds, {User, {password, Password}}).
 
 is_system_provisioned() ->
     is_system_provisioned(ns_config:latest()).
@@ -61,8 +46,6 @@ is_system_provisioned(Config) ->
     case ns_config:search(Config, get_key(admin)) of
         {value, {_U, _}} ->
             true;
-        {value, [{creds, [{_U, _}|_]}]} ->
-            true;
         _ ->
             false
     end.
@@ -70,30 +53,7 @@ is_system_provisioned(Config) ->
 get_user(special) ->
     "@";
 get_user(Role) ->
-    case cluster_compat_mode:is_cluster_30() of
-        true ->
-            get_user_30(Role);
-        false ->
-            get_user_old(Role)
-    end.
-
-get_user_30(Role) ->
     case ns_config:search(get_key(Role)) of
-        {value, {U, _}} ->
-            U;
-        _ ->
-            undefined
-    end.
-
-get_user_old(admin) ->
-    case ns_config:search_prop(ns_config:latest(), rest_creds, creds, []) of
-        [] ->
-            undefined;
-        [{U, _}|_] ->
-            U
-    end;
-get_user_old(ro_admin) ->
-    case ns_config:search(read_only_user_creds) of
         {value, {U, _}} ->
             U;
         _ ->
@@ -112,26 +72,9 @@ get_creds(Config, Role) ->
     end.
 
 credentials_changed(Role, User, Password) ->
-    case cluster_compat_mode:is_cluster_30() of
-        true ->
-            credentials_changed_30(Role, User, Password);
-        false ->
-            credentials_changed_old(Role, User, Password)
-    end.
-
-credentials_changed_30(Role, User, Password) ->
     case ns_config:search(get_key(Role)) of
         {value, {User, {password, {Salt, Mac}}}} ->
             hash_password(Salt, Password) =/= Mac;
-        _ ->
-            true
-    end.
-
-credentials_changed_old(admin, User, Password) ->
-    case ns_config:search_prop(ns_config:latest(), rest_creds, creds, []) of
-        [{U, Auth} | _] ->
-            P = proplists:get_value(password, Auth, ""),
-            User =/= U orelse Password =/= P;
         _ ->
             true
     end.
@@ -143,27 +86,6 @@ authenticate(Role, User, Password) ->
     authenticate_non_special(Role, User, Password).
 
 authenticate_non_special(Role, User, Password) ->
-    case cluster_compat_mode:is_cluster_30() of
-        true ->
-            authenticate_30(Role, User, Password);
-        false ->
-            authenticate_old(Role, User, Password)
-    end.
-
-authenticate_old(admin, User, Password) ->
-    case ns_config:search_prop(ns_config:latest(), rest_creds, creds, []) of
-        [{User, Auth} | _] ->
-            Password =:= proplists:get_value(password, Auth, "");
-        [] ->
-            %% An empty list means no login/password auth check.
-            true;
-        _ ->
-            false
-    end;
-authenticate_old(ro_admin, User, Password) ->
-    ns_config:search(read_only_user_creds) =:= {value, {User, {password, Password}}}.
-
-authenticate_30(Role, User, Password) ->
     do_authenticate(Role, ns_config:search(get_key(Role)), User, Password).
 
 do_authenticate(_Role, {value, {User, {password, {Salt, Mac}}}}, User, Password) ->
@@ -175,23 +97,6 @@ do_authenticate(_Role, _Creds, _User, _Password) ->
 
 unset_credentials(Role) ->
     ns_config:set(get_key(Role), null).
-
-upgrade(Config)->
-    case ns_config:search_prop(Config, rest_creds, creds, []) of
-        [{User, Auth} | _] ->
-            Password = proplists:get_value(password, Auth, ""),
-            [{set, rest_creds, {User, {password, hash_password(Password)}}}];
-        _ ->
-            [{set, rest_creds, null}]
-    end ++
-        case ns_config:search(Config, read_only_user_creds) of
-            {value, {ROUser, {password, ROPassword}}} ->
-                [{set, read_only_user_creds, {ROUser, {password, hash_password(ROPassword)}}}];
-            false ->
-                [{set, read_only_user_creds, null}];
-            {value, null} ->
-                []
-        end.
 
 hash_password(Password) ->
     Salt = crypto:rand_bytes(16),
