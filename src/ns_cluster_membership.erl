89450292931a3df76f4348452601e44bfd35fe0c
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2017-02-08 13:22:04 -0800

    MB-21966 Add index a topology aware service.
    Change-Id: I5026e62bf31312010bea4530f23557e09174e22f
    Reviewed-on: http://review.couchbase.org/73517
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- src/ns_cluster_membership.erl
+++ src/ns_cluster_membership.erl
@@ -248,7 +248,13 @@ default_services() ->
 topology_aware_services() ->
     case cluster_compat_mode:is_cluster_45() of
         true ->
-            [fts | maybe_example_service()];
+            Services = [fts | maybe_example_service()],
+            case cluster_compat_mode:is_cluster_spock() of
+                true ->
+                    [index | Services];
+                false ->
+                    Services
+            end;
         false ->
             []
     end.
