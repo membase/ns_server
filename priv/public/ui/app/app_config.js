d8aaea55933cdcd0e1428180efd25bf25956a1b2
Pavel Blagodov <stochmail@gmail.com>
2017-02-17 16:03:00 +0100

    disable ng-animate by default
    ngAnimate adds animation specific classes by default to all element
    with ng-if ng-show ng-repeat etc. This change allows us to specify a
    particular element that ready to be animated.
    
    Change-Id: I1a8a8d6bd65da0fd5f46ad32bab4c7b11b3e4a65
    Reviewed-on: http://review.couchbase.org/73818
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>

--- priv/public/ui/app/app_config.js
+++ priv/public/ui/app/app_config.js
@@ -5,11 +5,13 @@
     .module('app')
     .config(appConfig);
 
-  function appConfig($httpProvider, $stateProvider, $urlRouterProvider, $uibModalProvider, $transitionsProvider, $uibTooltipProvider) {
+  function appConfig($httpProvider, $stateProvider, $urlRouterProvider, $uibModalProvider, $transitionsProvider, $uibTooltipProvider, $animateProvider) {
     $httpProvider.defaults.headers.common['invalid-auth-response'] = 'on';
     $httpProvider.defaults.headers.common['Cache-Control'] = 'no-cache';
     $httpProvider.defaults.headers.common['Pragma'] = 'no-cache';
     $httpProvider.defaults.headers.common['ns-server-ui'] = 'yes';
+
+    $animateProvider.classNameFilter(/enable-ng-animation/);
 
     $uibModalProvider.options.backdrop = 'static';
     // When using a tooltip in an absolute positioned element,
