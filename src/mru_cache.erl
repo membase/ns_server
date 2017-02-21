02162deb84b2c025c1cd06d6d1e253f6c6f8fa0b
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2017-02-20 23:30:42 -0800

    Test simple mru_cache invariants.
    Change-Id: I0904794628842a9a88b9071bfe7a3fc28a03db90
    Reviewed-on: http://review.couchbase.org/74361
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/mru_cache.erl
+++ src/mru_cache.erl
@@ -15,6 +15,8 @@
 
 -module(mru_cache).
 
+-include("triq.hrl").
+
 -export([new/2, dispose/1,
          lookup/2, add/3, update/3, delete/2,
          flush/1]).
@@ -271,3 +273,136 @@ get_one(Table, Key) ->
 
 update_item(Table, Key, Value) ->
     ets:update_element(Table, Key, {2, Value}).
+
+%% triq stuff
+-define(CACHE, test_cache).
+-define(CACHE_SIZE, 8).
+-define(KEYS, [list_to_atom([C]) || C <- lists:seq($a, $a + 2 * ?CACHE_SIZE)]).
+
+%% generators for the cache operation
+key() ->
+    oneof(?KEYS).
+
+value() ->
+    int().
+
+ops() ->
+    list(op()).
+
+op() ->
+    frequency([{1, op_flush()},
+               {20, ?LET(Key, key(),
+                         frequency([{3, op_lookup(Key)},
+                                    {2, op_add(Key)},
+                                    {2, op_update(Key)},
+                                    {2, op_delete(Key)}]))}]).
+
+op_lookup(Key) ->
+    {lookup, Key}.
+
+op_add(Key) ->
+    {add, Key, value()}.
+
+op_update(Key) ->
+    {update, Key, value()}.
+
+op_delete(Key) ->
+    {delete, Key}.
+
+op_flush() ->
+    flush.
+
+%% routines to run and validate operations on cache
+-record(cache_state, {meta, items, stale_count, recent_count}).
+
+get_cache_state() ->
+    {Recent, Stale} = get_tables(?CACHE),
+    Items = lists:sort([{K, {recent, V}} || {K, V} <- ets:tab2list(Recent)] ++
+                           [{K, {stale, V}} || {K, V} <- ets:tab2list(Stale)]),
+    RecentCount = ets:info(Recent, size),
+    StaleCount = ets:info(Stale, size),
+    #cache_state{meta = ets:tab2list(?CACHE),
+                 items = Items,
+                 recent_count = RecentCount,
+                 stale_count = StaleCount}.
+
+dispatch_op(Op) when is_atom(Op) ->
+    dispatch_op({Op});
+dispatch_op(Op) when is_tuple(Op) ->
+    [F | Args] = tuple_to_list(Op),
+    R = erlang:apply(mru_cache, F, [?CACHE | Args]),
+    {R, get_cache_state()}.
+
+run_check_cache(Operations, Check) ->
+    run_check_cache(Operations,
+                    fun (Op, Result, State) ->
+                            case Check(Op, Result) of
+                                ok ->
+                                    {ok, State};
+                                Other ->
+                                    Other
+                            end
+                    end, ignored).
+
+run_check_cache(Operations, Check, InitState) ->
+    mru_cache:new(?CACHE, ?CACHE_SIZE),
+    try
+        check_cache_loop(Operations, [], Check, InitState)
+    after
+        mru_cache:dispose(?CACHE)
+    end.
+
+check_cache_loop([], _, _, _) ->
+    true;
+check_cache_loop([Op | Ops], Trace, Check, State) ->
+    NewTrace = [Op | Trace],
+
+    try dispatch_op(Op) of
+        Result ->
+            case Check(Op, Result, State) of
+                {ok, NewState} ->
+                    check_cache_loop(Ops, [Op | Trace], Check, NewState);
+                {failed, Diag} ->
+                    {failed, [{ops, lists:reverse(NewTrace)} | Diag]}
+            end
+    catch
+        T:E ->
+            Stack = erlang:get_stacktrace(),
+            {raised, [{what, {T, E}},
+                      {stack, Stack},
+                      {ops, lists:reverse(NewTrace)},
+                      {last_state, State},
+                      {cache_state, get_cache_state()}]}
+    end.
+
+check_invariants(Operations) ->
+    run_check_cache(
+      Operations,
+      fun (_, {_, State}) ->
+              #cache_state{meta = Meta,
+                           recent_count = Recent,
+                           stale_count = Stale,
+                           items = Items} = State,
+              Invariants =
+                  [{meta, (proplists:get_keys(Meta) --
+                               [max_size, tables]) =:= []},
+                   {max_size, Recent + Stale =< ?CACHE_SIZE},
+                   {recent, Recent < ?CACHE_SIZE},
+                   {no_dups, lists:sort(proplists:get_keys(Items)) =:=
+                        lists:usort(proplists:get_keys(Items))}],
+
+              Violated = [P || {_, false} = P <- Invariants],
+              case Violated of
+                  [] ->
+                      ok;
+                  _ ->
+                      {failed, [{cache_state, State},
+                                {violated_invariants, Violated}]}
+              end
+      end).
+
+%% triq properties
+prop_invariants_() ->
+    {?FORALL(Ops, ops(), check_invariants(Ops) =:= true),
+     [{iters, 1000},
+      {diag, fun check_invariants/1}]}.
