32556f2c80b9f9edb6f6b17dac910cfaeb927b36
Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
2017-03-06 13:09:03 -0800

    MB-23138 Increase ns_cluster:change_address timeout.
    Because on under-provisioned systems it may take longer than 5
    seconds.
    
    Change-Id: Ib80387deba3a8f0117d95a5a2d83f9fc6b3451cb
    Reviewed-on: http://review.couchbase.org/74726
    Tested-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Dave Finlay <dave.finlay@couchbase.com>

--- src/ns_cluster.erl
+++ src/ns_cluster.erl
@@ -27,6 +27,8 @@
 -define(ADD_NODE_TIMEOUT, ns_config:get_global_timeout({ns_cluster, add_node}, 160000)).
 -define(ENGAGE_TIMEOUT, ns_config:get_global_timeout({ns_cluster, engage}, 30000)).
 -define(COMPLETE_TIMEOUT, ns_config:get_global_timeout({ns_cluster, complete}, 120000)).
+-define(CHANGE_ADDRESS_TIMEOUT,
+        ns_config:get_global_timeout({ns_cluster, change_address}, 30000)).
 
 -define(cluster_log(Code, Fmt, Args),
         ale:xlog(?USER_LOGGER, ns_log_sink:get_loglevel(?MODULE, Code),
@@ -128,7 +130,7 @@ complete_join(NodeKVList) ->
 change_address(Address) ->
     case misc:is_good_address(Address) of
         ok ->
-            gen_server:call(?MODULE, {change_address, Address});
+            gen_server:call(?MODULE, {change_address, Address}, ?CHANGE_ADDRESS_TIMEOUT);
         Error ->
             Error
     end.
