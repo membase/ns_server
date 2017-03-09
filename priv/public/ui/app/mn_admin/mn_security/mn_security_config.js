e6842850b50e1508b548b3513e66f520bf096716
Pavel Blagodov <stochmail@gmail.com>
2017-03-09 17:02:22 +0300

    change rules to display Security tabs in 5.0
    So in order to see the tab the user should have
    cluster.admin.security.read permission or should
    use Enterprise Edition. When the above condition is
    reached we look at the compatibility mode version.
    If compatibility mode at least 5.0 we show up link
    to new "Users" tab otherwise the link leads
    to "Root Certificate".
    
    Also, the patch contains changes that allows
    CE users to use Security tab
    
    Change-Id: Ifbd84648f60d81788c403ac049d7f2f13e742b9c
    Reviewed-on: http://review.couchbase.org/74916
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- priv/public/ui/app/mn_admin/mn_security/mn_security_config.js
+++ priv/public/ui/app/mn_admin/mn_security/mn_security_config.js
@@ -42,8 +42,7 @@
         controller: "mnUserRolesController as userRolesCtl",
         templateUrl: "app/mn_admin/mn_security/mn_user_roles/mn_user_roles.html",
         data: {
-          compat: "atLeast50",
-          enterprise: true
+          compat: "atLeast50"
         }
       })
       .state('app.admin.security.rootCertificate', {
