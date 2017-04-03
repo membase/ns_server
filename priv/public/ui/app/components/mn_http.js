a68713df691ca342de23797ab44549e2d562dd17
Pavel Blagodov <stochmail@gmail.com>
2017-04-03 11:39:35 +0700

    MB-23244: encode form data in accordance with x-www-form-urlencoded...
    ...algorithm
    
    Change-Id: I654f4aba8b2babaabd695436c761a2271e664409
    Reviewed-on: http://review.couchbase.org/75949
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>

--- priv/public/ui/app/components/mn_http.js
+++ priv/public/ui/app/components/mn_http.js
@@ -62,7 +62,7 @@
           if (!mnHttpConfig.isNotForm) {
             config.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
             if (!angular.isString(config.data)) {
-              config.data = $httpParamSerializerJQLike(config.data);
+              config.data = jQueryLikeParamSerializer(config.data);
             }
           }
         break;
@@ -79,6 +79,46 @@
         timeoutID = $timeout(cancel("timeout"), timeout);
       }
       return config;
+    }
+
+    function serializeValue(v) {
+      if (angular.isObject(v)) {
+        return angular.isDate(v) ? v.toISOString() : angular.toJson(v);
+      }
+      return v;
+    }
+    //function is borrowed from the Angular source code because we want to
+    //use $httpParamSerializerJQLik but with properly encoded params via
+    //encodeURIComponent since it uses correct application/x-www-form-urlencoded
+    //encoding algorithm, in accordance with
+    //https://www.w3.org/TR/html5/forms.html#url-encoded-form-data
+    function jQueryLikeParamSerializer(params) {
+      if (!params) {
+        return '';
+      }
+      var parts = [];
+      serialize(params, '', true);
+      return parts.join('&');
+
+      function serialize(toSerialize, prefix, topLevel) {
+        if (toSerialize === null || angular.isUndefined(toSerialize)) {
+          return;
+        }
+        if (angular.isArray(toSerialize)) {
+          angular.forEach(toSerialize, function (value, index) {
+            serialize(value, prefix + '[' + (angular.isObject(value) ? index : '') + ']');
+          });
+        } else if (angular.isObject(toSerialize) && !angular.isDate(toSerialize)) {
+          angular.forEach(toSerialize, function (value, key) {
+            serialize(value, prefix +
+                (topLevel ? '' : '[') +
+                key +
+                (topLevel ? '' : ']'));
+          });
+        } else {
+          parts.push(encodeURIComponent(prefix) + '=' + encodeURIComponent(serializeValue(toSerialize)));
+        }
+      }
     }
 
     function clearOnResponse(response) {
