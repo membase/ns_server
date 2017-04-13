3e0561f11a6b93543c868002e81d80a967c95e25
Artem Stemkovski <astemkov@gmail.com>
2017-04-12 21:20:03 -0700

    make settings parsers global functions
    Change-Id: I4f319d80410ae1741fa3ac86988678c40ab365af
    Reviewed-on: http://review.couchbase.org/76754
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/menelaus_web_settings.erl
+++ src/menelaus_web_settings.erl
@@ -23,59 +23,58 @@
          handle_internal_settings/1,
          handle_internal_settings_post/1]).
 
+get_bool("true") ->
+    {ok, true};
+get_bool("false") ->
+    {ok, false};
+get_bool(_) ->
+    invalid.
+
+get_number(Min, Max) ->
+    fun (SV) ->
+            menelaus_util:parse_validate_number(SV, Min, Max)
+    end.
+
+get_number(Min, Max, Default) ->
+    fun (SV) ->
+            case SV of
+                "" ->
+                    {ok, Default};
+                _ ->
+                    menelaus_util:parse_validate_number(SV, Min, Max)
+            end
+    end.
+
+get_string(SV) ->
+    {ok, list_to_binary(string:strip(SV))}.
+
 internal_settings_conf() ->
-    GetBool = fun (SV) ->
-                      case SV of
-                          "true" -> {ok, true};
-                          "false" -> {ok, false};
-                          _ -> invalid
-                      end
-              end,
-
-    GetNumber = fun (Min, Max) ->
-                        fun (SV) ->
-                                menelaus_util:parse_validate_number(SV, Min, Max)
-                        end
-                end,
-    GetNumberOrEmpty = fun (Min, Max, Empty) ->
-                               fun (SV) ->
-                                       case SV of
-                                           "" -> Empty;
-                                           _ ->
-                                               menelaus_util:parse_validate_number(SV, Min, Max)
-                                       end
-                               end
-                       end,
-    GetString = fun (SV) ->
-                        {ok, list_to_binary(string:strip(SV))}
-                end,
-
-    [{index_aware_rebalance_disabled, indexAwareRebalanceDisabled, false, GetBool},
-     {rebalance_index_waiting_disabled, rebalanceIndexWaitingDisabled, false, GetBool},
-     {index_pausing_disabled, rebalanceIndexPausingDisabled, false, GetBool},
-     {rebalance_ignore_view_compactions, rebalanceIgnoreViewCompactions, false, GetBool},
-     {rebalance_moves_per_node, rebalanceMovesPerNode, 1, GetNumber(1, 1024)},
-     {rebalance_moves_before_compaction, rebalanceMovesBeforeCompaction, 64, GetNumber(1, 1024)},
-     {{couchdb, max_parallel_indexers}, maxParallelIndexers, <<>>, GetNumber(1, 1024)},
-     {{couchdb, max_parallel_replica_indexers}, maxParallelReplicaIndexers, <<>>, GetNumber(1, 1024)},
-     {max_bucket_count, maxBucketCount, 10, GetNumber(1, 8192)},
-     {{request_limit, rest}, restRequestLimit, undefined, GetNumberOrEmpty(0, 99999, {ok, undefined})},
-     {{request_limit, capi}, capiRequestLimit, undefined, GetNumberOrEmpty(0, 99999, {ok, undefined})},
+    [{index_aware_rebalance_disabled, indexAwareRebalanceDisabled, false, fun get_bool/1},
+     {rebalance_index_waiting_disabled, rebalanceIndexWaitingDisabled, false, fun get_bool/1},
+     {index_pausing_disabled, rebalanceIndexPausingDisabled, false, fun get_bool/1},
+     {rebalance_ignore_view_compactions, rebalanceIgnoreViewCompactions, false, fun get_bool/1},
+     {rebalance_moves_per_node, rebalanceMovesPerNode, 1, get_number(1, 1024)},
+     {rebalance_moves_before_compaction, rebalanceMovesBeforeCompaction, 64, get_number(1, 1024)},
+     {{couchdb, max_parallel_indexers}, maxParallelIndexers, <<>>, get_number(1, 1024)},
+     {{couchdb, max_parallel_replica_indexers}, maxParallelReplicaIndexers, <<>>, get_number(1, 1024)},
+     {max_bucket_count, maxBucketCount, 10, get_number(1, 8192)},
+     {{request_limit, rest}, restRequestLimit, undefined, get_number(0, 99999, undefined)},
+     {{request_limit, capi}, capiRequestLimit, undefined, get_number(0, 99999, undefined)},
      {drop_request_memory_threshold_mib, dropRequestMemoryThresholdMiB, undefined,
-      GetNumberOrEmpty(0, 99999, {ok, undefined})},
-     {gotraceback, gotraceback, <<"crash">>, GetString},
-     {{auto_failover_disabled, index}, indexAutoFailoverDisabled, true, GetBool},
-     {{cert, use_sha1}, certUseSha1, false, GetBool}] ++
+      get_number(0, 99999, undefined)},
+     {gotraceback, gotraceback, <<"crash">>, fun get_string/1},
+     {{auto_failover_disabled, index}, indexAutoFailoverDisabled, true, fun get_bool/1},
+     {{cert, use_sha1}, certUseSha1, false, fun get_bool/1}] ++
         case cluster_compat_mode:is_goxdcr_enabled() of
             false ->
-                [{{xdcr, max_concurrent_reps}, xdcrMaxConcurrentReps, 32, GetNumber(1, 256)},
-                 {{xdcr, checkpoint_interval}, xdcrCheckpointInterval, 1800, GetNumber(60, 14400)},
-                 {{xdcr, worker_batch_size}, xdcrWorkerBatchSize, 500, GetNumber(500, 10000)},
-                 {{xdcr, doc_batch_size_kb}, xdcrDocBatchSizeKb, 2048, GetNumber(10, 100000)},
-                 {{xdcr, failure_restart_interval}, xdcrFailureRestartInterval, 30, GetNumber(1, 300)},
+                [{{xdcr, max_concurrent_reps}, xdcrMaxConcurrentReps, 32, get_number(1, 256)},
+                 {{xdcr, checkpoint_interval}, xdcrCheckpointInterval, 1800, get_number(60, 14400)},
+                 {{xdcr, worker_batch_size}, xdcrWorkerBatchSize, 500, get_number(500, 10000)},
+                 {{xdcr, doc_batch_size_kb}, xdcrDocBatchSizeKb, 2048, get_number(10, 100000)},
+                 {{xdcr, failure_restart_interval}, xdcrFailureRestartInterval, 30, get_number(1, 300)},
                  {{xdcr, optimistic_replication_threshold}, xdcrOptimisticReplicationThreshold, 256,
-                  GetNumberOrEmpty(0, 20*1024*1024, undefined)},
-                 {xdcr_anticipatory_delay, xdcrAnticipatoryDelay, 0, GetNumber(0, 99999)}];
+                  get_number(0, 20*1024*1024, undefined)},
+                 {xdcr_anticipatory_delay, xdcrAnticipatoryDelay, 0, get_number(0, 99999)}];
             true ->
                 []
         end.
