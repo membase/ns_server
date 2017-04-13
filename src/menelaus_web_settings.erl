5c9e4a913119f1ca0e84986ee55a253931947509
Artem Stemkovski <astemkov@gmail.com>
2017-04-13 16:14:45 -0700

    fix typo
    Change-Id: I4ab7b8b4e4a22146132c23579aba32b9a381ffb8
    Reviewed-on: http://review.couchbase.org/76809
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>
    Tested-by: Artem Stemkovski <artem@couchbase.com>

--- src/menelaus_web_settings.erl
+++ src/menelaus_web_settings.erl
@@ -49,7 +49,7 @@ get_string(SV) ->
     {ok, list_to_binary(string:strip(SV))}.
 
 conf(security) ->
-    [{disable_ui_over_http, diasableUIOverHttp, false, fun get_bool/1}];
+    [{disable_ui_over_http, disableUIOverHttp, false, fun get_bool/1}];
 conf(internal) ->
     [{index_aware_rebalance_disabled, indexAwareRebalanceDisabled, false, fun get_bool/1},
      {rebalance_index_waiting_disabled, rebalanceIndexWaitingDisabled, false, fun get_bool/1},
