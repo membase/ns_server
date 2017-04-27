e9e734b386d585c5ca55cb4ae49950b55c10caf3
Pavel Blagodov <stochmail@gmail.com>
2017-04-27 20:19:43 +0700

    MB-23244: app-classic: encode form data in accordance with ...
    ... x-www-form-urlencoded algorithm
    
    Change-Id: I2a8606eb4a65b30d852979a7fb90b88133e069c0
    Reviewed-on: http://review.couchbase.org/77441
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>

--- priv/public/ui/app-classic/components/mn_http.js
+++ priv/public/ui/app-classic/components/mn_http.js
@@ -62,12 +62,7 @@
           if (!mnHttpConfig.isNotForm) {
             config.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
             if (!angular.isString(config.data)) {
-              //Angular uses it's own method for encoding uri component which called
-              //encodeUriQuery. They need a custom method because encodeURIComponent
-              //is too aggressive and encodes stuff that doesn't have to be encoded per
-              //http://tools.ietf.org/html/rfc3986. However semicolon is important symbol
-              //for mochiweb, so we should encode it.
-              config.data = $httpParamSerializerJQLike(config.data).replace(/;/gi, '%3B');
+              config.data = jQueryLikeParamSerializer(config.data);
             }
           }
         break;
@@ -84,6 +79,46 @@
         timeoutID = $timeout(cancel("timeout"), timeout);
       }
       return config;
+    }
+
+    function serializeValue(v) {
+      if (angular.isObject(v)) {
+        return angular.isDate(v) ? v.toISOString() : angular.toJson(v);
+      }
+      return v;
+    }
+    //function is borrowed from the Angular source code because we want to
+    //use $httpParamSerializerJQLik but with properly encoded params via
+    //encodeURIComponent since it uses correct application/x-www-form-urlencoded
+    //encoding algorithm, in accordance with
+    //https://www.w3.org/TR/html5/forms.html#url-encoded-form-data
+    function jQueryLikeParamSerializer(params) {
+      if (!params) {
+        return '';
+      }
+      var parts = [];
+      serialize(params, '', true);
+      return parts.join('&');
+
+      function serialize(toSerialize, prefix, topLevel) {
+        if (toSerialize === null || angular.isUndefined(toSerialize)) {
+          return;
+        }
+        if (angular.isArray(toSerialize)) {
+          angular.forEach(toSerialize, function (value, index) {
+            serialize(value, prefix + '[' + (angular.isObject(value) ? index : '') + ']');
+          });
+        } else if (angular.isObject(toSerialize) && !angular.isDate(toSerialize)) {
+          angular.forEach(toSerialize, function (value, key) {
+            serialize(value, prefix +
+                (topLevel ? '' : '[') +
+                key +
+                (topLevel ? '' : ']'));
+          });
+        } else {
+          parts.push(encodeURIComponent(prefix) + '=' + encodeURIComponent(serializeValue(toSerialize)));
+        }
+      }
     }
 
     function clearOnResponse(response) {
