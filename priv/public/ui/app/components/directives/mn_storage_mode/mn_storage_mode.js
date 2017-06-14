6b07a6aa8756f39f6be5f46d80e6c158d81452f6
Pavel Blagodov <stochmail@gmail.com>
2017-06-14 12:10:22 +0300

    improve mnFormatStorageModeError filter
    Change-Id: Ib387daa1ea76aa028d351ce20323b5f21b6eebbd
    Reviewed-on: http://review.couchbase.org/79469
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>

--- priv/public/ui/app/components/directives/mn_storage_mode/mn_storage_mode.js
+++ priv/public/ui/app/components/directives/mn_storage_mode/mn_storage_mode.js
@@ -8,11 +8,20 @@
 
   function mnFormatStorageModeError() {
     return function (error) {
-      switch (error) {
-        case "storageMode must be one of forestdb, memory_optimized, plasma":
-          return "please choose an index storage mode";
-        default:
-          return error;
+      if (!error) {
+        return;
+      }
+      var errorCode =
+          error.indexOf("Storage mode cannot be set to") > -1 ? 1 :
+          error.indexOf("storageMode must be one of") > -1 ? 2 :
+          0;
+      switch (errorCode) {
+      case 1:
+        return "please choose another index storage mode";
+      case 2:
+        return "please choose an index storage mode";
+      default:
+        return error;
       }
     };
   }
