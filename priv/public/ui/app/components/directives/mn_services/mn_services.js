a0485d32e5ad8db99143172d8f544a32d0f734cd
Pavel Blagodov <stochmail@gmail.com>
2017-06-22 15:39:09 +0300

    MB-24690: do not disable services checkboxes on CE
    so the user can see server side error message and choose one of
    possible combination
    
    Change-Id: I56f6fa99f148e83db656bed680e3f5c657b1cccf
    Reviewed-on: http://review.couchbase.org/79887
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>

--- priv/public/ui/app/components/directives/mn_services/mn_services.js
+++ priv/public/ui/app/components/directives/mn_services/mn_services.js
@@ -11,21 +11,9 @@
       scope: {
         config: '=mnServices'
       },
-      templateUrl: 'app/components/directives/mn_services/mn_services.html',
-      controller: controller
+      templateUrl: 'app/components/directives/mn_services/mn_services.html'
     };
 
     return mnServices;
-
-    function controller($scope) {
-      mnPools.get().then(function (pool) {
-        $scope.isEnterprise = pool.isEnterprise;
-        $scope.onChange = function (value, model) {
-          if (!pool.isEnterprise) {
-            $scope.config.services.model[model] = value;
-          }
-        };
-      });
-    }
   }
 })();
