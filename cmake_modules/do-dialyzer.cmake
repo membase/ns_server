5969e6c7260d47344487ee0a2a4e575d4d0349f7
Amit Krishnan <krish.amit@gmail.com>
2017-07-21 15:08:03 -0700

    Update make dialyzer target to use dialyzer binary...
    ...from installation root and not from the system path.
    
    Change-Id: I81e390640fe9bb8deb1a409e5b74278c0317623d
    Reviewed-on: http://review.couchbase.org/81261
    Tested-by: Build Bot <build@couchbase.com>
    Tested-by: Amit Krishnan <krish.amit@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- cmake_modules/do-dialyzer.cmake
+++ cmake_modules/do-dialyzer.cmake
@@ -4,14 +4,14 @@ GET_FILENAME_COMPONENT (_couchdb_bin_dir "${COUCHDB_BIN_DIR}" REALPATH)
 IF (NOT EXISTS "${COUCHBASE_PLT}")
   MESSAGE ("Generating ${COUCHBASE_PLT}...")
   EXECUTE_PROCESS (COMMAND "${CMAKE_COMMAND}" -E echo
-    dialyzer --output_plt "${COUCHBASE_PLT}" --build_plt
+    "${DIALYZER_EXECUTABLE}" --output_plt "${COUCHBASE_PLT}" --build_plt
     --apps compiler crypto erts inets kernel os_mon sasl ssl stdlib xmerl
     ${_couchdb_bin_dir}/src/mochiweb
     ${_couchdb_bin_dir}/src/snappy ${_couchdb_bin_dir}/src/etap
     ${_couchdb_bin_dir}/src/lhttpc
     ${_couchdb_bin_dir}/src/erlang-oauth deps/gen_smtp/ebin)
 
-  EXECUTE_PROCESS (COMMAND dialyzer --output_plt "${COUCHBASE_PLT}" --build_plt
+  EXECUTE_PROCESS (COMMAND "${DIALYZER_EXECUTABLE}" --output_plt "${COUCHBASE_PLT}" --build_plt
     --apps compiler crypto erts inets kernel os_mon sasl ssl stdlib xmerl
     ${_couchdb_bin_dir}/src/mochiweb
     ${_couchdb_bin_dir}/src/snappy ${_couchdb_bin_dir}/src/etap
@@ -29,7 +29,7 @@ STRING (REGEX REPLACE "deps/ns_couchdb/ebin/couch_log.beam\;?" "" couchdb_beamfi
 # If you update the dialyzer command, please also update this echo
 # command so it displays what is invoked. Yes, this is annoying.
 EXECUTE_PROCESS (COMMAND "${CMAKE_COMMAND}" -E echo
-  dialyzer --plt "${COUCHBASE_PLT}" ${DIALYZER_FLAGS}
+  "${DIALYZER_EXECUTABLE}" --plt "${COUCHBASE_PLT}" ${DIALYZER_FLAGS}
   --apps ${beamfiles}
   deps/ale/ebin
   ${_couchdb_bin_dir}/src/couchdb ${_couchdb_bin_dir}/src/couch_set_view ${_couchdb_bin_dir}/src/couch_view_parser
@@ -39,7 +39,7 @@ EXECUTE_PROCESS (COMMAND "${CMAKE_COMMAND}" -E echo
   deps/ns_ssl_proxy/ebin
   ${couchdb_beamfiles})
 EXECUTE_PROCESS (RESULT_VARIABLE _failure
-  COMMAND dialyzer --plt "${COUCHBASE_PLT}" ${DIALYZER_FLAGS}
+  COMMAND "${DIALYZER_EXECUTABLE}" --plt "${COUCHBASE_PLT}" ${DIALYZER_FLAGS}
   --apps ${beamfiles}
   deps/ale/ebin
   ${_couchdb_bin_dir}/src/couchdb ${_couchdb_bin_dir}/src/couch_set_view ${_couchdb_bin_dir}/src/couch_view_parser
