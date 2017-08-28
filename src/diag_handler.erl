dafd2137e6611fc2774c0167767c3ece897648c4
Artem Stemkovski <astemkov@gmail.com>
2017-08-28 09:23:00 -0700

    MB-25682 pretty-print each ets table in a separate process
    eliminates process memory consumption buildup during cbcollect
    (backported from Vulcan - http://review.couchbase.org/83239)
    
    Change-Id: I34f271314c1ab97c3205d94749294a9fcf1c32a7
    Reviewed-on: http://review.couchbase.org/84179
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

--- src/diag_handler.erl
+++ src/diag_handler.erl
@@ -563,6 +563,12 @@ do_handle_per_node_stats(Resp, Node, PerNodeDiag)->
     do_handle_per_node_ets_tables(Resp, Node, DiagNoStats).
 
 print_ets_table(Resp, Node, Key, Table, Info, Values) ->
+    misc:executing_on_new_process(
+      fun () ->
+              do_print_ets_table(Resp, Node, Key, Table, Info, Values)
+      end).
+
+do_print_ets_table(Resp, Node, Key, Table, Info, Values) ->
     write_chunk_format(Resp, "per_node_~p(~p, ~p) =~n",
                        [Key, Node, Table]),
     case Info of
@@ -593,15 +599,12 @@ write_ets_tables(Resp, Node, Key, PerNodeDiag) ->
                         [{'_', [EtsTables0]}]
                 end,
 
-    misc:executing_on_new_process(
-      fun () ->
-              lists:foreach(
-                fun ({{Table, Info}, Values}) ->
-                        print_ets_table(Resp, Node, Key, Table, Info, Values);
-                    ({Table, Values}) ->
-                        print_ets_table(Resp, Node, Key, Table, [], Values)
-                end, EtsTables)
-      end),
+    lists:foreach(
+      fun ({{Table, Info}, Values}) ->
+              print_ets_table(Resp, Node, Key, Table, Info, Values);
+          ({Table, Values}) ->
+              print_ets_table(Resp, Node, Key, Table, [], Values)
+      end, EtsTables),
 
     lists:keydelete(Key, 1, PerNodeDiag).
 
