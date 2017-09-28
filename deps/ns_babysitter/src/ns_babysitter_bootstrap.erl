ed5bd4a502b632f24606f0e95dfe2fa0d6149eb1
Ajit Yagaty <ajit.yagaty@couchbase.com>
2017-09-28 13:41:41 -0700

    MB-10409: Init script changes to pass IPV6 param.
    The static_config file has been chosen as the medium to convey
    if IPv6 mode has been enabled by user. The init script invokes
    an Erlang helper function to indicate the value of the term
    "ipv6" in the static_config file. If the "ipv6" flag has been
    set to "true" then the init script would do the following:
    
    [1] Set the "name" parameter to either "babysitter_of_ns_1@127.0.0.1"
        or "babysitter_of_ns_1@::1".
    
    [2] Set the "proto_dist" parameter to either "inet_tcp" or "inet6_tcp".
    
    The terms in static_config file are provided as application env
    variables. The application environment variable "ipv6" will be
    used internally by the ns_server components to handle the mode
    appropriately.
    
    Change-Id: Ic19ac9e182c58a386c2e1a0b089b2839db195fa6
    Reviewed-on: http://review.couchbase.org/83843
    Reviewed-by: Artem Stemkovski <artem@couchbase.com>
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>

--- deps/ns_babysitter/src/ns_babysitter_bootstrap.erl
+++ deps/ns_babysitter/src/ns_babysitter_bootstrap.erl
@@ -1,6 +1,7 @@
 -module(ns_babysitter_bootstrap).
 
 -export([start/0, stop/0, get_quick_stop/0, remote_stop/1, override_resolver/0]).
+-export([ipv6_from_static_config/1]).
 
 -include("ns_common.hrl").
 
@@ -44,3 +45,32 @@ quick_stop() ->
 override_resolver() ->
     inet_db:set_lookup([file, dns]),
     start().
+
+%% This function will be called by the init script to determine
+%% the networking mode to start with.
+ipv6_from_static_config(StaticConfigPath) ->
+    Terms = case file:consult(StaticConfigPath) of
+                {ok, T} when is_list(T) ->
+                    T;
+                Error ->
+                    {error, Error}
+            end,
+
+    ExitStatus = case Terms of
+                     {error, _} ->
+                         io:format("Failed to read static config: ~p. "
+                                   "It must be a readable file with list of pairs.",
+                                   [StaticConfigPath]),
+                         1;
+                     _ ->
+                         case [true || {K, V} <- Terms, K =:= ipv6 andalso V =:= true] of
+                             [true] ->
+                                 io:format("true");
+                             _ ->
+                                 io:format("false")
+                         end,
+
+                         0
+                 end,
+
+    init:stop(ExitStatus).
