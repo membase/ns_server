ba7d13efe4936af7132802b352d44b56d8e899bf
Pavel Blagodov <stochmail@gmail.com>
2017-11-30 16:41:01 +0300

    MB-25475: add finish with defaults button
    Change-Id: Ia0000e78f1b36b3f1b803373e301e18e03e50bd5
    Reviewed-on: http://review.couchbase.org/86189
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>
    Tested-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app-new/wizard/mn-terms-and-conditions-component.js
+++ priv/public/ui/app-new/wizard/mn-terms-and-conditions-component.js
@@ -4,6 +4,8 @@ mn.components.MnTermsAndConditions =
   (function () {
     "use strict";
 
+    mn.helper.extends(MnTermsAndConditions, mn.helper.MnDestroyableComponent);
+
     MnTermsAndConditions.annotations = [
       new ng.core.Component({
         templateUrl: "app-new/wizard/mn-terms-and-conditions.html"
@@ -13,20 +15,27 @@ mn.components.MnTermsAndConditions =
     MnTermsAndConditions.parameters = [
       mn.services.MnWizard,
       mn.services.MnPools,
+      mn.services.MnApp,
       mn.services.MnAuth,
       window['@uirouter/angular'].UIRouter
     ];
 
     MnTermsAndConditions.prototype.onSubmit = onSubmit;
     MnTermsAndConditions.prototype.registerChange = registerChange;
+    MnTermsAndConditions.prototype.validateAgreeFlag = validateAgreeFlag;
+    MnTermsAndConditions.prototype.finishWithDefaut = finishWithDefaut;
 
     return MnTermsAndConditions;
 
-    function MnTermsAndConditions(mnWizardService, mnPoolsService, mnAuthService, uiRouter) {
+    function MnTermsAndConditions(mnWizardService, mnPoolsService, mnAppService, mnAuthService, uiRouter) {
+      mn.helper.MnDestroyableComponent.call(this);
+
       this.focusField = true;
 
       this.uiRouter = uiRouter;
       this.isEnterprise = mnPoolsService.stream.isEnterprise;
+      this.wizardForm = mnWizardService.wizardForm;
+      this.initialValues = mnWizardService.initialValues;
 
       this.license =
         mnPoolsService
@@ -52,6 +61,80 @@ mn.components.MnTermsAndConditions =
       this.termsForm.get("agree").setValue(false);
 
       this.registerChange();
+      this.groupHttp = mnWizardService.stream.groupHttp;
+      this.mnAppLoding = mnAppService.stream.loading;
+
+      this.groupHttp
+        .loading
+        .merge(mnWizardService.stream.secondGroupHttp.loading)
+        .takeUntil(this.mnDestroy)
+        .subscribe(this.mnAppLoding.next.bind(this.mnAppLoding));
+
+      this.groupHttp
+        .success
+        .takeUntil(this.mnDestroy)
+        .subscribe(function (result) {
+          mnWizardService.stream.secondGroupHttp.post({
+            indexesHttp: {
+              storageMode: mnWizardService.initialValues.storageMode
+            },
+            authHttp: [mnWizardService.wizardForm.newCluster.value.user, false]
+          });
+        });
+
+      mnWizardService.stream.secondGroupHttp
+        .success
+        .takeUntil(this.mnDestroy)
+        .subscribe(function () {
+          mnAuthService.stream.loginHttp.post(mnWizardService.getUserCreds());
+        });
+
+      mnAuthService.stream.loginHttp
+        .success
+        .takeUntil(this.mnDestroy)
+        .subscribe(function () {
+          if (mnWizardService.wizardForm.termsAndConditions.get("register").value) {
+            mnWizardService.stream.emailHttp.response.first().subscribe();
+            mnWizardService.stream.emailHttp.post([
+              mnWizardService.wizardForm.termsAndConditions.get("user").value,
+              mnWizardService.initialValues.implementationVersion
+            ]);
+          }
+          uiRouter.urlRouter.sync();
+        });
+    }
+
+    function validateAgreeFlag() {
+      var error = this.termsForm.get("agree").value ? null : {required: true};
+      this.termsForm.get("agree").setErrors(error);
+    }
+
+    function finishWithDefaut() {
+      this.submitted = true;
+
+      if (this.mnAppLoding.getValue()) {
+        return;
+      }
+
+      this.validateAgreeFlag();
+      this.groupHttp.clearErrors();
+
+      if (this.termsForm.invalid) {
+        return;
+      }
+
+      this.groupHttp.post({
+        poolsDefaultHttp: [{
+          clusterName: this.wizardForm.newCluster.get("clusterName").value
+        }, false],
+        servicesHttp: {
+          services: 'kv,index,fts,n1ql,eventing',
+          setDefaultMemQuotas : true
+        },
+        diskStorageHttp: this.initialValues.clusterStorage,
+        hostnameHttp: this.initialValues.hostname,
+        statsHttp: true
+      });
     }
 
     function registerChange() {
@@ -60,8 +143,7 @@ mn.components.MnTermsAndConditions =
 
     function onSubmit(user) {
       this.submitted = true;
-      var error = this.termsForm.get("agree").value ? null : {required: true};
-      this.termsForm.get("agree").setErrors(error);
+      this.validateAgreeFlag();
 
       if (this.termsForm.invalid) {
         return;
