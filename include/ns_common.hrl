510dea14cf079f501488eba897df7dc0e8861170
Artem Stemkovski <astemkov@gmail.com>
2017-12-14 15:25:25 -0800

    renumber Vulcan to 5.5
    Change-Id: I2829ff5842e5131bf107802ffe9f026543ff10c9
    Reviewed-on: http://review.couchbase.org/86862
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Poonam Dhavale <poonam@couchbase.com>

--- include/ns_common.hrl
+++ include/ns_common.hrl
@@ -223,11 +223,11 @@
 -define(VERSION_46, [4, 6]).
 -define(VERSION_50, [5, 0]).
 
--define(VULCAN_VERSION_NUM, [5, 1]).
+-define(VULCAN_VERSION_NUM, [5, 5]).
 
 %% Points to latest release
 -define(LATEST_VERSION_NUM, ?VULCAN_VERSION_NUM).
--define(MASTER_ADVERTISED_VERSION, [5, 1, 0]).
+-define(MASTER_ADVERTISED_VERSION, [5, 5, 0]).
 
 -define(DEFAULT_UI_COMPAT_VERSION, ?VERSION_45).
 
