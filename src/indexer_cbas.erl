3fc427d70f20d98c8ff125c16d01fd00f2d75767
Artem Stemkovski <astemkov@gmail.com>
2018-01-12 14:42:22 -0800

    do not start index_status_keeper for cbas
    because there's no status to keep
    
    Change-Id: I8e45e12f525bc7ff5925a54ba9364632c9910363
    Reviewed-on: http://review.couchbase.org/88054
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Poonam Dhavale <poonam@couchbase.com>

--- src/indexer_cbas.erl
+++ src/indexer_cbas.erl
@@ -15,33 +15,16 @@
 %%
 -module(indexer_cbas).
 
--export([start_keeper/0, get_indexes/0]).
-
--export([get_type/0, get_remote_indexes/1, get_local_status/0, restart/0, get_status_mapping/0,
+-export([get_type/0, restart/0,
          get_gauges/0, get_counters/0, get_computed/0, grab_stats/0, prefix/0,
          per_index_stat/2, global_index_stat/1, compute_gauges/1, get_service_gauges/0,
          get_service_counters/0, service_stat_prefix/0, service_event_name/0, compute_service_gauges/1]).
 
-get_indexes() ->
-    index_status_keeper:get_indexes(?MODULE).
-
 get_type() ->
     cbas.
 
-get_remote_indexes(_Node) ->
-    [].
-
-get_local_status() ->
-    {ok, {[{<<"status">>, []}, {<<"code">>, <<"success">>}]}}.
-
 restart() ->
     [].
-
-get_status_mapping() ->
-    [].
-
-start_keeper() ->
-    index_status_keeper:start_link(?MODULE).
 
 get_gauges() ->
     ['incoming-records-count-total', 'failed-at-parser-records-count-total'].
