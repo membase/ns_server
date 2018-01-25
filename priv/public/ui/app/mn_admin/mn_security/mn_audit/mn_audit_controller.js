a34a9a461d14e372fe6bec8ca74d0d1eb3c74f33
Rob Ashcom <rob.ashcom@gmail.com>
2018-01-25 08:48:15 -0800

    MB-26622 Auditing Tab
    - added log rotation size control
    - added users whitelist
    - added repeatable component for modules + events
    
    Change-Id: I6cc1df959765c1484a52d6fa206d4369f0cc50c2
    Reviewed-on: http://review.couchbase.org/88392
    Tested-by: Pavel Blagodov <stochmail@gmail.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Pavel Blagodov <stochmail@gmail.com>

--- priv/public/ui/app/mn_admin/mn_security/mn_audit/mn_audit_controller.js
+++ priv/public/ui/app/mn_admin/mn_security/mn_audit/mn_audit_controller.js
@@ -4,21 +4,51 @@
   angular.module('mnAudit', [
     'mnAuditService',
     'mnHelper',
-    'mnPromiseHelper'
+    'mnPromiseHelper',
+    'mnFilters'
   ]).controller('mnAuditController', mnAuditController);
 
   function mnAuditController($scope, mnAuditService, mnPromiseHelper, mnHelper) {
     var vm = this;
 
     vm.submit = submit;
+    vm.toggleAll = toggleAll;
+    vm.mapNames = mapNames;
+    vm.findEnabled = findEnabled;
 
     activate();
 
+    function mapNames(name) {
+      switch (name) {
+      case "auditd":
+        return "Audit";
+      case "ns_server":
+        return "Server";
+      case "n1ql":
+      case "xdcr":
+      case "fts":
+        return name.toUpperCase();
+      default:
+        return name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();
+      }
+    }
+
+    function findEnabled(moduleName, bool) {
+      return !!_.find(vm.state.eventsDescriptors[moduleName], {enabledByUI: bool});
+    }
+    function setEnabled(moduleName, bool) {
+      vm.state.eventsDescriptors[moduleName].forEach(function (desc) {
+        desc.enabledByUI = bool;
+      });
+    }
+    function toggleAll(moduleName) {
+      setEnabled(moduleName, findEnabled(moduleName, false));
+    }
     function activate() {
-      mnPromiseHelper(vm, mnAuditService.getAuditSettings())
+      mnPromiseHelper(vm, mnAuditService.getState())
         .applyToScope("state");
 
-      $scope.$watch('auditCtl.state', watchOnState, true);
+      $scope.$watch('auditCtl.state', _.debounce(watchOnState, 500), true);
     }
     function watchOnState(state) {
       if (!state || !$scope.rbac.cluster.admin.security.write) {
--- priv/public/ui/app/mn_admin/mn_security/mn_audit/mn_audit_service.js
+++ priv/public/ui/app/mn_admin/mn_security/mn_audit/mn_audit_service.js
@@ -2,22 +2,46 @@
   "use strict";
 
   angular.module('mnAuditService', [
+    'mnPoolDefault'
   ]).factory('mnAuditService', mnAuditServiceFactory);
 
-  function mnAuditServiceFactory($http) {
+  function mnAuditServiceFactory($http, $q, mnPoolDefault) {
     var mnAuditService = {
       getAuditSettings: getAuditSettings,
-      saveAuditSettings: saveAuditSettings
+      saveAuditSettings: saveAuditSettings,
+      getAuditDescriptors: getAuditDescriptors,
+      getState: getState
     };
 
     return mnAuditService;
+
+    function getState() {
+      var queries = [
+        getAuditSettings()
+      ];
+
+      if (mnPoolDefault.export.compat.atLeast55 &&
+          mnPoolDefault.export.isEnterprise) {
+        queries.push(getAuditDescriptors())
+      }
+
+      return $q.all(queries).then(unpack);
+    }
 
     function getAuditSettings() {
       return $http({
         method: 'GET',
         url: '/settings/audit'
-      }).then(function(resp) {
-        return unpack(resp.data);
+      }).then(function (resp) {
+        return resp.data;
+      });
+    }
+    function getAuditDescriptors() {
+      return $http({
+        method: 'GET',
+        url: '/settings/audit/descriptors'
+      }).then(function (resp) {
+        return _.clone(resp.data);
       });
     }
     function saveAuditSettings(data, validateOnly) {
@@ -30,12 +54,34 @@
         url: '/settings/audit',
         params: params,
         data: pack(data)
+      }).then(function (resp) {
+        if (resp.data.errors && resp.data.errors.disabledUsers) {
+          resp.data.errors.disabledUsers =
+            resp.data.errors.disabledUsers.replace(/\/local/gi,"/couchbase");
+        }
+        return resp;
       });
+    }
+    function mergeEvets(result, value) {
+      return result.concat(value);
+    }
+    function filterDisabled(result, desc) {
+      if (!desc.enabledByUI) {
+        result.push(desc.id);
+      }
+      return result;
     }
     function pack(data) {
       var result = {
         auditdEnabled: data.auditdEnabled
       };
+      if (mnPoolDefault.export.compat.atLeast55 && mnPoolDefault.export.isEnterprise) {
+        result.disabled = _.reduce(
+          _.reduce(data.eventsDescriptors, mergeEvets, []),
+          filterDisabled, []
+        ).join(',');
+        result.disabledUsers = data.disabledUsers.replace(/\/couchbase/gi,"/local");
+      }
       if (data.auditdEnabled) {
         result.rotateInterval = data.rotateInterval * formatTimeUnit(data.rotateUnit);
         result.logPath = data.logPath;
@@ -49,7 +95,10 @@
         case 'days': return 86400;
       }
     }
-    function unpack(data) {
+    function unpack(resp) {
+      var data = resp[0];
+      var eventsDescriptors = resp[1];
+
       if (data.rotateInterval % 86400 == 0) {
         data.rotateInterval /= 86400;
         data.rotateUnit = 'days';
@@ -59,6 +108,16 @@
       } else {
         data.rotateInterval /= 60;
         data.rotateUnit = 'minutes';
+      }
+      if (mnPoolDefault.export.compat.atLeast55 && mnPoolDefault.export.isEnterprise) {
+        var mapDisabledIDs = _.groupBy(data.disabled);
+        eventsDescriptors.forEach(function (desc) {
+          desc.enabledByUI = !mapDisabledIDs[desc.id];
+        });
+        data.eventsDescriptors = _.groupBy(eventsDescriptors, "module");
+        data.disabledUsers = data.disabledUsers.map(function (user) {
+          return user.name + "/" + (user.domain === "local" ? "couchbase" : user.domain);
+        }).join(',');
       }
       data.logPath = data.logPath || "";
       return data;
