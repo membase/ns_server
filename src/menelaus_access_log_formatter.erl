cb9c9fb5fcf910ec420c7fc4b1f78c2fad4e8ab3
Artem Stemkovski <astemkov@gmail.com>
2018-02-05 23:04:27 -0800

    another bulk update of copyrights
    Change-Id: Ie845677c060b8b35c1864476df314435b7e53f23
    Reviewed-on: http://review.couchbase.org/88908
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Aliaksey Artamonau <aliaksey.artamonau@couchbase.com>

--- src/menelaus_access_log_formatter.erl
+++ src/menelaus_access_log_formatter.erl
@@ -1,5 +1,5 @@
 %% @author Couchbase <info@couchbase.com>
-%% @copyright 2014-2015 Couchbase, Inc.
+%% @copyright 2014-2018 Couchbase, Inc.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
--- test/misc_tests.erl
+++ test/misc_tests.erl
@@ -1,6 +1,6 @@
 % Copyright (c) 2008, Cliff Moon
 % Copyright (c) 2008, Powerset, Inc
-% Copyright (c) 2009-2016, Couchbase, Inc.
+% Copyright (c) 2009-2018, Couchbase, Inc.
 %
 % All rights reserved.
 %
